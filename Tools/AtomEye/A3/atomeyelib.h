4e62ac41610e145b59871dc458b5fbdd26d58cb2
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-04-15 11:35:16 +0000

    Added done_init parameter and atomeyelib_help routine
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@4724 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/AtomEye/A3/atomeyelib.h
+++ Tools/AtomEye/A3/atomeyelib.h
@@ -1,8 +1,10 @@
 /* atomeye C api to be called from Python */
 
-int atomeyelib_main(int argc, char* argv[], void (*onclick)(int atom));
+int atomeyelib_main(int argc, char* argv[], void (*onclick)(int atom), void*(*onredraw), int *done_init);
 int atomeyelib_run_command(int iw, char *line, char **outstr);
+int atomeyelib_help(int iw, char *line, char **outstr);
 int atomeyelib_redraw(int iw);
 int atomeyelib_close(int iw);
 int atomeyelib_load_libatoms(int iw, Atoms *atoms, char *title, char **outstr);
-int atomeyelib_set_output(int on_off);
+
+/* int atomeyelib_set_output(int on_off); */
