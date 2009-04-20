3466eda6330156504087ceac41f56a50c5ed31db
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-04-20 16:44:04 +0000

    Auto-generate f2py_f2cmap
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@4781 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/setup.py
+++ Tools/quippy/setup.py
@@ -124,6 +124,19 @@ if not os.path.exists('./sizeof_void_ptr'):
 sizeof_void_ptr = int(os.popen('./sizeof_void_ptr').read())
 print 'sizeof_void_ptr = %d bytes' % sizeof_void_ptr
 
+# Generate .f2py_f2cmap
+size_t_lookup = {4:'int', 8:'long_long'}
+if sizeof_void_ptr not in size_t_lookup:
+    raise ValueError("Can't guess C type for size_t with sizeof(void *) = %d" % sizeof_void_ptr)
+cmapf = open('.fpy_f2cmap','w')
+cmapf.write("""{
+ 'real':{'dp':'double'},
+ 'complex':{'dp':'complex_double'},
+ 'integer':{'C_SIZE_T':'%s'}
+}
+""" % size_t_lookup[sizeof_void_ptr])
+cmapf.close()
+
 # Bit of a hack: we have to add directory which will contain .mod files
 build_base = 'build'
 if 'build' in sys.argv:
