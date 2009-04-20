50d3188e291331225892f9e2db67c6ea977781fd
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-04-20 13:09:52 +0000

    Remove onredraw, add onclose and onadvance
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@4774 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/AtomEye/A3/atomeyelib.h
+++ Tools/AtomEye/A3/atomeyelib.h
@@ -1,6 +1,7 @@
 /* atomeye C api to be called from Python */
 
-int atomeyelib_main(int argc, char* argv[], void (*onclick)(int atom), void*(*onredraw), int *done_init);
+int atomeyelib_main(int argc, char* argv[], void (*onclick)(int atom), 
+		    void (*onclose)(), void (*onadvance)(char *instr), int *done_init);
 int atomeyelib_run_command(int iw, char *line, char **outstr);
 int atomeyelib_help(int iw, char *line, char **outstr);
 int atomeyelib_redraw(int iw);
