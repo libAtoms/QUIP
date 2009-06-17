a853bee5efcafca81da02cc428c2f0698bf3f157
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-06-17 15:54:46 +0000

    IPModel_ASAP cutoff is array
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5230 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Core/IP.f95
+++ QUIP_Core/IP.f95
@@ -361,7 +361,7 @@ function IP_cutoff(this)
   case (FF_Brenner_2002)
      IP_cutoff = this%ip_brenner_2002%cutoff
   case (FF_ASAP)
-     IP_cutoff = this%ip_asap%cutoff
+     IP_cutoff = maxval(this%ip_asap%cutoff)
   ! Add new IP here
   case (FF_Template)
      IP_cutoff = this%ip_template%cutoff
