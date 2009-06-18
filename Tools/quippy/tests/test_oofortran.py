64f4824bae4aa2146c29fc8868d7d4eb7d1661ab
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-06-18 16:23:35 +0000

    Remove unnecessary copying. Fixture is recreated afresh for each test.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5244 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/test_oofortran.py
+++ Tools/quippy/tests/test_oofortran.py
@@ -7,6 +7,9 @@ class TestOOFortran(unittest.TestCase):
    def setUp(self):
       self.dia = diamond(5.44, 14)
 
+   def tearDown(self):
+      del self.dia
+
    def testdia(self):
       self.assertEqual(self.dia.n, 8)
       self.assert_((self.dia.z == 14).all())
@@ -22,16 +25,14 @@ class TestOOFortran(unittest.TestCase):
       self.assertEqual(self.dia.data, cp.data)
 
    def testgetset(self):
-      cp = self.dia.copy()
-      cp.cutoff = 1.3
-      self.assertEqual(cp.cutoff, 1.3)
+      self.dia.cutoff = 1.3
+      self.assertEqual(self.dia.cutoff, 1.3)
 
    def testroutine(self):
       self.assert_(self.dia.has_property('pos'))
 
    def testroutinekwargs(self):
-      cp = self.dia.copy()
-      cp.calc_connect(own_neighbour=True)
+      self.dia.calc_connect(own_neighbour=True)
 
    def testroutine2(self):
       sup = supercell(self.dia, 2, 2, 2)
@@ -40,29 +41,26 @@ class TestOOFortran(unittest.TestCase):
       self.assertRaises(TypeError, DynamicalSystem, atoms_in=ds)
 
    def testinterface(self):
-      cp = self.dia.copy()
-      self.assertRaises(TypeError, cp.add_atoms, 1)
-      cp.add_atoms([1.0,1.0,1.0], 1)
-      self.assertEqual(cp.n, 9)
-      self.assertEqual(cp.z[9], 1)
-      self.assertEqual(list(cp.pos[9]), [1.0, 1.0, 1.0])
+      self.assertRaises(TypeError, self.dia.add_atoms, 1)
+      self.dia.add_atoms([1.0,1.0,1.0], 1)
+      self.assertEqual(self.dia.n, 9)
+      self.assertEqual(self.dia.z[9], 1)
+      self.assertEqual(list(self.dia.pos[9]), [1.0, 1.0, 1.0])
 
    def testinterfacekwargs(self):
-      cp = self.dia.copy()
-      cp.add_atoms(pos=farray([1.0,1.0,1.0]), z=1)
-      self.assertEqual(cp.n, 9)
-      self.assertEqual(cp.z[9], 1)
-      self.assertEqual(list(cp.pos[9]), [1.0, 1.0, 1.0])
+      self.dia.add_atoms(pos=farray([1.0,1.0,1.0]), z=1)
+      self.assertEqual(self.dia.n, 9)
+      self.assertEqual(self.dia.z[9], 1)
+      self.assertEqual(list(self.dia.pos[9]), [1.0, 1.0, 1.0])
       
    def testfpointernone(self):
-      cp = self.dia.copy()
-      cp._fpointer = None
-      self.assertRaises(ValueError, cp._get_n)
-      self.assertRaises(ValueError, cp._set_n, 0)
+      self.dia._fpointer = None
+      self.assertRaises(ValueError, self.dia._get_n)
+      self.assertRaises(ValueError, self.dia._set_n, 0)
 
 
 def getTestSuite():
-   return unittest.TestLoader().loadTestsFromTestCase(TestOOFortran)
+   return unittest.TestSuite(unittest.TestLoader().loadTestsFromTestCase(TestOOFortran))
 
 if __name__ == '__main__':
    suite = getTestSuite()
