0d78ece514e5dfe4b04180369990caa7f2507449
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-07-01 20:11:50 +0000

    density_1d now reads all configs into memory, then processes them, in preparation for later 2-pass computation of error bars
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5354 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/density_1d.f95
+++ Tools/density_1d.f95
@@ -11,7 +11,10 @@ program density_1d
     integer, parameter                    :: DISTANCES_INIT = 1000000
     integer, parameter                    :: DISTANCES_INCR = 1000000
 
-    type(Atoms)                           :: structure
+    type(atoms_ll)                        :: structure_ll
+    type(atoms_ll_entry), pointer         :: structure_ll_entry
+    type(Atoms), pointer                  :: structure
+    type(Atoms)                           :: structure_in
     type(Table)                           :: distances, atom_table
     real(dp)                              :: d
     type(Inoutput)                        :: xyzfile, datafile
@@ -158,157 +161,167 @@ program density_1d
     call print('Reading data...')
 
     ! initialize frame counters
-    frame_count = 0
     frames_processed = 0
     frames_processed_intermed = 0
 
-    call atoms_set_cutoff(structure,cutoff)
+    status = 0
+    frame_count = 0
+    do while (frame_count < from-1)
+      frame_count = frame_count + 1
+      call read_xyz(xyzfile,status)
+      if (status/=0) exit
+    end do
+
+    ! read rest of configs, skipping decimation related ones, put in structure_ll
+    do while (status == 0 .and. (to <= 0 .or. frame_count < to))
+      frame_count = frame_count + 1
+      call new_entry(structure_ll, structure)
+      write(mainlog%unit,'(a,a,i0,$)') achar(13),'Read Frame ',frame_count
+      call read_xyz(structure_in, xyzfile, status=status)
+      call atoms_copy_without_connect(structure, structure_in, properties="pos:Z")
+      if (to > 0 .and. frame_count >= to) then
+	exit
+      endif
+
+      do i=1, (decimation-1)
+	frame_count = frame_count + 1
+	call read_xyz(xyzfile, status)
+	if (status /= 0) exit
+      end do
+    end do
+
+    if (status /= 0) then
+      call remove_last_entry(structure_ll)
+    endif
 
     call allocate(distances,0,1,0,0,DISTANCES_INIT)
     call set_increment(distances,DISTANCES_INCR)
 
-    !skip frames before from
-    do while (frame_count < (from-1))
-      call read_xyz(xyzfile,status)
-      if (status/=0) exit
-      frame_count = frame_count + 1
-    end do
-    !The next read frame is the (from)th.
-    call read_xyz(structure, xyzfile, status=status)
-    frame_count = frame_count + 1
-
     first_time=.true.
 
-    do
+    structure_ll_entry => structure_ll%first
 
-       if (status/=0) exit
+    frame_count = from
 
-       write(mainlog%unit,'(a,a,i0,$)') achar(13),'Frame ',frame_count
+    do while (associated(structure_ll_entry))
 
-       if (frame_count.ge.from) then
-          if ((frame_count.gt.to) .and.(to.gt.0)) exit
+       structure => structure_ll_entry%at
 
-          call atoms_set_cutoff(structure,cutoff)
+       write(mainlog%unit,'(a,a,i0,$)') achar(13),'Processing Frame ',frame_count
 
-          if (prop) call update_list(structure,atom_table,trim(prop_name),prop_val)
+	if (prop) call update_list(structure,atom_table,trim(prop_name),prop_val)
 
-          call wipe(distances)
+	call wipe(distances)
 
-          num_atoms = 0
+	num_atoms = 0
 
-          hist = 0.0_dp
-          do j = 1, structure%N
+	hist = 0.0_dp
+	do j = 1, structure%N
 
-             !Count the atoms
-             if (list) then
-                if (find(atom_table,j)/=0) num_atoms = num_atoms + 1
-             else
-                if (structure%Z(j) == Zb) num_atoms = num_atoms + 1
-             end if
+	   !Count the atoms
+	   if (list) then
+	      if (find(atom_table,j)/=0) num_atoms = num_atoms + 1
+	   else
+	      if (structure%Z(j) == Zb) num_atoms = num_atoms + 1
+	   end if
 
-             !Do we have a "Mask" atom? Cycle if not
-             if (list) then
-                if (find(atom_table,j)==0) cycle
-             else
-                if (structure%Z(j) /= Zb) cycle
-             end if
+	   !Do we have a "Mask" atom? Cycle if not
+	   if (list) then
+	      if (find(atom_table,j)==0) cycle
+	   else
+	      if (structure%Z(j) /= Zb) cycle
+	   end if
 
-             d = distance_min_image(structure,j,(/0._dp,0._dp,0._dp/))
-             if (d < cutoff) then
+	   d = distance_min_image(structure,j,(/0._dp,0._dp,0._dp/))
+	   if (d < cutoff+3.0*Gaussian_sigma) then
 #ifdef DEBUG
-                call print('Storing distance (/0,0,0/)--'//j//' = '//round(d,5)//'A')
+	      call print('Storing distance (/0,0,0/)--'//j//' = '//round(d,5)//'A')
 #endif
-                !Add this distance to the list
-                call accum_histogram(hist, d, 0.0_dp, cutoff, num_bins, Gaussian_smoothing, Gaussian_sigma)
-             end if
-          end do
+	      !Add this distance to the list
+	      call accum_histogram(hist, d, 0.0_dp, cutoff, num_bins, Gaussian_smoothing, Gaussian_sigma)
+	   end if
+	end do
 
 
-          frames_processed = frames_processed + 1
-          frames_processed_intermed = frames_processed_intermed + 1
+	frames_processed = frames_processed + 1
+	frames_processed_intermed = frames_processed_intermed + 1
 
 #ifdef DEBUG
-          call print('Number of atoms = '//num_atoms)
+	call print('Number of atoms = '//num_atoms)
 #endif
 
-          !Calculate B atom density
-          density = real(num_atoms,dp) / cell_volume(structure)
+	!Calculate B atom density
+	density = real(num_atoms,dp) / cell_volume(structure)
 
-          !Normalise histogram
-          do i = 1, num_bins
-             r = (real(i,dp) - 1._dp) * bin_width
-             dV = 4.0_dp * PI * (r*r + r*bin_width + bin_width*bin_width/3.0_dp ) * bin_width
-             hist(i) = hist(i) / (dV * density * 1._dp)
-          end do
+	!Normalise histogram
+	do i = 1, num_bins
+	   r = (real(i,dp) - 1._dp) * bin_width
+	   dV = 4.0_dp * PI * (r*r + r*bin_width + bin_width*bin_width/3.0_dp ) * bin_width
+	   hist(i) = hist(i) / (dV * density * 1._dp)
+	end do
 
-          !Accumulate the data
-          hist_sum = hist_sum + hist
-          hist_sum_intermed = hist_sum_intermed + hist
+	!Accumulate the data
+	hist_sum = hist_sum + hist
+	hist_sum_intermed = hist_sum_intermed + hist
 
-          !copy the current averages into the y coordinates of the plot
-          data(:,2) = hist_sum / real(frames_processed,dp)
-          data_intermed(:,2) = hist_sum_intermed / real(frames_processed_intermed,dp)
+	!copy the current averages into the y coordinates of the plot
+	data(:,2) = hist_sum / real(frames_processed,dp)
+	data_intermed(:,2) = hist_sum_intermed / real(frames_processed_intermed,dp)
 
-          !integrate the average data
-          hist_int = 0.0_dp
-          hist_int_intermed = 0.0_dp
-          do i = 1, num_bins
-             r = data(i,1)
-             dV = PI * (4.0_dp * r*r + bin_width*bin_width/3.0_dp) * bin_width
-             hist_int = hist_int + data(i,2)*dV*density
-             data(i,3) = hist_int
-             hist_int_intermed = hist_int_intermed + data_intermed(i,2)*dV*density
-             data_intermed(i,3) = hist_int_intermed
-          end do
+	!integrate the average data
+	hist_int = 0.0_dp
+	hist_int_intermed = 0.0_dp
+	do i = 1, num_bins
+	   r = data(i,1)
+	   dV = PI * (4.0_dp * r*r + bin_width*bin_width/3.0_dp) * bin_width
+	   hist_int = hist_int + data(i,2)*dV*density
+	   data(i,3) = hist_int
+	   hist_int_intermed = hist_int_intermed + data_intermed(i,2)*dV*density
+	   data_intermed(i,3) = hist_int_intermed
+	end do
 
-          if (Density_Time_Evolution_Rate > 0) then
-            if (mod(frames_processed,Density_Time_Evolution_Rate)==0) then
-               if (first_time) then
-                 call initialise(datafile,datafilename,action=OUTPUT)
-                 first_time = .false.
-               else
-                 call initialise(datafile,datafilename,action=OUTPUT,append=.true.)
-                 call print('',file=datafile)
-                 call print('',file=datafile)
-               endif
-               call print('# Density 1D',file=datafile)
-               call print('# Input file: '//trim(xyzfilename),file=datafile)
-               call print('#      Frames read = '//frame_count,file=datafile)
-               call print('# Frames processed = '//frames_processed,file=datafile)
-               call print(data_intermed,file=datafile)
-               call finalise(datafile)
-               hist_sum_intermed = 0.0_dp
-               frames_processed_intermed = 0
-            endif
-          endif
+	if (Density_Time_Evolution_Rate > 0) then
+	  if (mod(frames_processed,Density_Time_Evolution_Rate)==0) then
+	     if (first_time) then
+	       call initialise(datafile,datafilename,action=OUTPUT)
+	       first_time = .false.
+	     else
+	       call initialise(datafile,datafilename,action=OUTPUT,append=.true.)
+	       call print('',file=datafile)
+	       call print('',file=datafile)
+	     endif
+	     call print('# Density 1D',file=datafile)
+	     call print('# Input file: '//trim(xyzfilename),file=datafile)
+	     call print('#      Frames read = '//frame_count,file=datafile)
+	     call print('# Frames processed = '//frames_processed,file=datafile)
+	     call print(data_intermed,file=datafile)
+	     call finalise(datafile)
+	     hist_sum_intermed = 0.0_dp
+	     frames_processed_intermed = 0.0_dp
+	  endif
+	endif
 
-          !Write the current data. This allows the user to Ctrl-C after a certain number
-          !of frames if things are going slowly
-          if (IO_Rate > 0) then
-            if (mod(frames_processed,IO_Rate)==0) then
-               call initialise(datafile,datafilename,action=OUTPUT)
-               call print('# Density 1D',file=datafile)
-               call print('# Input file: '//trim(xyzfilename),file=datafile)
-               call print('#      Frames read = '//frame_count,file=datafile)
-               call print('# Frames processed = '//frames_processed,file=datafile)
-               call print(data,file=datafile)
-               call finalise(datafile)
-            endif
-          endif
-       endif     
+	!Write the current data. This allows the user to Ctrl-C after a certain number
+	!of frames if things are going slowly
+	if (IO_Rate > 0) then
+	  if (mod(frames_processed,IO_Rate)==0) then
+	     call initialise(datafile,datafilename,action=OUTPUT)
+	     call print('# Density 1D',file=datafile)
+	     call print('# Input file: '//trim(xyzfilename),file=datafile)
+	     call print('#      Frames read = '//frame_count,file=datafile)
+	     call print('# Frames processed = '//frames_processed,file=datafile)
+	     call print(data,file=datafile)
+	     call finalise(datafile)
+	  endif
+	endif
 
-       !Skip ahead (decimation-1) frames in the xyz file
-       do i = 1, (decimation-1)
-          call read_xyz(xyzfile,status)
-          if (status/=0) exit
-          frame_count = frame_count + 1
-       end do
-       if (status/=0) exit
        !Try to read another frame
-       call read_xyz(structure,xyzfile,status=status)
-       frame_count = frame_count + 1
-
+       structure_ll_entry => structure_ll_entry%next
+       frame_count = frame_count + decimation
+  
     end do
+
+    frame_count = frame_count - decimation
 
     if (Density_Time_Evolution_Rate > 0) then
       call initialise(datafile,datafilename,action=OUTPUT,append=.true.)
@@ -329,7 +342,7 @@ program density_1d
 
     !Free up memory
     call finalise(distances)
-    call finalise(structure)
+    call finalise(structure_in)
     call finalise(xyzfile)
 
     deallocate(hist, hist_sum, data)
@@ -371,13 +384,13 @@ contains
       py = real(iy-1-(n_samples/2), dp)/n_samples_d*range
       do iz=1, n_samples+1
       pz = real(iz-1-(n_samples/2), dp)/n_samples_d*range
-        io = (ix-1)*(n_samples+1)**2 + (iy-1)*(n_samples+1) + (iz-1) + 1
-        d_a(io) = sqrt((px+d)**2+py**2+pz**2)
-        w_a(io) = normalization*exp(-(px**2+py**2+pz**2)/sigma**2)
+	io = (ix-1)*(n_samples+1)**2 + (iy-1)*(n_samples+1) + (iz-1) + 1
+	d_a(io) = sqrt((px+d)**2+py**2+pz**2)
+	w_a(io) = normalization*exp(-(px**2+py**2+pz**2)/sigma**2)
       end do
       end do
       end do
-      hist_t = histogram(d_a, minx, maxx, num_bins, weight_vector=w_a)
+      hist_t = histogram(d_a, minx, maxx, num_bins, weight_vector=w_a, drop_outside=.true.)
       deallocate(d_a)
       deallocate(w_a)
     else
