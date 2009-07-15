8a65d5760faaf6e3399faa2587a49684953db591
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-07-15 17:29:29 +0000

    Test of povray module
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5456 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

---
+++ Tools/quippy/tests/test_povray.py
@@ -0,0 +1,58 @@
+from quippy import *
+import unittest
+from quippytest import *
+
+class TestPovray(QuippyTestCase):
+
+   def setUp(self):
+      from StringIO import StringIO
+      self.s = StringIO()
+      self.at = diamond(5.44, 14)
+
+      self.pov_ref = """#include "colors.inc"
+
+camera
+{
+  right x*400/600
+  location <0.000000,0.000000,-100.000000>
+  look_at <0,0,0>
+}
+
+background
+{
+  colour White
+}
+
+light_source
+{
+  <0.000000,0.000000,-100.000000>
+  colour White
+}
+union {
+sphere { <0.000000,-0.000000,0.000000>, 1.0 pigment {color <1.000000,1.000000,1.000000> } finish { phong .8 } }
+sphere { <1.360000,-1.360000,1.360000>, 1.0 pigment {color <1.000000,1.000000,1.000000> } finish { phong .8 } }
+sphere { <2.720000,-2.720000,0.000000>, 1.0 pigment {color <1.000000,1.000000,1.000000> } finish { phong .8 } }
+sphere { <4.080000,-4.080000,1.360000>, 1.0 pigment {color <1.000000,1.000000,1.000000> } finish { phong .8 } }
+sphere { <2.720000,-0.000000,2.720000>, 1.0 pigment {color <1.000000,1.000000,1.000000> } finish { phong .8 } }
+sphere { <4.080000,-1.360000,4.080000>, 1.0 pigment {color <1.000000,1.000000,1.000000> } finish { phong .8 } }
+sphere { <0.000000,-2.720000,2.720000>, 1.0 pigment {color <1.000000,1.000000,1.000000> } finish { phong .8 } }
+sphere { <1.360000,-4.080000,4.080000>, 1.0 pigment {color <1.000000,1.000000,1.000000> } finish { phong .8 } }
+}"""
+
+   def testpov(self):
+      self.at.write(self.s, format='pov')
+      self.assertEqual(self.pov_ref, self.s.getvalue())
+
+
+def getTestSuite():
+   tl = unittest.TestLoader()
+   return unittest.TestSuite([tl.loadTestsFromTestCase(TestPovray)])
+
+if __name__ == '__main__':
+   suite = getTestSuite()
+   unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+
+
+
