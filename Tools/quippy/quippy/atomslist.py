f529d9de12d7f31eab093b5a4f5d78b2c2a8e88c
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-07-15 17:27:04 +0000

    Move netcdf code out to xyz_netcdf.py. Define atoms_reader and atoms_writer decorators
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5451 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy/atomslist.py
+++ Tools/quippy/quippy/atomslist.py
@@ -18,12 +18,15 @@ class AtomsList(object):
             format = source.__class__
 
       if format in AtomsReaders:
-         self._source = iter(AtomsReaders[format](source, *args, **kwargs))
+         self._source = AtomsReaders[format](source, *args, **kwargs)
+         self._itersource = iter(self._source)
       else:
          if not hasattr(source, '__iter__'):
             self._source = [source]
+            self._itersource = iter(self._source)
          else:
             self._source = source
+            self._itersource = iter(self._source)
          
       self._randomaccess = hasattr(self._source, '__getitem__') and hasattr(self._source, '__len__')
       if lazy:
@@ -52,9 +55,10 @@ class AtomsList(object):
       for frame in range(len(self._list)):
          yield self[frame]
       if not self._randomaccess:
-         for at in self._source:
+         for at in self._itersource:
             self._list.append(at)
             yield at
+         self._itersource.close()
       self._randomaccess = True
 
    def __iter__(self):
@@ -73,14 +77,14 @@ class AtomsList(object):
       if isinstance(frame, int):
          if frame < 0: frame = frame + len(self._list)
          if not self._randomaccess and frame >= len(self._list):
-            self._list.extend(list(itertools.islice(self._source, frame-len(self._list)+1)))
+            self._list.extend(list(itertools.islice(self._itersource, frame-len(self._list)+1)))
          if self._list[frame] is None:
             self._list[frame] = self._source[frame]
          return self._list[frame]
 
       elif isinstance(frame, slice):
          if not self._randomaccess and frame.stop >= len(self._list):
-            self._list.extend(list(itertools.islice(self._source, frame.stop != sys.maxint and frame.stop-len(self._list)+1 or None)))
+            self._list.extend(list(itertools.islice(self._itersource, frame.stop != sys.maxint and frame.stop-len(self._list)+1 or None)))
          allatoms = range(len(self._list))
          subatoms = [ allatoms[i] for i in range(*frame.indices(len(allatoms))) ]
          res = []
@@ -147,156 +151,30 @@ class AtomsList(object):
 
       res = []
       for i, a in enumerate(self.iteratoms()):
-         res.append(a.write(dest))
+         res.append(dest.send(a))
          if progress and i % update_interval == 0:
             if self._randomaccess:
                pb(i)
             else:
                print i
+      dest.close()
       return res
 
+# Decorators to add readers and writers
 
-try:
-   from quippy import CInOutput, INPUT, OUTPUT, INOUT
+def atoms_reader(source):
+   def decorate(func):
+      from quippy import AtomsReaders
+      if not source in AtomsReaders:
+         AtomsReaders[source] = func
+      return func
+   return decorate
 
-   class CInOutputReader(object):
-      """Generator to read atoms sequentially from a CInOutput"""
+def atoms_writer(dest):
+   def decorate(func):
+      from quippy import AtomsWriters
+      if not dest in AtomsWriters:
+         AtomsWriters[dest] = func
+      return func
+   return decorate
 
-      def __init__(self, source, frame=None, start=0, stop=None, step=1):
-         if isinstance(source, str):
-            self.source = CInOutput(source, action=INPUT, append=False)
-         else:
-            self.source = source
-
-         if frame is not None:
-            self.start = frame
-            self.stop = frame+1
-            self.step = 1
-         else:
-            self.start = start
-            if stop is None:
-               stop = len(self.source)
-            self.stop = stop
-            self.step = step
-
-      def __iter__(self):
-         for frame in range(self.start,self.stop,self.step):
-            yield self.source[frame]
-
-      def __len__(self):
-         return len(source)
-
-      def __getitem__(self, idx):
-         return self.source[idx]
-
-   def CInOutputStdinReader(source):
-      source = CInOutput(source, action=INPUT)
-      while True:
-         try:
-            yield source.read()
-         except RuntimeError:
-            break         
-
-   def CInOutputWriter(dest, append=False):
-      """Generator to write atoms sequentially to a CInOutput"""
-
-      if isinstance(dest, str):
-         dest = CInOutput(dest, action=OUTPUT, append=append)
-
-      at = yield None
-      while True:
-         dest.write(at)
-         at = yield None
-
-   AtomsReaders['xyz'] = AtomsReaders['nc'] = AtomsReaders[CInOutput] = CInOutputReader
-   AtomsWriters['xyz'] = AtomsWriters['nc'] = AtomsWriters[CInOutput] = CInOutputWriter
-   AtomsReaders['stdin'] = CInOutputStdinReader
-   AtomsWriters['stdout'] = CInOutputWriter
-
-   
-except ImportError:
-   print 'Warning: CInOutput not found - Atoms read/write disabled'
-
-
-try:
-   from netCDF4 import Dataset
-
-   def netcdf4_read(source, frame=0):
-
-      if isinstance(source, str):
-         source = Dataset(source)
-      
-      from quippy import Atoms, make_lattice
-      from math import pi
-      from quippy import (PROPERTY_INT, PROPERTY_REAL, PROPERTY_STR, PROPERTY_LOGICAL,
-                          T_NONE, T_INTEGER, T_REAL, T_COMPLEX,
-                          T_CHAR, T_LOGICAL, T_INTEGER_A,
-                          T_REAL_A, T_COMPLEX_A, T_CHAR_A, T_LOGICAL_A)
-
-      DEG_TO_RAD = pi/180.0
-
-      remap_names = {'coordinates': 'pos',
-                     'velocities': 'velo',
-                     'cell_lengths': None,
-                     'cell_angles': None}
-
-      prop_type_to_value = {PROPERTY_INT: 0,
-                            PROPERTY_REAL: 0.0,
-                            PROPERTY_STR: "",
-                            PROPERTY_LOGICAL: False}
-
-      prop_dim_to_ncols = {('frame','atom','spatial'): 3,
-                           ('frame','atom','label'): 1,
-                           ('frame', 'atom'): 1}
-
-
-      cl = source.variables['cell_lengths'][frame]
-      ca = source.variables['cell_angles'][frame]
-      lattice = make_lattice(cl[0],cl[1],cl[2],ca[0]*DEG_TO_RAD,ca[1]*DEG_TO_RAD,ca[2]*DEG_TO_RAD)
-
-      at = Atoms(n=len(source.dimensions['atom']), lattice=lattice)
-
-      for name, var in source.variables.iteritems():
-         name = remap_names.get(name, name)
-
-         if name is None:
-            continue
-
-         if 'frame' in var.dimensions:
-            if 'atom' in var.dimensions:
-               # It's a property
-               at.add_property(name, prop_type_to_value[var.type],
-                              n_cols=prop_dim_to_ncols[var.dimensions])
-               getattr(at, name.lower())[...] = var[frame].T
-            else:
-               # It's a param
-               if var.dimensions == ('frame','string'):
-                  # if it's a single string, join it and strip it
-                  at.params[name] = ''.join(var[frame]).strip()
-               else:
-                  at.params[name] = var[frame]
-
-      yield at
-
-   AtomsReaders[Dataset] = netcdf4_read
-   if not 'nc' in AtomsReaders:
-      AtomsReaders['nc'] = netcdf4_read
-   
-
-   class NetCDFAtomsList(Dataset, AtomsList):
-      def __init__(self, source):
-         AtomsList.__init__(self, source)
-         Dataset.__init__(self, source)
-
-      def __getattr__(self, name):
-         return Dataset.__getattr__(self,name)
-
-      def __setattr__(self, name, value):
-         if name.startswith('_'):
-            self.__dict__[name] = value
-         else:
-            Dataset.__setattr__(self, name, value)
-
-except ImportError:
-   print 'Warning: netCDF4 not found. NetCDFAtomsReader disabled.'
-      
