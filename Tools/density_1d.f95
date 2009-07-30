5dc4139f1775bc4824ad7f00bf641e0785751037
cv255 <cv255@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-07-30 14:09:51 +0000

    Added standard deviation calculation.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5533 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/density_1d.f95
+++ Tools/density_1d.f95
@@ -56,6 +56,10 @@ program density_1d
     integer :: last_file_frame_n
     real(dp) :: cur_time
 
+    type(Table)                         :: density_bins      !densities of each bin for all the unskipped structure
+    type(Table)                         :: density_bins_intermed      !densities of each bin for all the unskipped structure
+    real(dp), allocatable, dimension(:)   :: means, sds
+    logical                             :: do_statistics
 
   !Start up LOTF, suppressing messages
     call system_initialise(NORMAL)
@@ -82,6 +86,7 @@ program density_1d
     call param_register(params_in, 'Gaussian', 'F', Gaussian_smoothing)
     call param_register(params_in, 'sigma', '0.0', Gaussian_sigma)
     call param_register(params_in, 'fortran_io', 'F', fortran_io)
+    call param_register(params_in, 'do_statistics', 'F', do_statistics)
     if (.not. param_read_args(params_in, do_check = .true.)) then
        if (EXEC_NAME == '<UNKNOWN>') then
           call print_usage
@@ -116,6 +121,7 @@ program density_1d
     endif
     call print('     Gaussians: '//Gaussian_smoothing)
     if (Gaussian_smoothing) call print('        sigma: '//round(Gaussian_sigma,3))
+    call print(' do_statistics: '//do_statistics)
     call print('==================================')
     call print('')
 
@@ -152,8 +158,16 @@ program density_1d
     !Make num_bins and bin_width consistent
     num_bins = ceiling(cutoff / bin_width)
     bin_width = cutoff / real(num_bins,dp)
+    if (do_statistics) call initialise(density_bins,0,num_bins,0,0,0)
+    if (do_statistics) call initialise(density_bins_intermed,0,num_bins,0,0,0)
 
-    allocate( hist(num_bins), hist_sum(num_bins), hist_sum_intermed(num_bins), data(num_bins,3), data_intermed(num_bins,3) )
+    allocate( hist(num_bins), hist_sum(num_bins), hist_sum_intermed(num_bins))
+    if (do_statistics) then
+       allocate(data(num_bins,4), data_intermed(num_bins,4) )
+       allocate(means(num_bins), sds(num_bins))
+    else
+       allocate(data(num_bins,3), data_intermed(num_bins,3) )
+    endif
 
     hist = 0.0_dp
     hist_sum = 0.0_dp
@@ -193,26 +207,26 @@ program density_1d
         frame_count = frame_count + 1
         write(mainlog%unit,'(a,a,i0,$)') achar(13),'Read Frame ',frame_count
         call read_xyz(structure_in, xyzfile, status=status)
-	skip_frame = .false.
-	if (min_time > 0.0_dp .or. max_time > 0.0_dp) then
-	  if (get_value(structure_in%params,"Time",cur_time)) then
-	    if (cur_time < min_time .or. cur_time > max_time) skip_frame = .true.
-	  else
-	    call system_abort("ERROR: min_time="//min_time//" > 0.0 or max_time="//max_time//" > 0.0, but Time field wasn't found in config " // frame_count)
-	  endif
-	endif
-	if (.not. skip_frame) then
-	  call new_entry(structure_ll, structure)
-	  call atoms_copy_without_connect(structure, structure_in, properties="pos:Z")
-	endif
+        skip_frame = .false.
+        if (min_time > 0.0_dp .or. max_time > 0.0_dp) then
+          if (get_value(structure_in%params,"Time",cur_time)) then
+            if (cur_time < min_time .or. cur_time > max_time) skip_frame = .true.
+          else
+            call system_abort("ERROR: min_time="//min_time//" > 0.0 or max_time="//max_time//" > 0.0, but Time field wasn't found in config " // frame_count)
+          endif
+        endif
+        if (.not. skip_frame) then
+         call new_entry(structure_ll, structure)
+          call atoms_copy_without_connect(structure, structure_in, properties="pos:Z")
+        endif
         if (to > 0 .and. frame_count >= to) then
-	  exit
+          exit
         endif
   
         do i=1, (decimation-1)
-  	frame_count = frame_count + 1
-  	call read_xyz(xyzfile, status)
-  	if (status /= 0) exit
+          frame_count = frame_count + 1
+          call read_xyz(xyzfile, status)
+          if (status /= 0) exit
         end do
       end do
   
@@ -226,51 +240,51 @@ program density_1d
 
       status = 0
       if (from > 0) then
-	frame_count = from
+        frame_count = from
       else
-	frame_count = 1
+        frame_count = 1
       endif
       last_file_frame_n = 0
 
       if (xyzfile_is_list) then
-	call initialise(xyzfile_list, trim(xyzfilename), INPUT)
-	xyzfilename = read_line(xyzfile_list, status)
-	if (status /= 0) call finalise(xyzfile_list)
+        call initialise(xyzfile_list, trim(xyzfilename), INPUT)
+        xyzfilename = read_line(xyzfile_list, status)
+        if (status /= 0) call finalise(xyzfile_list)
       endif
 
       do while (status == 0) ! loop over files
-	call initialise(cxyzfile,trim(xyzfilename),action=INPUT)
-	status = 0
-	do while ((to <= 0 .or. frame_count <= to) .and. status == 0)
-	  write(mainlog%unit,'(4a,i0,a,i0,$)') achar(13), 'Read file ',trim(xyzfilename), ' Frame ',frame_count,' which in this file is frame (zero based) ',(frame_count-1-last_file_frame_n)
-	  call read(cxyzfile, structure_in, frame=frame_count-1-last_file_frame_n, status=status)
-	  if (status == 0) then
-	    skip_frame = .false.
-	    if (min_time > 0.0_dp .or. max_time > 0.0_dp) then
-	      if (get_value(structure_in%params,"Time",cur_time)) then
-		if (cur_time < min_time .or. cur_time > max_time) skip_frame = .true.
-	      else
-		call system_abort("ERROR: min_time="//min_time//" > 0 but Time field wasn't found in config " // frame_count)
-	      endif
-	    endif
-	    if (.not. skip_frame) then
+        call initialise(cxyzfile,trim(xyzfilename),action=INPUT)
+        status = 0
+        do while ((to <= 0 .or. frame_count <= to) .and. status == 0)
+          write(mainlog%unit,'(4a,i0,a,i0,$)') achar(13), 'Read file ',trim(xyzfilename), ' Frame ',frame_count,' which in this file is frame (zero based) ',(frame_count-1-last_file_frame_n)
+          call read(cxyzfile, structure_in, frame=frame_count-1-last_file_frame_n, status=status)
+          if (status == 0) then
+            skip_frame = .false.
+            if (min_time > 0.0_dp .or. max_time > 0.0_dp) then
+              if (get_value(structure_in%params,"Time",cur_time)) then
+                if (cur_time < min_time .or. cur_time > max_time) skip_frame = .true.
+              else
+                call system_abort("ERROR: min_time="//min_time//" > 0 but Time field wasn't found in config " // frame_count)
+              endif
+            endif
+            if (.not. skip_frame) then
 	      call new_entry(structure_ll, structure)
-	      call atoms_copy_without_connect(structure, structure_in, properties="pos:Z")
-	    else
-	      write (mainlog%unit,'(a,$)') " skip"
-	    endif
-	    frame_count = frame_count + decimation
-	  endif
-	end do
-	last_file_frame_n = frame_count - 1
-	call finalise(cxyzfile)
+              call atoms_copy_without_connect(structure, structure_in, properties="pos:Z")
+            else
+              write (mainlog%unit,'(a,$)') " skip"
+            endif
+            frame_count = frame_count + decimation
+          endif
+        end do
+        last_file_frame_n = frame_count - 1
+        call finalise(cxyzfile)
 
-	if (xyzfile_is_list) then
-	  xyzfilename = read_line(xyzfile_list, status)
-	  if (status /= 0) call finalise(xyzfile_list)
-	else
-	  status = 1
-	endif
+        if (xyzfile_is_list) then
+          xyzfilename = read_line(xyzfile_list, status)
+          if (status /= 0) call finalise(xyzfile_list)
+        else
+          status = 1
+        endif
 
       end do
 
@@ -292,7 +306,7 @@ program density_1d
 
        write(mainlog%unit,'(a,a,i0,$)') achar(13),'Processing Frame ',frame_count
 
-	if (prop) call list_matching_prop(structure,atom_table,trim(prop_name),prop_val)
+        if (prop) call list_matching_prop(structure,atom_table,trim(prop_name),prop_val)
 
 	call wipe(distances)
 
@@ -326,77 +340,104 @@ program density_1d
 	end do
 
 
-	frames_processed = frames_processed + 1
-	frames_processed_intermed = frames_processed_intermed + 1
+        frames_processed = frames_processed + 1
+        frames_processed_intermed = frames_processed_intermed + 1
 
 #ifdef DEBUG
-	call print('Number of atoms = '//num_atoms)
+        call print('Number of atoms = '//num_atoms)
 #endif
 
-	!Calculate B atom density
-	density = real(num_atoms,dp) / cell_volume(structure)
+        !Calculate B atom density
+        density = real(num_atoms,dp) / cell_volume(structure)
 
-	!Normalise histogram
-	do i = 1, num_bins
-	   r = (real(i,dp) - 1._dp) * bin_width
-	   dV = 4.0_dp * PI * (r*r + r*bin_width + bin_width*bin_width/3.0_dp ) * bin_width
-	   hist(i) = hist(i) / (dV * density * 1._dp)
-	end do
+        !Normalise histogram
+        do i = 1, num_bins
+           r = (real(i,dp) - 1._dp) * bin_width
+           dV = 4.0_dp * PI * (r*r + r*bin_width + bin_width*bin_width/3.0_dp ) * bin_width
+           hist(i) = hist(i) / (dV * density * 1._dp)
+        end do
 
-	!Accumulate the data
-	hist_sum = hist_sum + hist
-	hist_sum_intermed = hist_sum_intermed + hist
+        !Accumulate the data
+        hist_sum = hist_sum + hist
+        hist_sum_intermed = hist_sum_intermed + hist
+        if (do_statistics) then
+!call print('Append '//hist(1:num_bins))
+           call append(density_bins,realpart=hist(1:num_bins))
+           call append(density_bins_intermed,realpart=hist(1:num_bins))
+        endif
 
-	!copy the current averages into the y coordinates of the plot
-	data(:,2) = hist_sum / real(frames_processed,dp)
-	data_intermed(:,2) = hist_sum_intermed / real(frames_processed_intermed,dp)
+        !copy the current averages into the y coordinates of the plot
+        data(:,2) = hist_sum / real(frames_processed,dp)
+        data_intermed(:,2) = hist_sum_intermed / real(frames_processed_intermed,dp)
 
-	!integrate the average data
-	hist_int = 0.0_dp
-	hist_int_intermed = 0.0_dp
-	do i = 1, num_bins
-	   r = data(i,1)
-	   dV = PI * (4.0_dp * r*r + bin_width*bin_width/3.0_dp) * bin_width
-	   hist_int = hist_int + data(i,2)*dV*density
-	   data(i,3) = hist_int
-	   hist_int_intermed = hist_int_intermed + data_intermed(i,2)*dV*density
-	   data_intermed(i,3) = hist_int_intermed
-	end do
+        !integrate the average data
+        hist_int = 0.0_dp
+        hist_int_intermed = 0.0_dp
+        do i = 1, num_bins
+           r = data(i,1)
+           dV = PI * (4.0_dp * r*r + bin_width*bin_width/3.0_dp) * bin_width
+           hist_int = hist_int + data(i,2)*dV*density
+           data(i,3) = hist_int
+           hist_int_intermed = hist_int_intermed + data_intermed(i,2)*dV*density
+           data_intermed(i,3) = hist_int_intermed
+        end do
 
-	if (Density_Time_Evolution_Rate > 0) then
-	  if (mod(frames_processed,Density_Time_Evolution_Rate)==0) then
-	     if (first_time) then
-	       call initialise(datafile,datafilename,action=OUTPUT)
-	       first_time = .false.
-	     else
-	       call initialise(datafile,datafilename,action=OUTPUT,append=.true.)
-	       call print('',file=datafile)
-	       call print('',file=datafile)
-	     endif
-	     call print('# Density 1D',file=datafile)
-	     call print('# Input file: '//trim(xyzfilename),file=datafile)
-	     call print('#      Frames read = '//frame_count,file=datafile)
-	     call print('# Frames processed = '//frames_processed,file=datafile)
-	     call print(data_intermed,file=datafile)
-	     call finalise(datafile)
-	     hist_sum_intermed = 0.0_dp
-	     frames_processed_intermed = 0.0_dp
-	  endif
-	endif
+        !calculate the standard deviation to print in the 4th column
+        if (do_statistics) then
+           !intermed
+           means = 0._dp !should be equal to data_intermed(:,2)
+           sds = 0._dp
+           do i=1,num_bins !calc
+!call print('Raw data:'//density_bins_intermed%real(i,1:density_bins_intermed%N))
+              call calc_stat(density_bins_intermed%real(i,1:density_bins_intermed%N),means(i),sds(i))
+!call print(i//' mean: '//means(i)//' SD: '//sds(i))
+           enddo
+           data_intermed(1:num_bins,4) = sds(1:num_bins)
+           !accumulated
+           means = 0._dp !should be equal to data(:,2)
+           sds = 0._dp
+           do i=1,num_bins !calc
+!call print('Raw data:'//density_bins%real(i,1:density_bins%N))
+              call calc_stat(density_bins%real(i,1:density_bins%N),means(i),sds(i))
+!call print(i//' mean: '//means(i)//' SD: '//sds(i))
+           enddo
+           data(1:num_bins,4) = sds(1:num_bins)
+        endif
 
-	!Write the current data. This allows the user to Ctrl-C after a certain number
-	!of frames if things are going slowly
-	if (IO_Rate > 0) then
-	  if (mod(frames_processed,IO_Rate)==0) then
-	     call initialise(datafile,datafilename,action=OUTPUT)
-	     call print('# Density 1D',file=datafile)
-	     call print('# Input file: '//trim(xyzfilename),file=datafile)
-	     call print('#      Frames read = '//frame_count,file=datafile)
-	     call print('# Frames processed = '//frames_processed,file=datafile)
-	     call print(data,file=datafile)
-	     call finalise(datafile)
-	  endif
-	endif
+        if (Density_Time_Evolution_Rate > 0) then
+          if (mod(frames_processed,Density_Time_Evolution_Rate)==0) then
+             if (first_time) then
+               call initialise(datafile,datafilename,action=OUTPUT)
+               first_time = .false.
+             else
+               call initialise(datafile,datafilename,action=OUTPUT,append=.true.)
+               call print('',file=datafile)
+               call print('',file=datafile)
+             endif
+             call print('# Density 1D',file=datafile)
+             call print('# Input file: '//trim(xyzfilename),file=datafile)
+             call print('#      Frames read = '//frame_count,file=datafile)
+             call print('# Frames processed = '//frames_processed,file=datafile)
+             call print(data_intermed,file=datafile)
+             call finalise(datafile)
+             hist_sum_intermed = 0.0_dp
+             frames_processed_intermed = 0.0_dp
+          endif
+        endif
+
+        !Write the current data. This allows the user to Ctrl-C after a certain number
+        !of frames if things are going slowly
+        if (IO_Rate > 0) then
+          if (mod(frames_processed,IO_Rate)==0) then
+             call initialise(datafile,datafilename,action=OUTPUT)
+             call print('# Density 1D',file=datafile)
+             call print('# Input file: '//trim(xyzfilename),file=datafile)
+             call print('#      Frames read = '//frame_count,file=datafile)
+             call print('# Frames processed = '//frames_processed,file=datafile)
+             call print(data,file=datafile)
+             call finalise(datafile)
+          endif
+        endif
 
        !Try to read another frame
        structure_ll_entry => structure_ll_entry%next
@@ -467,9 +508,9 @@ contains
       py = real(iy-1-(n_samples/2), dp)/n_samples_d*range
       do iz=1, n_samples+1
       pz = real(iz-1-(n_samples/2), dp)/n_samples_d*range
-	io = (ix-1)*(n_samples+1)**2 + (iy-1)*(n_samples+1) + (iz-1) + 1
-	d_a(io) = sqrt((px+d)**2+py**2+pz**2)
-	w_a(io) = normalization*exp(-(px**2+py**2+pz**2)/sigma**2)
+        io = (ix-1)*(n_samples+1)**2 + (iy-1)*(n_samples+1) + (iz-1) + 1
+        d_a(io) = sqrt((px+d)**2+py**2+pz**2)
+        w_a(io) = normalization*exp(-(px**2+py**2+pz**2)/sigma**2)
       end do
       end do
       end do
@@ -577,5 +618,27 @@ contains
     end do
 
   end function Gaussian_histogram
+
+  subroutine calc_stat(data,mean,sd)
+
+    real(dp), dimension(:), intent(in)  :: data
+    real(dp),               intent(out) :: mean,sd
+
+    integer :: i
+
+    mean = 0._dp
+    sd = 0._dp
+
+    do i=1,size(data)
+       mean = mean + data(i)
+    enddo
+    mean = mean / size(data)
+
+    do i=1,size(data)
+       sd = sd + (data(i)-mean)**2.0_dp
+    enddo
+    sd = sqrt(sd/size(data))
+
+  end subroutine calc_stat
 
 end program density_1d
