fa17da1d9b79d97cdefd9be2532b9646f077821d
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-08-07 10:09:19 +0000

    More informative assertArrayAlmostEqual() routine
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5570 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/quippytest.py
+++ Tools/quippy/tests/quippytest.py
@@ -1,8 +1,13 @@
 import unittest
-from numpy import all
+from numpy import all, unravel_index
+from quippy import frange
 
 class QuippyTestCase(unittest.TestCase):
    
    def assertArrayAlmostEqual(self, a, b, tol=1e-8):
-      self.assert_(abs(a - b).max() < tol)
+      self.assertEqual(a.shape, b.shape)
+      absdiff = abs(a-b)
+      if absdiff.max() > tol:
+         loc = [x+1 for x in unravel_index(absdiff.argmax()-1, absdiff.shape) ]
+         self.fail('Maximum abs difference between array elements is %f at location %r' % (absdiff.max(), loc))
       
