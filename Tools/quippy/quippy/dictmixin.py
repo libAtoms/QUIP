cecaa3d42e43c60c653f3b1d43b5708e1f7061fa
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-08-27 16:00:57 +0000

    Moved paramreader functionality into ParamReaderMixin class, so that fortran Dictionary can use it too
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5623 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy/dictmixin.py
+++ Tools/quippy/quippy/dictmixin.py
@@ -68,3 +68,107 @@ class DictMixin:
 def MakeFullDict(tgt):
     'Extends the dictionary interface for existing classes'
     tgt.__bases__ = tuple(list(tgt.__bases__) + [DictMixin])
+
+
+from farray import *
+import string, re
+
+class ParamReaderMixin:
+    """Mixin which adds parse(), read(), write() and asstring() to a dictionary"""
+
+    def __copy__(self):
+        return self.copy()
+         
+    def parse(self, s):
+        key_quoted_value = re.compile(r'([A-Za-z_]+[A-Za-z0-9_]*)\s*=\s*["\{\}]([^"\{\}]+)["\{\}]\s*')
+        key_value = re.compile(r'([A-Za-z_]+[A-Za-z0-9_]*)\s*=\s*([-0-9A-Za-z_.:\[\]()]+)\s*')
+
+        s = s.strip()
+
+        while 1:
+           # Match quoted string first, then fall through to plain key=value
+           m = key_quoted_value.match(s)
+           if m is None:
+              m = key_value.match(s)
+              if m is not None:
+                 s = key_value.sub('', s, 1)
+           else:
+              s = key_quoted_value.sub('', s, 1)
+
+           if m is None: break # No more matches
+
+           key = m.group(1)
+           value = m.group(2)
+
+           # Try to convert to (list of) floats or ints
+           try:
+              numvalue = []
+              for x in string.split(value):
+                 if x.find('.') == -1:
+                    numvalue.append(int(float(x)))
+                 else:
+                    numvalue.append(float(x))
+              if len(numvalue) == 1: 
+                 numvalue = numvalue[0] # Only one number
+              elif len(numvalue) == 3:
+                 numvalue = farray(numvalue) # 3-vector
+              elif len(numvalue) == 9:
+                 # 3x3 matrix, fortran ordering
+                 numvalue = farray(numvalue).reshape((3,3), order='F')
+              else:
+                 raise ValueError
+              value = numvalue
+           except ValueError:
+              pass
+
+           self[key] = value
+
+    def read(self, f):
+        if isinstance(f, str):
+            self.parse(f)
+        elif hasattr(f, 'keys') and hasattr(f, '__getitem__'): 
+            self.update(f)
+        else:
+            try:
+                for line in f:
+                    self.parse(line)
+            except TypeError:
+                raise TypeError("Don't know how to read from object - "+str(f))
+
+
+    def __repr__(self):
+        return "%s('%s')" % (self.__class__.__name__, str(self))
+
+    def __str__(self):
+        return self.asstring()
+
+    def asstring(self, sep=' '):
+        if len(self) == 0: return ''
+
+        type_val_map = {(bool,True): 'T',
+                        (bool,False): 'F',
+                        (numpy.bool_,True): 'T',
+                        (numpy.bool_,False): 'F'}
+        
+        s = ''
+        for key in self.keys():
+            val = self[key]
+
+            if hasattr(val, '__iter__'):
+                val = ' '.join( str(type_val_map.get((type(x),x), x)) for x in farray(val).reshape(val.size, order='F'))
+                val.replace('[', '')
+                val.replace(']', '')
+            else:
+                val = type_val_map.get((type(val),val), val)
+
+            if type(val) == type('') and ' ' in val:
+                s = s + '%s="%s"%s' % (key, val, sep)
+            else:
+                s = s + '%s=%s%s' % (key, str(val), sep)
+
+        return s.strip()
+
+    def write(self, f):
+        f.write(self.asstring(sep='\n'))
+
+
