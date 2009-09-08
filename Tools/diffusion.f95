f42f172f055208eb2b373a47bf2025b6fb5eef19
cv255 <cv255@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-09-08 14:36:43 +0000

    diffusion constant calculating helper program.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5661 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

---
+++ Tools/diffusion.f95
@@ -0,0 +1,117 @@
+!diffusion constant calculating helper program.
+!prints the average square displacement as the function of the frames.
+!The diffusivity can be calculated by fitting 4*D*t to <r^2> (t).
+program diffusion
+
+use libatoms_module
+
+  implicit none
+
+ 
+    type(Atoms)                           :: structure, reference
+    type(Inoutput)                        :: xyzfile, datafile
+    real(dp)                              :: ave_r2, one_r2
+    integer                               :: frame_count, frames_processed
+    integer                               :: i
+    integer                               :: status
+
+    !Input
+    type(Dictionary)                      :: params_in
+    character(FIELD_LENGTH)               :: xyzfilename, datafilename
+    integer                               :: from, to
+    integer                               :: IO_Rate
+    integer                               :: one_atom
+
+    call system_initialise(SILENT)
+    call verbosity_push(NORMAL)
+
+    call initialise(params_in)
+    call param_register(params_in, 'xyzfile', param_mandatory, xyzfilename)
+    call param_register(params_in, 'datafile', param_mandatory, datafilename)
+    call param_register(params_in, 'from', '1', from)
+    call param_register(params_in, 'to', '0', to)
+    call param_register(params_in, 'one_atom', '0', one_atom)
+    call param_register(params_in, 'IO_Rate', '1', IO_Rate)
+    if (.not. param_read_args(params_in, do_check = .true.)) then
+       call print('diffusion xyzfile datafile [from=1] [to=0] [one_atom] [IO_Rate]')
+       call system_abort('could not parse argument line')
+    end if
+    call finalise(params_in)
+
+    if (from<1) call system_abort('from must be a positive intger.')
+    ! Read the input and output filename and open them
+    call initialise(xyzfile,xyzfilename,action=INPUT)
+    call initialise(datafile,datafilename,action=OUTPUT)
+    call print('#average displacement square of all the atoms',file=datafile)
+
+    call print(' Input file: '//trim(xyzfilename))
+    call print('Output file: '//trim(datafilename))
+    call print('From step '//from//' to step '//to)
+    call print('Also check atom '//one_atom)
+    call print('')
+
+    call print('Reading data...')
+
+    call read_xyz(structure, xyzfile, status=status)
+    !reference = structure
+
+    frame_count = 0
+    frames_processed = 0
+
+    do
+     
+       if (status/=0) exit
+  
+       frame_count = frame_count + 1
+  
+       if ((frame_count.gt.to) .and. (to.gt.0)) exit
+  
+       write(mainlog%unit,'(a,a,i0,$)') achar(13),'Frame ',frame_count
+  
+       if (frame_count.eq.from) then
+          call print('Reference structure is at step '//frame_count)
+          reference = structure
+          call map_into_cell(reference)
+       endif
+     
+       if (frame_count.ge.from) then
+          frames_processed = frames_processed + 1
+  
+          call map_into_cell(structure)
+          one_r2 = 0._dp
+          ave_r2 = 0._dp
+          do i = 1, structure%N
+             ave_r2 = ave_r2 + norm2(realpos(reference,i) - realpos(structure,i))
+          enddo
+          ave_r2 = ave_r2 / structure%N
+          if (one_atom.ne.0) then
+             one_r2 = norm2(realpos(reference,one_atom) - realpos(structure,one_atom))
+          endif
+     
+          if (mod(frame_count-1,IO_Rate).eq.0) then
+             if (one_atom.eq.0) then
+                call print(frame_count//' '//ave_r2,file=datafile)
+             else
+                call print(frame_count//' '//ave_r2//' '//one_r2,file=datafile)
+             endif
+          endif
+       endif
+       call read_xyz(structure,xyzfile,status=status)
+     
+    end do
+  
+    call print('')
+    call print('Read '//frame_count//' frames, processed '//frames_processed//' frames.')
+  
+    !Free up memory
+    call finalise(structure)
+    call finalise(reference)
+    call finalise(xyzfile)
+    call finalise(datafile)
+  
+    call print('Finished.')
+
+    call verbosity_pop
+    call system_finalise
+
+end program diffusion
