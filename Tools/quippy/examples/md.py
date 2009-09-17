30f7c869672c4466be1817bd4d91d0ef98345a5c
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-09-17 18:21:29 +0000

    Align example with new tutorial
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5707 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/examples/md.py
+++ Tools/quippy/examples/md.py
@@ -1,56 +1,38 @@
 """
-Simple example of molecular dynamics of 64 atoms of bulk silicon at 300K
-
-(c) James Kermode 2009
+Simple example of molecular dynamics of 64 atoms of bulk silicon at 1000 K
+James Kermode 2009
 """
 
 from quippy import *
 
 # Set up atomic configuation
-s = supercell(diamond(5.44, 14), 2, 2, 2)
-s.set_cutoff(5.0)
-s.calc_connect()
+s = supercell(diamond(5.44, 14), 3, 3, 3)
 
-xml="""
-<SW_params n_types="2" label="PRB_31_plus_H">
-<comment> Stillinger and Weber, Phys. Rev. B  31 p 5262 (1984), extended for other elements </comment>
-<per_type_data type="1" atomic_num="1" />
-<per_type_data type="2" atomic_num="14" />
-<per_pair_data atnum_i="1" atnum_j="1" AA="0.0" BB="0.0"
-      p="0" q="0" a="1.0" sigma="1.0" eps="0.0" />
-<per_pair_data atnum_i="1" atnum_j="14" AA="8.581214" BB="0.0327827"
-      p="4" q="0" a="1.25" sigma="2.537884" eps="2.1672" />
+# Initialise potential from XML string
+pot = Potential('IP SW', """<SW_params n_types="1">
+<comment> Stillinger and Weber, Phys. Rev. B  31 p 5262 (1984)</comment>
+<per_type_data type="1" atomic_num="14" />
+
 <per_pair_data atnum_i="14" atnum_j="14" AA="7.049556277" BB="0.6022245584"
       p="4" q="0" a="1.80" sigma="2.0951" eps="2.1675" />
 
-<!-- triplet terms: atnum_c is the center atom, neighbours j and k -->
-<per_triplet_data atnum_c="1"  atnum_j="1"  atnum_k="1"
-      lambda="21.0" gamma="1.20" eps="2.1675" />
-<per_triplet_data atnum_c="1"  atnum_j="1"  atnum_k="14"
-      lambda="21.0" gamma="1.20" eps="2.1675" />
-<per_triplet_data atnum_c="1"  atnum_j="14" atnum_k="14"
-      lambda="21.0" gamma="1.20" eps="2.1675" />
-
-<per_triplet_data atnum_c="14" atnum_j="1"  atnum_k="1"
-      lambda="21.0" gamma="1.20" eps="2.1675" />
-<per_triplet_data atnum_c="14" atnum_j="1"  atnum_k="14"
-      lambda="21.0" gamma="1.20" eps="2.1675" />
 <per_triplet_data atnum_c="14" atnum_j="14" atnum_k="14"
       lambda="21.0" gamma="1.20" eps="2.1675" />
 </SW_params>
-"""
+""")
 
-# Initialise potential from XML string
-pot = Potential('IP SW', xml)
+s.set_cutoff(pot.cutoff()+2.0)
+s.calc_connect()
 
-# Set up dynamical system at 300K
+# Set up dynamical system at 1000K
 ds = DynamicalSystem(s)
-ds.rescale_velo(300.0)
+ds.rescale_velo(1000.0)
 ds.zero_momentum()
 
-traj = AtomsList(ds.run(pot, dt=1.0, n_steps=10, save_interval=1))
+outf = CInOutput('si-1000.xyz', OUTPUT)
+traj = AtomsList(ds.run(pot, dt=1.0, n_steps=1000, save_interval=10, out=outf))
 traj.loadall() # Run the dynamics
 traj.show() # Display in AtomEye
+outf.close()
 raw_input('Press ENTER to terminate')
-
 
