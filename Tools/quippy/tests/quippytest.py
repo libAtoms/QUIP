2ae26a59c4b896c6012bf859dfcba775bc31003d
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-10-02 13:47:59 +0000

    Reduce tolerance to 1e-7
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5769 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/quippytest.py
+++ Tools/quippy/tests/quippytest.py
@@ -4,10 +4,10 @@ from quippy import frange
 
 class QuippyTestCase(unittest.TestCase):
    
-   def assertArrayAlmostEqual(self, a, b, tol=1e-8):
+   def assertArrayAlmostEqual(self, a, b, tol=1e-7):
       self.assertEqual(a.shape, b.shape)
       absdiff = abs(a-b)
       if absdiff.max() > tol:
          loc = [x+1 for x in unravel_index(absdiff.argmax()-1, absdiff.shape) ]
-         self.fail('Maximum abs difference between array elements is %f at location %r' % (absdiff.max(), loc))
+         self.fail('Maximum abs difference between array elements is %e at location %r' % (absdiff.max(), loc))
       
