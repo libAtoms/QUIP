e48202c788f1bec550f1d1f6cf8b74eb4a39a420
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-11-16 18:08:54 +0000

    Set library_option for "intel" as well as "intelem" compiler.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5913 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/patch_f2py.py
+++ Tools/quippy/patch_f2py.py
@@ -119,3 +119,6 @@ if not already_patched:
 
    import numpy.distutils.fcompiler.intel
    numpy.distutils.fcompiler.intel.IntelEM64TFCompiler.library_option = library_option
+   numpy.distutils.fcompiler.intel.IntelFCompiler.library_option = library_option
+
+   
