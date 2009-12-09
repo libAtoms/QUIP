52bea3a15fa80b5ccb10c137d138f743a277ebb4
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-12-09 11:45:24 +0000

    Extended elastic_fields() routine for non-cubic systems. Clarified nomenclature for shear components of strain: we now use compressed Voigt notation throughout, with correct factors of two. Added test-case for cubic Si in test_elastic.py
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5981 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/test_elastic.py
+++ Tools/quippy/tests/test_elastic.py
@@ -215,5 +215,116 @@ if hasattr(quippy, 'Potential') and got_scipy:
 
          self.assertArrayAlmostEqual(C, self.C_ref_relaxed, tol=0.2)
 
+
+   class TestElasticFieldsCubic(QuippyTestCase):
+
+      def setUp(self):
+
+         # Use unrelaxed elastic constants since we're not going to minimise cell after each test deformation
+         self.C = farray([[ 151.4276439 ,   76.57244456,   76.57244456,    0.        ,    0.        ,    0.        ],
+                          [  76.57244456,  151.4276439 ,   76.57244456,    0.        ,    0.        ,    0.        ],
+                          [  76.57244456,   76.57244456,  151.4276439 ,    0.        ,    0.        ,    0.        ],
+                          [   0.        ,    0.        ,    0.        ,  109.85498798,    0.        ,    0.        ],
+                          [   0.        ,    0.        ,    0.        ,    0.        ,  109.85498798,    0.        ],
+                          [   0.        ,    0.        ,    0.        ,    0.        ,   0.        ,  109.85498798]])
+         
+         # Relaxed lattice constant
+         self.a = 5.43094977
+
+         self.at0 = diamond(self.a, 14)
+         self.eps = 1e-3
+
+         self.xml="""
+         <SW_params n_types="2" label="PRB_31_plus_H">
+         <comment> Stillinger and Weber, Phys. Rev. B  31 p 5262 (1984), extended for other elements </comment>
+         <per_type_data type="1" atomic_num="1" />
+         <per_type_data type="2" atomic_num="14" />
+         <per_pair_data atnum_i="1" atnum_j="1" AA="0.0" BB="0.0"
+               p="0" q="0" a="1.0" sigma="1.0" eps="0.0" />
+         <per_pair_data atnum_i="1" atnum_j="14" AA="8.581214" BB="0.0327827"
+               p="4" q="0" a="1.25" sigma="2.537884" eps="2.1672" />
+         <per_pair_data atnum_i="14" atnum_j="14" AA="7.049556277" BB="0.6022245584"
+               p="4" q="0" a="1.80" sigma="2.0951" eps="2.1675" />
+
+         <!-- triplet terms: atnum_c is the center atom, neighbours j and k -->
+         <per_triplet_data atnum_c="1"  atnum_j="1"  atnum_k="1"
+               lambda="21.0" gamma="1.20" eps="2.1675" />
+         <per_triplet_data atnum_c="1"  atnum_j="1"  atnum_k="14"
+               lambda="21.0" gamma="1.20" eps="2.1675" />
+         <per_triplet_data atnum_c="1"  atnum_j="14" atnum_k="14"
+               lambda="21.0" gamma="1.20" eps="2.1675" />
+
+         <per_triplet_data atnum_c="14" atnum_j="1"  atnum_k="1"
+               lambda="21.0" gamma="1.20" eps="2.1675" />
+         <per_triplet_data atnum_c="14" atnum_j="1"  atnum_k="14"
+               lambda="21.0" gamma="1.20" eps="2.1675" />
+         <per_triplet_data atnum_c="14" atnum_j="14" atnum_k="14"
+               lambda="21.0" gamma="1.20" eps="2.1675" />
+         </SW_params>
+         """
+
+         self.pot = Potential('IP SW', self.xml)
+         verbosity_push(SILENT)
+
+      def tearDown(self):
+         verbosity_pop()
+
+      def check_strain(self, pattern):
+
+         # Apply desired strain
+         pattern = farray(pattern)
+         applied_strain = numpy.where(pattern == 1, self.eps, 0.0)
+         strain_m = strain_matrix(applied_strain)
+         at = transform(self.at0, strain_m)
+
+         # Compute virial stress for comparison
+         at.calc_connect()
+         self.pot.calc(at, calc_virial=True)
+         virial_stress = stress_vector(-at.virial*GPA/at.cell_volume())
+
+         elastic_fields(at, a=self.a, cij=self.C)
+
+         for i in frange(at.n):
+
+            # Check computed strain matches applied strain
+            strain_i = farray([at.s_xx_sub1[i], at.s_yy_sub1[i], at.s_zz_sub1[i], at.s_yz[i], at.s_xz[i], at.s_xy[i]])
+            self.assertArrayAlmostEqual(strain_i, applied_strain)
+
+            # Check stress matches dot(C, applied_strain)
+            stress_i = farray([at.sig_xx[i], at.sig_yy[i], at.sig_zz[i], at.sig_yz[i], at.sig_xz[i], at.sig_xy[i]])
+
+            self.assertArrayAlmostEqual(stress_i, dot(self.C,applied_strain))
+
+            # Check calculated stress matches computed virial stress
+            self.assertArrayAlmostEqual(stress_i, virial_stress, tol=self.eps)
+
+
+      def test1(self):
+         self.check_strain([1,0,0,0,0,0])
+
+      def test2(self):
+         self.check_strain([0,1,0,0,0,0])
+
+      def test3(self):
+         self.check_strain([0,0,1,0,0,0])
+
+      def test4(self):
+         self.check_strain([0,0,0,1,0,0])
+
+      def test5(self):
+         self.check_strain([0,0,0,0,1,0])
+
+      def test6(self):
+         self.check_strain([0,0,0,0,0,1])
+
+      def test14(self):
+         self.check_strain([1,0,0,1,0,0])
+
+      def test15(self):
+         self.check_strain([1,0,0,0,1,0])
+
+      def test16(self):
+         self.check_strain([1,0,0,0,0,1])
+
 if __name__ == '__main__':
    unittest.main()
