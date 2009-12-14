93e506e10c5d78404317adc2ed242eeea39ae7c5
cv255 <cv255@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-12-14 15:33:27 +0000

    Check QM list change. Added have_silica. Mass weighted momentum conservation.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@5995 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Core/cp2k_filepot_old.f95
+++ QUIP_Core/cp2k_filepot_old.f95
@@ -26,7 +26,7 @@ program cp2k_filepot
     real(dp)                      :: energy
     real(dp), allocatable         :: f0(:,:)
     real(dp), pointer             :: forces_p(:,:)
-    type(Inoutput)                :: xyz
+    type(InOutput)                :: xyz
  
     !Input
     type(Dictionary)              :: params_in
@@ -40,7 +40,13 @@ program cp2k_filepot
     character(len=FIELD_LENGTH)   :: basis_set_file, potential_file, dft_file, global_file, cell_file
   logical                               :: clean_up_files
 !  logical                     :: Delete_Metal_Connections
-
+    type(Table) :: intrares_impropers
+logical :: have_silica_potential
+logical :: QM_list_changed
+logical :: dummy
+integer, pointer :: old_cluster_mark_p(:), cluster_mark_p(:)
+integer :: i
+  real(dp),dimension(3)          :: old_QM_cell
 !    call system_initialise(verbosity=SILENT,enable_timing=.true.)
     call system_initialise(verbosity=NORMAL,enable_timing=.true.)
     call system_timer('program')
@@ -63,6 +69,7 @@ program cp2k_filepot
     call param_register(params_in, 'global_file', '', global_file)
     call param_register(params_in, 'cell_file', '', cell_file)
     call param_register(params_in, 'clean_up_files', 'T', clean_up_files)
+    call param_register(params_in, 'have_silica_potential', 'F', have_silica_potential) !if yes, use 2.8A SILICA_CUTOFF for the connectivities
 !    call param_register(params_in, 'Delete_Metal_Connections', 'T', Delete_Metal_Connections)
 
     if (.not.param_read_args(params_in, do_check=.true.,ignore_unknown=.true.,task='cp2k_filepot args_str')) then
@@ -84,6 +91,7 @@ program cp2k_filepot
     call print('  dft_file '//trim(dft_file))
     call print('  global_file '//trim(global_file))
     call print('  cell_file '//trim(cell_file))
+    call print('  have_silica_potential '//have_silica_potential)
     call print('---------------------------------------')
     call print('')
 
@@ -92,20 +100,41 @@ program cp2k_filepot
     call read_xyz(my_atoms,coord_file)
 
     !create connectivity & topology
-#ifdef HAVE_DANNY
-    call set_cutoff(my_atoms,0._dp)
-    call calc_connect_danny(my_atoms,cut_3body=2.8_dp)
-#else
-    call set_cutoff(my_atoms,0._dp)
+    if (have_silica_potential) then
+       call set_cutoff(my_atoms,SILICA_2body_CUTOFF)
+    else
+       call set_cutoff(my_atoms,0._dp)
+    endif
     call calc_connect(my_atoms)
-#endif
 !    if (Delete_Metal_Connections) call delete_metal_connects(my_atoms)
     call map_into_cell(my_atoms)
     call calc_dists(my_atoms)
     if (trim(Run_Type).ne.'QS') then
-       call create_CHARMM(my_atoms,do_CHARMM=.true.)
+       call create_CHARMM(my_atoms,do_CHARMM=.true.,intrares_impropers=intrares_impropers)
     endif
 
+if (trim(Run_Type).eq.'QMMM_CORE') call system_abort('not yet tested.')
+if ((trim(Run_Type).eq.'QMMM_EXTENDED').or.&
+    (trim(Run_Type).eq.'QMMM_CORE')) then
+   if (.not.has_property(my_atoms, 'old_cluster_mark')) call system_abort('no old_cluster_mark')
+   if (.not.has_property(my_atoms, 'cluster_mark')) call system_abort('no cluster_mark')
+   
+   dummy = assign_pointer(my_atoms, 'old_cluster_mark', old_cluster_mark_p)
+   dummy = assign_pointer(my_atoms, 'cluster_mark', cluster_mark_p)
+
+QM_list_changed = .false.
+   do i=1,my_atoms%N
+      if (old_cluster_mark_p(i).ne.cluster_mark_p(i)) then
+         if (any((/old_cluster_mark_p(i),cluster_mark_p(i)/).eq.HYBRID_NO_MARK) .or. &
+             any((/old_cluster_mark_p(i),cluster_mark_p(i)/).eq.HYBRID_TERM_MARK) ) then
+            QM_list_changed = .true.
+         endif
+      endif
+   enddo
+   call set_value(my_atoms%params,'QM_list_changed',QM_list_changed)
+call print('set_value QM_list_changed '//QM_list_changed)
+
+endif
     !generate args_str for go_cp2k
     write (args_str,'(a)') 'Run_Type='//trim(Run_Type)//' PSF_Print='//trim(Print_PSF)
     if (trim(cp2k_program).ne.'') args_str = trim(args_str)//' cp2k_program='//trim(cp2k_program)
@@ -120,14 +149,23 @@ program cp2k_filepot
     else
        args_str = trim(args_str)//' clean_up_files=F'
     endif
+    if (have_silica_potential) then
+       args_str = trim(args_str)//' have_silica_potential=T'
+    else
+       args_str = trim(args_str)//' have_silica_potential=F'
+    endif
 
     call print('FILEPOT |'//args_str,verbosity=SILENT)
 
     !call CP2K
     allocate(f0(3,my_atoms%N))
-    call go_cp2k(my_atoms=my_atoms, forces=f0, energy=energy, args_str=args_str)
+    call go_cp2k(my_atoms=my_atoms, forces=f0, energy=energy, args_str=args_str,intrares_impropers=intrares_impropers)
     !momentum conservation
-    call sum0(f0)
+    f0 = sum0(f0, my_atoms)
+
+if ((trim(Run_Type).eq.'QMMM_EXTENDED')) then
+    if (.not.get_value(my_atoms%params,'QM_cell',old_QM_cell)) call system_abort('Something has gone wrong. No QM_cell property found.')
+endif
 
     !write energy and forces
     call set_value(my_atoms%params,'energy',energy)
@@ -150,40 +188,33 @@ program cp2k_filepot
 
 contains
 
-  !momentum conservation
-  !   weighing function: 1 (simply subtract sumF/n)
-  !?!   weighing function: m (keeping same acceleration on the atoms)
-  subroutine sum0(force)
+! momentum conservation over all atoms, mass weighted
+  function sum0(force,at) result(force0)
 
-    real(dp), dimension(:,:), intent(inout) :: force
-    integer   :: i
-    real(dp)  :: sumF(3)
+    real(dp), dimension(:,:), intent(in) :: force
+    type(Atoms),              intent(in) :: at
+    real(dp) :: force0(size(force,1),size(force,2))
+    integer :: i
+    real(dp) :: sumF(3), sum_weight
 
-    do i = 1, size(force,2)
-       sumF(1) = sum(force(1,1:size(force,2)))
-       sumF(2) = sum(force(2,1:size(force,2)))
-       sumF(3) = sum(force(3,1:size(force,2)))
-    enddo
+    sumF = sum(force,2)
+    call print('Sum of the forces is '//sumF(1:3))
 
-    if ((sumF(1).feq.0.0_dp).and.(sumF(2).feq.0.0_dp).and.(sumF(3).feq.0.0_dp)) then
-       call print('Sum of the forces are zero.')
-       return
+    if ((sumF(1) .feq. 0.0_dp) .and.  (sumF(2) .feq. 0.0_dp) .and.  (sumF(3) .feq. 0.0_dp)) then
+       call print('Sum of the forces is zero.')
+       force0 = force
+    else
+      !F_corr weighted by element mass
+      sum_weight = sum(ElementMass(at%Z(1:at%N)))
+
+      force0(1,:) = force(1,:) - sumF(1) * ElementMass(at%Z(:)) / sum_weight
+      force0(2,:) = force(2,:) - sumF(2) * ElementMass(at%Z(:)) / sum_weight
+      force0(3,:) = force(3,:) - sumF(3) * ElementMass(at%Z(:)) / sum_weight
     endif
 
-    call print('Sum of the forces was '//sumF(1:3))
-    sumF = sumF / size(force,2)
-
-    do i = 1, size(force,2)
-       force(1:3,i) = force(1:3,i) - sumF(1:3)
-    enddo
-
-    do i = 1, size(force,2)
-       sumF(1) = sum(force(1,1:size(force,2)))
-       sumF(2) = sum(force(2,1:size(force,2)))
-       sumF(3) = sum(force(3,1:size(force,2)))
-    enddo
+    sumF = sum(force0,2)
     call print('Sum of the forces after mom.cons.: '//sumF(1:3))
 
-  end subroutine sum0
+  end function sum0
 
 end program cp2k_filepot
