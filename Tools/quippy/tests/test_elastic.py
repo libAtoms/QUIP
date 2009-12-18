29e3947d151b3db2dd28626924695ff8735cb3b2
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2009-12-18 18:25:59 +0000

    Added tests of elastic_fields() for relaxed C_ij matrix
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6031 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/test_elastic.py
+++ Tools/quippy/tests/test_elastic.py
@@ -220,7 +220,6 @@ if hasattr(quippy, 'Potential') and got_scipy:
 
       def setUp(self):
 
-         # Use unrelaxed elastic constants since we're not going to minimise cell after each test deformation
          self.C = farray([[ 151.4276439 ,   76.57244456,   76.57244456,    0.        ,    0.        ,    0.        ],
                           [  76.57244456,  151.4276439 ,   76.57244456,    0.        ,    0.        ,    0.        ],
                           [  76.57244456,   76.57244456,  151.4276439 ,    0.        ,    0.        ,    0.        ],
@@ -228,6 +227,14 @@ if hasattr(quippy, 'Potential') and got_scipy:
                           [   0.        ,    0.        ,    0.        ,    0.        ,  109.85498798,    0.        ],
                           [   0.        ,    0.        ,    0.        ,    0.        ,   0.        ,  109.85498798]])
          
+         self.C_relaxed = farray([[ 151.28712587,   76.5394162 ,   76.5394162 ,    0.        ,    0.        ,    0.        ],
+                                  [  76.5394162 ,  151.28712587,   76.5394162 ,    0.        ,    0.        ,    0.        ],
+                                  [  76.5394162 ,   76.5394162 ,  151.28712587,    0.        ,    0.        ,    0.        ],
+                                  [   0.        ,    0.        ,    0.        ,   56.32421772,    0.        ,    0.        ],
+                                  [   0.        ,    0.        ,    0.        ,    0.        ,   56.32421772,    0.        ],
+                                  [   0.        ,    0.        ,    0.        ,    0.        ,    0.        ,   56.32421772]])
+
+
          # Relaxed lattice constant
          self.a = 5.43094977
 
@@ -264,12 +271,14 @@ if hasattr(quippy, 'Potential') and got_scipy:
          """
 
          self.pot = Potential('IP SW', self.xml)
-         verbosity_push(SILENT)
+         self.metapot = MetaPotential('Simple', self.pot)
+         #verbosity_push(SILENT)
 
       def tearDown(self):
-         verbosity_pop()
+         #verbosity_pop()
+         pass
 
-      def check_strain(self, pattern):
+      def check_strain(self, pattern, C, relax=False):
 
          # Apply desired strain
          pattern = farray(pattern)
@@ -279,10 +288,15 @@ if hasattr(quippy, 'Potential') and got_scipy:
 
          # Compute virial stress for comparison
          at.calc_connect()
+
+         if relax:
+            # Minimise internal degrees of freedom
+            self.metapot.minim(at, 'cg', 1e-6, 100, do_pos=True, do_lat=False)
+         
          self.pot.calc(at, calc_virial=True)
          virial_stress = stress_vector(-at.virial*GPA/at.cell_volume())
 
-         elastic_fields(at, a=self.a, cij=self.C)
+         elastic_fields(at, a=self.a, cij=C)
 
          for i in frange(at.n):
 
@@ -292,39 +306,70 @@ if hasattr(quippy, 'Potential') and got_scipy:
 
             # Check stress matches dot(C, applied_strain)
             stress_i = farray([at.sig_xx[i], at.sig_yy[i], at.sig_zz[i], at.sig_yz[i], at.sig_xz[i], at.sig_xy[i]])
-
-            self.assertArrayAlmostEqual(stress_i, dot(self.C,applied_strain))
+            self.assertArrayAlmostEqual(stress_i, dot(C, applied_strain))
 
             # Check calculated stress matches computed virial stress
             self.assertArrayAlmostEqual(stress_i, virial_stress, tol=self.eps)
 
+            print i, strain_i, stress_i
+
 
       def test1(self):
-         self.check_strain([1,0,0,0,0,0])
+         verbosity_push(VERBOSE)
+         self.check_strain([1,0,0,0,0,0], self.C, False)
+         verbosity_pop()
 
       def test2(self):
-         self.check_strain([0,1,0,0,0,0])
+         self.check_strain([0,1,0,0,0,0], self.C, False)
 
       def test3(self):
-         self.check_strain([0,0,1,0,0,0])
+         self.check_strain([0,0,1,0,0,0], self.C, False)
 
       def test4(self):
-         self.check_strain([0,0,0,1,0,0])
+         self.check_strain([0,0,0,1,0,0], self.C, False)
 
       def test5(self):
-         self.check_strain([0,0,0,0,1,0])
+         self.check_strain([0,0,0,0,1,0], self.C, False)
 
       def test6(self):
-         self.check_strain([0,0,0,0,0,1])
+         self.check_strain([0,0,0,0,0,1], self.C, False)
 
       def test14(self):
-         self.check_strain([1,0,0,1,0,0])
+         self.check_strain([1,0,0,1,0,0], self.C, False)
 
       def test15(self):
-         self.check_strain([1,0,0,0,1,0])
+         self.check_strain([1,0,0,0,1,0], self.C, False)
 
       def test16(self):
-         self.check_strain([1,0,0,0,0,1])
+         self.check_strain([1,0,0,0,0,1], self.C, False)
+
+      def test1_relaxed(self):
+         self.check_strain([1,0,0,0,0,0], self.C_relaxed, True)
+
+      def test2_relaxed(self):
+         self.check_strain([0,1,0,0,0,0], self.C_relaxed, True)
+
+      def test3_relaxed(self):
+         self.check_strain([0,0,1,0,0,0], self.C_relaxed, True)
+
+      def test4_relaxed(self):
+         self.check_strain([0,0,0,1,0,0], self.C_relaxed, True)
+
+      def test5_relaxed(self):
+         self.check_strain([0,0,0,0,1,0], self.C_relaxed, True)
+
+      def test6_relaxed(self):
+         self.check_strain([0,0,0,0,0,1], self.C_relaxed, True)
+
+      def test14_relaxed(self):
+         self.check_strain([1,0,0,1,0,0], self.C_relaxed, True)
+
+      def test15_relaxed(self):
+         self.check_strain([1,0,0,0,1,0], self.C_relaxed, True)
+
+      def test16_relaxed(self):
+         self.check_strain([1,0,0,0,0,1], self.C_relaxed, True)
+
 
 if __name__ == '__main__':
    unittest.main()
