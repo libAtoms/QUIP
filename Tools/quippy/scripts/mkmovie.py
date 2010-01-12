8533b81de77a1a557915a4d4b3700a775ec24214
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-01-12 14:50:12 +0000

    Various enhancements: --skip-bad-times, --skip-time-duplicates, --merge, --merge-properties, --keep-images, --property, --arrows and --exec-code.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6076 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/scripts/mkmovie.py
+++ Tools/quippy/scripts/mkmovie.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 
 from quippy import *
-import sys, optparse, shutil
+import sys, optparse, shutil, os
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
+from quippy.progbar import ProgressBar
+
+def skip_bad_times(seq, fill_value=9969209968386869046778552952102584320.0):
+   """Skip frames with no Time param or with Time == fill_value."""
+   
+   for at in seq:
+      if not 'Time' in at.params or abs(at.params['Time'] - fill_value) <= abs(numpy.finfo(double).eps*fill_value): continue 
+      yield at
+
+
+def skip_time_duplicates(seq, initial_time=-1.0):
+   """Skip non-monotonically increasing frames in the Atoms sequence `seq`."""
+   
+   cur_time = initial_time
+   for at in seq:
+      if at.params['Time'] > cur_time:
+         yield at
+         cur_time = at.params['Time']
 
 p = optparse.OptionParser(usage='%prog [options] <input file>...')
 
 p.add_option('-r', '--range', action='store', help='Range of frames to include in output movie')
 p.add_option('-o', '--outfile', action='store', help='Movie output file')
 p.add_option('-n', '--nowindow', action='store_true', help='Disable AtomEye viewer window', default=False)
-p.add_option('-e', '--encoder', action='store', help='Movie encoder command', default='ffmpeg -i %s -r 25 -b 30M %s')
-p.add_option('-p', '--player', action='store', help='Movie player command', default='mplayer %s')
+p.add_option('-E', '--encoder', action='store', help='Movie encoder command', default='ffmpeg -i %s -r 25 -b 30M %s')
+p.add_option('-P', '--player', action='store', help='Movie player command', default='mplayer %s')
 p.add_option('-v', '--viewfile', action='store', help='AtomEye command script for initial view')
 p.add_option('-g', '--graph', action='store_true', help='Enable graph plotting')
 p.add_option('-x', '--xdata', action='store', help='Data for x axis of graph')
@@ -20,6 +38,15 @@ p.add_option('-y', '--ydata', action='store', help='Data for y axis of graph')
 p.add_option('--y2data', action='store', help='Data for y2 axis of graph')
 p.add_option('-t', '--text', action='store', help="""Annotate images with text. Argument should be a Python
 expression which evaluates to a string, e.g. '"G = %f" % at.G'""")
+p.add_option('-d', '--skip-time-duplicates', action="store_true", help="Skip duplicate frames (those with matching 'Time' params)")
+p.add_option('-b', '--skip-bad-times', action='store_true', help="Skip frames with no 'Time' param or with where Time equals NetCDF fill value")
+p.add_option('-m', '--merge', action='store', help="""Merge two input files. An auxilary input file name should be given.""")
+p.add_option('-M', '--merge-properties', action='store', help="""List of properties to overwrite from MERGE file. Default is all properties.""")
+p.add_option('-k', '--keep-images', action='store_true', help="""Do not remove frame images after movie has been made.""")
+p.add_option('-p', '--property', action='store', help="""Property to use to colour atoms (default none)""")
+p.add_option('-a', '--arrows', action='store', help="""Property to use to draw arrows (default none)""")
+p.add_option('-e', '--exec_code', action='store', help="""Python code to execute on each frame before writing it to output file. Atoms object is
+available as `at`.""")
 
 opt, args = p.parse_args()
 
@@ -27,8 +54,12 @@ if len(args) == 0:
    p.error('No input files specified')
 
 if opt.range is not None:
-   opt.range = slice(*[{True: lambda n: None, False: int}[x == ''](x) for x in (opt.range.split(':') + ['', '', ''])[:3]])
+   try:
+      opt.range = parse_slice(opt.range)
+   except:
+      p.error('Cannot parse slice "%s" - should be in format [start]:[stop][:step]')
 else:
+   # Default is all frames
    opt.range = slice(1, None, None)
 
 if opt.outfile is None:
@@ -45,9 +76,28 @@ nframes = len(range(*opt.range.indices(nframes)))
 atomseq = itertools.islice(itertools.chain.from_iterable(sources),
                            opt.range.start+1, opt.range.stop, opt.range.step)
 
+if opt.skip_bad_times:
+   atomseq = skip_bad_times(atomseq)
+
+if opt.skip_time_duplicates:
+   atomseq = skip_time_duplicates(atomseq)
+
+if opt.merge is not None:
+   merge_config = Atoms(opt.merge)
+
+   if opt.merge_properties is not None:
+      opt.merge_properties = parse_comma_colon_list(opt.merge_properties)
+   else:
+      opt.merge_properties = merge_config.properties.keys()
+
 a0 = Atoms(args[0])
+
+if opt.merge is not None:
+   for k in opt.merge_properties:
+      a0.add_property(k, getattr(merge_config, k))
+
 view = atomeye.AtomEyeView(nowindow=opt.nowindow)
-view.show(a0)
+view.show(a0, property=opt.property, arrows=opt.arrows)
 
 if opt.viewfile is not None:
    view.run_script(opt.viewfile)
@@ -109,6 +159,56 @@ if opt.text is not None:
 
    postprocess = add_text
 
-view.make_movie(atomseq, opt.outfile, postprocess=postprocess,
-                movieencoder=opt.encoder, movieplayer=opt.player, nframes=nframes, cleanup=True)
+# Make a movie using configurations from `atomseq`. A sequence of JPEG files are
+# w ritten and then encoded using `movieencoder` to form an MPEG - the default
+# command is ``fmpeg -i %s -r 25 -b 30M %s``. Optional arguments `start`, `stop` and `step`
+# can be used to limit the frames used for the movie. A textual progress bar will be
+# drawn unless `progress` is set to false. The intermediate JPEG files are cleared up
+# after the movie is made."""
+
+ndigit = 5
+if nframes is not None: ndigit = int(ceil(log10(nframes)))
+
+basename, ext = os.path.splitext(opt.outfile)
+fmt = '%s%%0%dd.jpg' % (basename, ndigit)
+progress = nframes is not None
+
+imgs = []
+if progress: pb = ProgressBar(0,nframes,80,showValue=True)
+try:
+   if progress: print 'Rendering frames...'
+   for i, at in enumerate(atomseq):
+      filename = fmt % i
+
+      if opt.merge:
+         for k in opt.merge_properties:
+            at.add_property(k, getattr(merge_config, k))
+
+      if opt.exec_code is not None:
+         exec(opt.exec_code)
+     
+      view.show(at, property=opt.property, arrows=opt.arrows)
+      view.capture(filename)
+      view.wait()
+      if postprocess is not None:
+          postprocess(at, i, filename)
+      imgs.append(filename)
+      if progress: pb(i+1)
+   if progress: print
+
+   if opt.encoder is not None:
+      if progress: print 'Encoding movie'
+      os.system(opt.encoder % (fmt, opt.outfile))
+
+   if opt.player is not None:
+      if progress: print 'Playing movie'
+      os.system(opt.player % opt.outfile)
+
+finally:
+   if not opt.keep_images:
+      view.wait()
+      for img in imgs:
+         if os.path.exists(img): os.remove(img)
+         if os.path.exists(img+'.cmap.eps'): os.remove(img+'.cmap.eps')
+
 
