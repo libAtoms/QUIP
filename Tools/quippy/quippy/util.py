eb0743f438312171c5c694d1fd72e2732c07f23c
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-01-12 14:48:06 +0000

    Added parse_slice() and parse_comma_colon_list()
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6074 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy/util.py
+++ Tools/quippy/quippy/util.py
@@ -5,6 +5,25 @@ def args_str(D):
    from dictmixin import PuPyDictionary
    return str(PuPyDictionary(D))
 
+def parse_slice(S):
+   """Parse string containing slice in form [start]:[stop]:[range] and return slice instance."""
+
+   class SliceParser(object):
+      def __getitem__(self, idx):
+         return idx
+
+   return eval('SliceParser()[%s]' % S)
+
+def parse_comma_colon_list(L):
+   """Parse a comma or colon seperated string into a list, converting each entry to lower-case."""
+   if ':' in L:
+      L = L.split(':')
+   elif ',' in L:
+      L = L.split(',')
+   else:
+      L = [L]
+
+   return [k.lower() for k in L]
 
 
 try:
