a660751e021d02641180fc630a96110df5b238cd
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-02-17 14:22:20 +0000

    Use unittest.main() so that command line is parsed.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6174 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/run_all.py
+++ Tools/quippy/tests/run_all.py
@@ -1,9 +1,12 @@
-import unittest, os.path, glob
+import sys, unittest, os.path, glob
 
 
 test_files = glob.glob(os.path.join(os.path.dirname(__file__), 'test*.py'))
 test_mods = [ os.path.splitext(os.path.basename(t))[0] for t in test_files ]
 
-suite = unittest.TestLoader().loadTestsFromNames(test_mods)
-unittest.TextTestRunner().run(suite)
+for name in test_mods:
+   __import__(name)
+   globals().update(vars(sys.modules[name]))
+
+unittest.main()
    
