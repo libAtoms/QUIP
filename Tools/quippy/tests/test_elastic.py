883e63d65271ba4d68f497a18fca0008d7b61064
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-02-19 15:57:49 +0000

    Reduce tolerance of comparisons
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6179 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/test_elastic.py
+++ Tools/quippy/tests/test_elastic.py
@@ -104,8 +104,8 @@ if hasattr(quippy, 'Potential') and hasattr(quippy, 'MetaPotential') and got_sci
 
          C, C_err = fit_elastic_constants(stressed_configs, 'cubic', verbose=False, graphics=False)
 
-         self.assertArrayAlmostEqual(C, self.C_ref, tol=1e-4)
-         self.assertArrayAlmostEqual(C_err, self.C_err_ref, tol=1e-4)
+         self.assertArrayAlmostEqual(C, self.C_ref, tol=1e-2)
+         self.assertArrayAlmostEqual(C_err, self.C_err_ref, tol=1e-2)
 
       def testcubic_relaxed(self):
 
@@ -120,8 +120,8 @@ if hasattr(quippy, 'Potential') and hasattr(quippy, 'MetaPotential') and got_sci
 
          C, C_err = fit_elastic_constants(stressed_configs, 'cubic', verbose=False, graphics=False)
 
-         self.assertArrayAlmostEqual(C, self.C_ref_relaxed, tol=1e-4)
-         self.assertArrayAlmostEqual(C_err, self.C_err_ref_relaxed, tol=1e-4)
+         self.assertArrayAlmostEqual(C, self.C_ref_relaxed, tol=1e-2)
+         self.assertArrayAlmostEqual(C_err, self.C_err_ref_relaxed, tol=1e-2)
 
 
       def testorthorhombic_unrelaxed(self):
