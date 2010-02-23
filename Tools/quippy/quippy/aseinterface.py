7dff7a3bcd89d664d5f09bf943ebed8340e97d48
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-02-23 17:05:29 +0000

    Do not require ASE atoms objects to be of exactly the right class. Add CastepCalculator class.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6185 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy/aseinterface.py
+++ Tools/quippy/quippy/aseinterface.py
@@ -2,7 +2,7 @@
 
 def atoms_to_ase(at):
    """Return a copy of `at` as an :class:`ase.Atoms` instance.
-   `at` must be a :class:`quippy.Atoms` instance."""
+   `at` should be a :class:`quippy.Atoms` instance."""
    
    from numpy import array
    from quippy import Atoms as quippy_Atoms, ElementMass
@@ -33,14 +33,11 @@ def atoms_to_ase(at):
                 
 def atoms_from_ase(at):
    """Return a copy of `at` as a :class:`quippy.Atoms` instance.
-   `at` must be an :class:`ase.Atoms` instance.
+   `at` should be an :class:`ase.Atoms` instance, or something compatible.
    """
    
    from quippy import Atoms as quippy_Atoms, ElementMass
    from ase import Atoms as ase_Atoms
-
-   if not isinstance(at, ase_Atoms):
-      raise ValueError('at (%r) is not an instance of "ase.Atoms" class' % at)
 
    qat = quippy_Atoms(n=at.get_number_of_atoms(),
                       lattice=at.get_cell().T)
@@ -63,7 +60,7 @@ def atoms_from_ase(at):
 class QuippyCalculator(object):
 
    def __init__(self, pot):
-      """Construct a PotentialCalculator object from `pot`, which
+      """Construct a QuippyCalculator object from `pot`, which
       should either be an instance of :class:`quippy.Potential` or
       :class:`quippy.MetaPotential`"""
       
@@ -113,6 +110,87 @@ class QuippyCalculator(object):
       self.update(atoms)
       return self.stress
 
+import castep, os
+
+class CastepCalculator(object):
+   def __init__(self, cell=None, param=None, castep_exec=None, stem=None):
+      """Construct a CastepCalculator object from `cell` and
+      `param` templates."""
+      
+      if isinstance(cell, str):
+         self.cell = castep.CastepCell(cell)
+      else:
+         self.cell = cell
+
+      if isinstance(param, str):
+         self.param = castep.CastepParam(param)
+      else:
+         self.param = param
+
+      if stem is None:
+         self.stem = 'castep_calculator'
+      else:
+         self.stem = stem
+
+      self.castep_exec = castep_exec
+      self.quippy_at = None
+      self.n = 0
+
+
+   def update(self, atoms):
+      qat = atoms_from_ase(atoms)
+      if self.quippy_at is None or self.quippy_at != qat:
+         self.calculate(qat)
+
+   def calculate(self, qat):
+      from numpy import array, zeros
+      from quippy import GPA
+      
+      self.quippy_at = qat.copy()
+      self.cell.update_from_atoms(self.quippy_at)
+
+      stem = '%s_%05d' % (self.stem, self.n)
+      castep.run_castep(self.cell, self.param, stem, self.castep_exec, test_mode=True)
+      self.n += 1
+
+      try:
+         print 'Reading from file %s' % (stem+'.castep')
+         result = Atoms(stem+'.castep', atoms_ref=self.quippy_at)
+         print self.quippy_at.pos
+         print result.pos
+         self.energy = float(result.energy)
+         self.forces = array(result.force.T)
+         if hasattr(result, 'virial'):
+            self.stress = -array(result.virial)*GPA/result.cell_volume()
+         else:
+            self.stress = zeros((3,3))
+      except IOError:
+         self.energy = 0.0
+         self.forces = zeros((qat.n,3))
+         self.stress = zeros((3,3))
+
+      
+   def get_potential_energy(self, atoms):
+      self.update(atoms)
+      return self.energy
+
+   def get_forces(self, atoms):
+      self.update(atoms)
+      return self.forces.copy()
+
+   def get_numeric_forces(self, atoms):
+      from numpy import zeros
+      
+      self.update(atoms)
+      df = zeros((3,self.quippy_at.n), order='F')
+      self.pot.calc(self.quippy_at, df=df)
+      return df.T
+
+   def get_stress(self, atoms):
+      self.update(atoms)
+      return self.stress
+   
+
 
 # If the ASE is installed, register AtomsReader from ase.Atoms instances
 # and add a 'to_ase()' method to quippy Atoms and Potential classes
@@ -124,6 +202,7 @@ try:
       yield atoms_from_ase(ase_at)
 
    AtomsReaders[ase_Atoms] = atoms_from_ase_gen
+   AtomsReaders['ase'] = atoms_from_ase_gen
    Atoms.to_ase = atoms_to_ase
    del atoms_from_ase_gen
 
