f6f12b8c45f159e37738d5369139a2e75cb2398b
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-04-22 10:15:52 +0000

    Write Gaussian .cube format for visualisation in VMD or Jmol
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6348 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

---
+++ Tools/quippy/quippy/cube.py
@@ -0,0 +1,47 @@
+from quippy import AtomsWriters, BOHR
+from farray import *
+import sys
+
+class CubeWriter(object):
+
+   def __init__(self, f, comment='', data=None, origin=(0., 0., 0.)):
+      if type(f) == type(''):
+         if f == 'stdout':
+            self.f = sys.stdout
+            self.opened = False
+         else:
+            self.opened = True
+            self.f = open(f,'w')
+      else:
+         self.opened = False
+         self.f = f
+
+      self.comment = comment
+      self.data = data
+      self.origin = origin
+
+   def write(self, at):
+       self.f.write(self.comment.strip()+'\n\n')
+       
+       origin_x, origin_y, origin_z = self.origin
+       self.f.write('%d %f %f %f\n' % (at.n, origin_x, origin_y, origin_z))
+
+       for i in (1,2,3):
+           n = self.data.shape[i-1]
+           voxel_x, voxel_y, voxel_z = at.lattice[:,i]/BOHR/n
+           self.f.write('%d %f %f %f\n' % (n, voxel_x, voxel_y, voxel_z))
+
+       for i in frange(at.n):
+           self.f.write('%d 0.0 %f %f %f\n' % (at.z[i], at.pos[1,i]/BOHR, at.pos[2,i]/BOHR, at.pos[3,i]/BOHR))
+
+       for d in self.data.flat:
+           self.f.write('%f\n' % d)
+
+       
+
+   def close(self):
+       if self.opened:  self.f.close()
+
+
+
+AtomsWriters['cube'] = CubeWriter
