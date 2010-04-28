d0b2afec93f5697236cf4d2da4326f97b9bee34e
gc121 <gc121@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-04-28 22:35:41 +0000

    added loop
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6368 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Programs/randomise_calc.f95
+++ QUIP_Programs/randomise_calc.f95
@@ -12,7 +12,7 @@ implicit none
   real(dp)::e
   real(dp):: lat(3,3), v(3,3)
   real(dp), allocatable::f(:,:)
-  
+  integer :: i
 
   call system_initialise()
   
@@ -20,17 +20,18 @@ implicit none
   !call Initialise(pot, 'FilePot command=./castep_driver.sh')
   call Initialise(pot, 'FilePot command=./castep_driver.py')
 
-  !call read_xyz(at, 'start.xyz')
-  call anatase(at2, 3.782_dp, 9.502_dp, 0.208_dp)
-  call supercell(at, at2, 2,2,1)
-  call randomise(at%pos, 0.2_dp)
-  lat = at%lattice
-  call randomise(lat, 0.5_dp)
-  call set_lattice(at, lat)
-  allocate(f(3,at%N))
-  call calc(pot, at, e=e, f=f, virial=v)
-  call add_property(at, 'f', f)
-  call print_xyz(at, 'output.xyz', properties='pos:f', comment='Energy='//e//' virial={'//reshape(v, (/9/))//'}', append=.true.)
+  call read_xyz(at2, 'start.xyz')
+  do i=1,20
+     at=at2
+     call randomise(at%pos, 0.2_dp)
+     lat = at%lattice
+     call randomise(lat, 0.5_dp)
+     call set_lattice(at, lat)
+     allocate(f(3,at%N))
+     call calc(pot, at, e=e, f=f, virial=v)
+     call add_property(at, 'f', f)
+     call print_xyz(at, 'output.xyz', properties='pos:f', comment='Energy='//e//' virial={'//reshape(v, (/9/))//'}', append=.true.)
+  end do
 
   call system_finalise()
 
