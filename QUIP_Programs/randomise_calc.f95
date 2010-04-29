c2930df4e4a87845b27117a4a3de03677f7e9f01
gc121 <gc121@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-04-29 10:04:11 +0000

    tweak
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6370 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Programs/randomise_calc.f95
+++ QUIP_Programs/randomise_calc.f95
@@ -20,18 +20,19 @@ implicit none
   !call Initialise(pot, 'FilePot command=./castep_driver.sh')
   call Initialise(pot, 'FilePot command=./castep_driver.py')
 
-  call read_xyz(at2, 'start.xyz')
+  call read_xyz(at2, 'FeSb3La.xyz')
+  allocate(f(3,at2%N))
   do i=1,20
      at=at2
      call randomise(at%pos, 0.2_dp)
      lat = at%lattice
      call randomise(lat, 0.5_dp)
      call set_lattice(at, lat)
-     allocate(f(3,at%N))
      call calc(pot, at, e=e, f=f, virial=v)
      call add_property(at, 'f', f)
      call print_xyz(at, 'output.xyz', properties='pos:f', comment='Energy='//e//' virial={'//reshape(v, (/9/))//'}', append=.true.)
   end do
+  deallocate(f)
 
   call system_finalise()
 
