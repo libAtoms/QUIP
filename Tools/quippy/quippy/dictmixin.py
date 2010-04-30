bd29a3e12fdbb4a386a22179c847ad7012646632
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-04-30 17:53:17 +0000

    Added GPL headers to all quippy files.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6393 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy/dictmixin.py
+++ Tools/quippy/quippy/dictmixin.py
@@ -1,9 +1,30 @@
+# HQ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# HQ X
+# HQ X   quippy: Python interface to QUIP atomistic simulation library
+# HQ X
+# HQ X   Copyright James Kermode 2010
+# HQ X
+# HQ X   These portions of the source code are released under the GNU General
+# HQ X   Public License, version 2, http://www.gnu.org/copyleft/gpl.html
+# HQ X
+# HQ X   If you would like to license the source code under different terms,
+# HQ X   please contact James Kermode, james.kermode@gmail.com
+# HQ X
+# HQ X   When using this software, please cite the following reference:
+# HQ X
+# HQ X   http://www.jrkermode.co.uk/quippy
+# HQ X
+# HQ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+# Original DictMixin version from
+# http://code.activestate.com/recipes/117236/
+# Copyright (c) Raymond Hettinger 2002
+# Licensed under the PSF License
+
 class DictMixin:
     '''Mixin defining all dictionary methods for classes that already have
        a minimum dictionary interface including getitem, setitem, delitem,
        and keys 
-
-       from http://code.activestate.com/recipes/117236/
        '''
 
     # first level definitions should be implemented by the sub-class
