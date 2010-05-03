fad0399220481dbf2c22d0eecb664b45b064bf45
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-05-03 21:36:14 +0000

    Add Tools/make_k_mesh.f95 as a wrapper to kpoints_initialise_mesh that writes xml kpoints files
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6397 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

---
+++ Tools/make_k_mesh.f95
@@ -0,0 +1,39 @@
+program make_k_mesh
+use libatoms_module
+use quip_module
+implicit none
+  type(KPoints) :: kp
+  integer :: i
+  type(Dictionary) :: cli_params
+  character(len=FIELD_LENGTH) :: outfile
+  type(inoutput) :: out_io
+  integer :: mesh(3)
+  logical :: monkhorst_pack
+
+  call system_initialise(verbosity=SILENT)
+
+  call initialise(cli_params)
+  call param_register(cli_params, "outfile", "stdout", outfile)
+  call param_register(cli_params, "mesh", "1 1 1", mesh)
+  call param_register(cli_params, "monkhorst_pack", "F", monkhorst_pack)
+  if (.not. param_read_args(cli_params, do_check=.true., ignore_unknown=.false.)) then
+    call print("Usage: make_k_mesh outfile=filename(stdout) mesh='n1 n2 n3'(1 1 1 ) monkhorst_pack=L(F)", ERROR)
+    call system_abort("Failed to parse command line arguments")
+  endif
+
+  if (any(mesh <= 0)) &
+    call system_abort("Invalid mesh " // mesh)
+  call initialise(kp, mesh, monkhorst_pack)
+
+  call initialise(out_io, trim(outfile))
+  call print('<KPoints N="'//kp%N//'" >', file=out_io)
+  do i=1, kp%N
+    call print('<point weight="'//kp%weights(i)//'" > '//kp%k_pts(:,i)//' </point>', file=out_io)
+  end do
+  call print('</KPoints>', file=out_io)
+  call finalise(out_io)
+  call finalise(kp)
+
+  call system_finalise()
+
+end program make_k_mesh
