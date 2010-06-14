0ee2842ed1c0c8b4ecdc4898367e49b215152aea
ab686 <ab686@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-06-14 21:02:34 +0000

    Program to select every n-th frame from a large xyz file
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6596 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

---
+++ GAProgs/comb.f95
@@ -0,0 +1,43 @@
+program comb
+
+  use libatoms_module
+
+  implicit none
+
+  type(atoms) :: at
+  type(cinoutput) :: infile, outfile
+  type(Dictionary) :: params
+
+  integer  :: i, n
+  
+  character(len=FIELD_LENGTH) :: in_file, out_file
+
+  call system_initialise(verbosity=SILENT)
+
+  call initialise(params)
+  call param_register(params, 'in_file', PARAM_MANDATORY, in_file)
+  call param_register(params, 'out_file', 'stdout', out_file)
+  call param_register(params, 'n', '20', n)
+
+  if (.not. param_read_args(params, do_check = .true.)) then
+     call verbosity_push(NORMAL)
+     call print("Usage: comb [in_file=file] [out_file=stdout] [n=20]")
+     call system_abort('Exit: Mandatory argument(s) missing...')
+  endif
+  call finalise(params)
+
+  call initialise(infile,trim(in_file))
+  call initialise(outfile,trim(out_file),action=OUTPUT)
+
+  call query(infile)
+
+  do i = n, infile%n_frame, n
+     call read(infile,at,frame=i-1)
+     call write(at,outfile)
+     call finalise(at)
+  enddo
+
+  call finalise(infile)
+  call finalise(outfile)
+
+end program comb
