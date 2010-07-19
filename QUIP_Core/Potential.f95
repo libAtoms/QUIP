8f1d1deaf1c21750979821d5bb0c96b16be7d5be
gc121 <gc121@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-07-19 21:53:32 +0000

    put in HAVE_TB flag
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6726 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Core/Potential.f95
+++ QUIP_Core/Potential.f95
@@ -55,9 +55,9 @@ module Potential_module
   use QUIP_Common_module
   use MPI_context_module
   use Potential_simple_module
-
+#ifdef HAVE_TB
   use TB_module
-
+#endif
   use adjustablepotential_module, only: adjustable_potential_init, adjustable_potential_optimise, &
        adjustable_potential_force, adjustable_potential_finalise
 !  use adjustablepotential_sw_module, only: adjustable_potential_sw_init, adjustable_potential_sw_optimise, &
