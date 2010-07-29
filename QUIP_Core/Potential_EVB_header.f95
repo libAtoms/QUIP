4a140f4eded61945245c0cd84a54635ded992f3c
cv255 <cv255@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-07-29 14:17:19 +0000

    corrections to EVB potential header.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6848 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Core/Potential_EVB_header.f95
+++ QUIP_Core/Potential_EVB_header.f95
@@ -3,17 +3,22 @@
   type Potential_EVB
      type(MPI_context) :: mpi
 
-     type(Potential), pointer :: pot1 => null() 
-     type(Potential), pointer :: pot2 => null() 
+     type(Potential), pointer :: pot1 => null()    !% The underlying MM potential, pot1 and pot2
 
-     character(FIELD_LENGTH) :: evb_step !% step number containing string to print energies
-     character(FIELD_LENGTH) :: mm_args_str !% Args string to be passed to 'calc' method of EVB%pot1 and EVB%pot2
-     character(FIELD_LENGTH) :: topology_suffix1 !% suffix in topology filename, to be added to mm_args_str of EVB%pot1
-     character(FIELD_LENGTH) :: topology_suffix2 !% suffix in topology filename, to be added to mm_args_str of EVB%pot2
-     integer :: form_bond(2)    !atom pair that is bonded in EVB1 only
-     integer :: break_bond(2)   !atom pair that is bonded in EVB2 only
-!     real(dp) :: energy_offset  !the energy offset of the 2 resonance states
-!     type(Constraint) :: off_diagonal
+     character(FIELD_LENGTH) :: mm_args_str        !% Args string to be passed to 'calc' method of pot1 and pot2
+
+     character(FIELD_LENGTH) :: topology_suffix1   !% Suffix in topology filename, to be added to mm_args_str of pot1
+     character(FIELD_LENGTH) :: topology_suffix2   !% Suffix in topology filename, to be added to mm_args_str of pot2
+
+     integer :: form_bond(2)                       !% Atom pair that is bonded in EVB1 only
+     integer :: break_bond(2)                      !% Atom pair that is bonded in EVB2 only
+
+     !real(dp) :: energy_offset                    !the energy offset of the 2 resonance states
+     real(dp) :: offdiagonal_A12                   !% The offdiagonal pre-exponent factor of EVB Hamiltonian
+     real(dp) :: offdiagonal_mu12                  !% The offdiagonal exponent factor of the EVB Hamiltonian
+
+     logical :: save_forces                        !% Whether to save forces from the 2 MM calculations in the Atoms object (needed for force on E_GAP)
+     logical :: save_energies                      !% Whether to save energies from the 2 MM calculations in the Atoms object (needed for E_GAP)
 
   end type Potential_EVB
 
