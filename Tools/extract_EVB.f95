2ddb3fc69732509c6e1ae1926c0778de7d36769e
cv255 <cv255@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-07-29 14:52:30 +0000

    prints E_GAP from the main, handles restarts.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6850 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/extract_EVB.f95
+++ Tools/extract_EVB.f95
@@ -28,7 +28,7 @@
 ! H0 X
 ! H0 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-!Program to calculate EVB given
+!Program to calculate the EVB E_GAP given
 !   -- an XYZ file with the coordinates,
 !   -- 2 PSF files with the 2 resonance states
 !   -- a form_bond_file containing the pair of atoms that form a bond in PSF1, but not in PSF2
@@ -41,44 +41,52 @@ program extract_EVB
 
   implicit none
 
-  type(Atoms)                   :: at
-  type(CInoutput)               :: coord_file
-  !real(dp),       pointer       :: avgpos_p(:,:)
-  integer                       :: iframe, step_nr
-  real(dp)                      :: time_passed
-  real(dp)                      :: energy1
-  type(Potential)               :: pot
-  type(Potential)               :: evbpot
-  character(len=STRING_LENGTH)  :: args_str
+  type(Atoms)                              :: at
+  type(CInoutput)                          :: coord_file
+  type(InOutput)                           :: out_file
+  integer                                  :: iframe, step_nr
+  real(dp)                                 :: time_passed
+  real(dp)                                 :: energy1, energy2
+  !real(dp), allocatable                    :: f1(:,:)
+  character(len=FIELD_LENGTH), allocatable :: output_lines(:)
+  integer                                  :: lines_used, i
 
-  !Input parameters
-  type(Dictionary)              :: params_in
-  character(len=FIELD_LENGTH)   :: coord_filename
-  character(len=FIELD_LENGTH)   :: PSF_file1, PSF_file2, filepot_program, cp2k_program !, Residue_Library
-  integer                       :: last_frame
-  real(dp)                      :: time_step
-  character(FIELD_LENGTH)       :: mm_args_str
+  type(Potential)                          :: pot
+  type(Potential)                          :: evbpot
+  character(len=STRING_LENGTH)             :: args_str
+  character(FIELD_LENGTH)                  :: mm_args_str
+                                           
+  !Input parameters                        
+  type(Dictionary)                         :: params_in
+  character(len=FIELD_LENGTH)              :: coord_filename, out_filename
+  character(len=FIELD_LENGTH)              :: PSF_file1, PSF_file2, &
+                                              filepot_program, cp2k_program !, Residue_Library
+  integer                                  :: last_frame
+  real(dp)                                 :: time_step
+  logical                                  :: append_output
+  integer                                  :: restart_every
 
 
-!    call system_initialise(verbosity=ANAL,enable_timing=.true.)
-!    call system_initialise(verbosity=VERBOSE,enable_timing=.true.)
     call system_initialise(verbosity=PRINT_NORMAL,enable_timing=.true.)
     call system_timer('program')
 
     !INPUT
       call initialise(params_in)
-      call param_register(params_in, 'coord_file'         , PARAM_MANDATORY, coord_filename      )
-      call param_register(params_in, 'last_frame'         , '0'            , last_frame          )
-      !call param_register(params_in, 'time_step'          , '0.0'          , time_step           )
-      call param_register(params_in, 'PSF_file1'          , PARAM_MANDATORY, PSF_file1           )
-      call param_register(params_in, 'PSF_file2'          , PARAM_MANDATORY, PSF_file2           )
-      !call param_register(params_in, 'Residue_Library'    , PARAM_MANDATORY, Residue_Library     )
-      call param_register(params_in, 'filepot_program'    , PARAM_MANDATORY, filepot_program     )
-      call param_register(params_in, 'cp2k_program'       , PARAM_MANDATORY, cp2k_program        )
+      call param_register(params_in, 'coord_file', PARAM_MANDATORY, coord_filename)
+      call param_register(params_in, 'outfile', PARAM_MANDATORY, out_filename)
+      call param_register(params_in, 'PSF_file1', PARAM_MANDATORY, PSF_file1)
+      call param_register(params_in, 'PSF_file2', PARAM_MANDATORY, PSF_file2)
+      call param_register(params_in, 'filepot_program', PARAM_MANDATORY, filepot_program)
+      call param_register(params_in, 'cp2k_program', PARAM_MANDATORY, cp2k_program)
+      !call param_register(params_in, 'Residue_Library', PARAM_MANDATORY, Residue_Library)
+      call param_register(params_in, 'last_frame', '0', last_frame)       !last frame to process
+      call param_register(params_in, 'time_step', '0.0', time_step)       !if time is not saved in the XYZ
+      call param_register(params_in, 'append_output', 'F', append_output) !useful for restarts
+      call param_register(params_in, 'restart_every', '1', restart_every) !useful if the restarts have an overlap with the previous run.
 
       if (.not. param_read_args(params_in, do_check = .true.)) then
         !call print_usage
-        call print("extract_EVB coord_file PSF_file1 PSF_file2 filepot_program cp2k_program [last_frame]",PRINT_ALWAYS)
+        call print("extract_EVB coord_file outfile PSF_file1 PSF_file2 filepot_program cp2k_program [last_frame] [time_step] [append_output] [restart_every]",PRINT_ALWAYS)
         call system_abort('could not parse argument line')
       end if
 
@@ -87,13 +95,21 @@ program extract_EVB
     !PRINT INPUT PARAMETERS
       call print('Run parameters:')
       call print('  coord_file      '// trim(coord_filename)     )
+      call print('  outfile         '// trim(out_filename)       )
       call print('  PSF_file1       '// trim(PSF_file1)          )
       call print('  PSF_file2       '// trim(PSF_file2)          )
       !call print('  Residue_Library '// trim(Residue_Library)    )
       call print('  cp2k_program    '// trim(cp2k_program)       )
       call print('  filepot_program '// trim(filepot_program)    )
       call print('  last_frame      '// last_frame               )
-      !call print('  time_step       '// time_step                )
+      call print('  time_step       '// time_step                )
+      call print('  restart_every   '// restart_every//' steps'  )
+      if (restart_every/=1) call print('    Prints after every '//restart_every//' steps, synchronised with the restart frequency.')
+      if (append_output) then
+         call print("    Will not overwrite out_file (appending probably restart).")
+      else
+         call print("    Will overwrite out_file!!")
+      endif
       call print('---------------------------------------')
       call print('')
 
@@ -110,12 +126,19 @@ program extract_EVB
     endif
     call print('Last frame to be processed '//last_frame)
 
+    !open output file, append if required
+    if (append_output) then
+       call initialise(out_file, out_filename, action=OUTPUT,append=.true.)
+    else
+       call initialise(out_file, out_filename, action=OUTPUT,append=.false.)
+       call print("# Step Nr.   Time[fs]  Energy_PSF1[eV]  Energy_PSF2[eV]     EVB(E1-E2)", file=out_file)
+    endif
 
     !initialise EVB potential
-!    call initialise(pot,'FilePot command='//trim(filepot_program)//' property_list=pos:avgpos:mol_id:atom_res_number min_cutoff=0.0')
     call initialise(pot,'FilePot command='//trim(filepot_program)//' property_list=pos min_cutoff=0.0')
-    call initialise(evbpot,args_str='EVB=T form_bond={1 2} break_bond={2 6}', &
+    call initialise(evbpot,args_str='EVB=T form_bond={1 2} break_bond={2 6} offdiagonal_A12=0.0 offdiagonal_mu12=0.0 save_energies=T save_forces=F', &
          pot1=pot)
+
     !setup topology files
     call system_command('cp '//trim(PSF_file1)//' quip_cp2k_evb1.psf')
     call system_command('cp '//trim(PSF_file2)//' quip_cp2k_evb2.psf')
@@ -123,18 +146,15 @@ program extract_EVB
 
     step_nr = 0
 
+    !allocate printing buffer
+    allocate(output_lines(restart_every))
+    lines_used = 0
+
     do iframe = 0, last_frame-1
 
        call read(coord_file, at, frame=iframe)
 
        write(mainlog%unit,'(a,a,i0,$)') achar(13),'Frame ',(iframe+1)
-
-       !if (iframe==0) then !check colvar atoms
-       !   if ((.not.input_in_cp2k_units) .and. skip_extra_firsts) cycle !skip very first without force information
-       !endif
-       !if (iframe==1) then
-       !   if ((.not.input_in_cp2k_units) .and. skip_extra_firsts .and. append_output) cycle !skip the overlapping second for the restarts
-       !endif
 
        !get Step_nr and Time
        if (.not.get_value(at%params,"i",step_nr)) then
@@ -144,23 +164,53 @@ program extract_EVB
           time_passed = step_nr * time_step
        endif
 
-       !calculate EVB energy
-          mm_args_str="Run_Type=MM cp2k_program="//trim(cp2k_program)//" clean_up_files=T save_output_files=F have_silica_potential=F centre_cp2k=F PSF_Print=USE_EXISTING_PSF"
-!          mm1_args_str="Run_Type=MM cp2k_program="//trim(cp2k_program)//" clean_up_files=T save_output_files=F have_silica_potential=F centre_cp2k=F PSF_Print=DRIVER_PRINT_AND_SAVE topology_suffix=_evb1"
+       !Calculate EVB energy
 
-          args_str = "evb_step="//step_nr//" mm_args_str={"//trim(mm_args_str)//"} topology_suffix1=_evb1 topology_suffix2=_evb2"
-        !  call calc_connect(at)
-        !  call map_into_cell(at)
-        !  call calc_dists(at)
-        !  call set_value(at%params,'Library',trim(Residue_Library))
-        !  call create_residue_labels_arb_pos(at,do_CHARMM=.true.,pos_field_for_connectivity="pos")
-          !call add_property(at,"avgpos",0._dp,n_cols=3)
-          !if(.not.assign_pointer(at,"avgpos",avgpos_p)) call system_abort("no avgpos")
-          !avgpos_p=at%pos(1:3,1:at%N)
-          call calc(evbpot,at,e=energy1,args_str=trim(args_str))
-   
+       mm_args_str="Run_Type=MM cp2k_program="//trim(cp2k_program)//" clean_up_files=T save_output_files=F have_silica_potential=F centre_cp2k=F PSF_Print=USE_EXISTING_PSF"
+
+       args_str = "evb_step="//step_nr//" mm_args_str={"//trim(mm_args_str)//"} topology_suffix1=_evb1 topology_suffix2=_evb2"
+     !  call set_value(at%params,'Library',trim(Residue_Library))
+
+       call calc(evbpot,at,e=energy1,args_str=trim(args_str))
+       !allocate(f1(1:3,1:at%N))
+       !call calc(evbpot,at,e=energy1,f=f1,args_str=trim(args_str))
+       !deallocate(f1)
+       !call print_xyz(at,'evb_all.xyz',all_properties=.true.)
+
+       if (.not. get_value(at%params,'EVB1_energy',energy1)) then
+          call system_abort('Could not find EVB1_energy')
+       endif
+       if (.not. get_value(at%params,'EVB1_energy',energy2)) then
+          call system_abort('Could not find EVB2_energy')
+       endif
+
+       !Printing
+
+       !if not append_output, we want to print time=0, too
+       if (.not.append_output .and. iframe==0) then
+          write (UNIT=output_lines(1),FMT="(I8,F13.3,3F13.8)") &
+                 step_nr, time_passed, energy1, energy2, (energy1-energy2)
+          call print(output_lines(1),file=out_file)
+          output_lines(1) = ""
           call finalise(at)
+          cycle
+       endif
 
+       !printing into buffer
+       write (UNIT=output_lines(lines_used+1),FMT="(I8,F13.3,3F13.8)") &
+              step_nr, time_passed, energy1, energy2, (energy1-energy2)
+       lines_used = lines_used + 1
+
+       !printing buffer into file
+       if (lines_used == restart_every) then
+          do i=1,lines_used
+             call print(output_lines(i),file=out_file)
+             output_lines(i) = ""
+          enddo
+          deallocate(output_lines)
+          allocate(output_lines(restart_every))
+          lines_used = 0
+       endif
 
        call finalise(at)
 
@@ -173,25 +223,5 @@ program extract_EVB
 
     call system_timer('program')
     call system_finalise
-
-!contains
-!
-!
-!  subroutine print_usage
-!
-!    call print("Usage: extract_cv coord_file force_file colvar_file outfile [restart_every=1] [append_output=F] [last_frame=0] [time_step=0.0]")
-!    call print("   coord_file     XYZ coordinate file")
-!    call print("   velo_file      XYZ file with velocities stored in 'velo' property")
-!    call print("   force_file     XYZ file with forces stored in 'frc' property")
-!    call print("   atomlist_file  file containing the (colvar) atoms whose properties should be printed")
-!    call print("   outfile        output file with positions, velocities and forces on the (colvar) atoms in time.")
-!    call print("   restart_every  optionally stores the output in a buffer and prints synchronised with the restart frequency.")
-!    call print("                     Useful for restarted runs.")
-!    call print("   append_output  optionally append to an existing output file without reprinting the header")
-!    call print("                     Useful for restarted runs.")
-!    call print("   last_frame     optionally processes only the first last_frame frames.  0 means process all frames.")
-!    call print("   time_step      optionally sets the time step between frames.  It is used if 'time' is not stored in the coord_file.")
-!
-!  end subroutine print_usage
 
 end program extract_EVB
