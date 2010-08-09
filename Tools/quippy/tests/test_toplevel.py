7f0eb23f877b785d5898b22f6428ddc1e1eeac8c
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-08-09 14:38:49 +0000

    Added quippy test-cases to run tests of top-level programs.
    Any script matching glob pattern ${QUIP_ROOT}/Tests/test_* will be
    run. Scripts should return a non-zero exit status if the test fails.
    test_md and test_eval have been modified to do this.
    
    Also replaced QUIP_HOME with QUIP_ROOT in these scripts.
    
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@6976 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

---
+++ Tools/quippy/tests/test_toplevel.py
@@ -0,0 +1,42 @@
+# HQ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# HQ X
+# HQ X   quippy: Python interface to QUIP atomistic simulation library
+# HQ X
+# HQ X   Copyright James Kermode 2010
+# HQ X
+# HQ X   These portions of the source code are released under the GNU General
+# HQ X   Public License, version 2, http://www.gnu.org/copyleft/gpl.html
+# HQ X
+# HQ X   If you would like to license the source code under different terms,
+# HQ X   please contact James Kermode, james.kermode@gmail.com
+# HQ X
+# HQ X   When using this software, please cite the following reference:
+# HQ X
+# HQ X   http://www.jrkermode.co.uk/quippy
+# HQ X
+# HQ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+import unittest, glob, os
+from quippytest import *
+
+if 'QUIP_ROOT' in os.environ:
+
+    class Test_TopLevel(QuippyTestCase):
+        pass
+
+
+    def make_test(script):
+        def run_script(self):
+            exitcode = os.system(script)
+            if exitcode != 0:
+                self.fail()
+
+        return run_script
+
+    for script in glob.glob(os.path.join(os.environ['QUIP_ROOT'], 'Tests/test_*')):
+        setattr(Test_TopLevel, os.path.basename(script), make_test(script))
+
+                          
+
+if __name__ == '__main__':
+   unittest.main()
