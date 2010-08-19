7af498240767715b459befc1f9ee9b8e1c0620dd
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-08-19 14:40:44 +0000

    Add mean_vol_spliy.py to quippy scripts
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7080 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

---
+++ Tools/quippy/scripts/mean_vol_split.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from quippy import *
+from numpy import *
+import optparse, sys
+
+p = optparse.OptionParser(usage='%prog file; # splits file by frame, with all output files lattice rescaled isotropically to match the mean volume of the inputs')
+
+opt, args = p.parse_args()
+
+for file in args:
+   v_mean = 0.0
+   al = AtomsReader(file)
+   n=0
+   for at in al:
+      v_mean = v_mean + at.cell_volume()
+      n += 1
+   v_mean = v_mean / real(n)
+   nd = int(log(n)/log(10))+1
+   fmt = ".%%0%dd" % nd
+   al = AtomsReader(file)
+   i=0
+   for at in al:
+      v = at.cell_volume()
+      lat = at.lattice
+      new_lat = lat*pow(v_mean/v,1.0/3.0)
+      at.set_lattice(new_lat, scale_positions=True)
+      dot_pos = file.rindex('.')
+      sys.exit()
+      new_file=file[:dot_pos]+(fmt % i)+"."+file[dot_pos+1:]
+      print new_file
+      at.write(new_file)
+      i += 1
