90547034f15d26ec7f8c1cf16bf524f045f0476d
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-08-19 15:04:03 +0000

    bug in mean_vol_split.py
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7082 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/scripts/mean_vol_split.py
+++ Tools/quippy/scripts/mean_vol_split.py
@@ -9,6 +9,7 @@ p = optparse.OptionParser(usage='%prog file; # splits file by frame, with all ou
 opt, args = p.parse_args()
 
 for file in args:
+   print file
    v_mean = 0.0
    al = AtomsReader(file)
    n=0
@@ -21,12 +22,13 @@ for file in args:
    al = AtomsReader(file)
    i=0
    for at in al:
+      if (hasattr(at,"time")):
+	 print "time=%f " % at.time,
       v = at.cell_volume()
       lat = at.lattice
       new_lat = lat*pow(v_mean/v,1.0/3.0)
       at.set_lattice(new_lat, scale_positions=True)
       dot_pos = file.rindex('.')
-      sys.exit()
       new_file=file[:dot_pos]+(fmt % i)+"."+file[dot_pos+1:]
       print new_file
       at.write(new_file)
