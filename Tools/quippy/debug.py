eaecfce07d5ab87f507386c3d0b0efb773f4062f
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-08-26 15:05:29 +0000

    Support for interactive debugging with ipython before installation.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7141 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/debug.py
+++ Tools/quippy/debug.py
@@ -11,9 +11,5 @@ sys.argv.insert(2, '--verbosity=2')
 if not sys.argv[3].startswith('--test='):
     sys.argv[3] = '--test=' + sys.argv[3]
 
-setup(name='quippy', cmdclass = {'clean': clean, 'test': test, 'build_ext': build_ext}, options = {
-    'build': {
-        'build_base': 'build.%s' % os.environ['QUIP_ARCH'],
-        }
-    }
-      )
+setup(name='quippy', cmdclass = {'test': test},
+      options={'build': { 'build_base': 'build.%s' % os.environ['QUIP_ARCH']}})
