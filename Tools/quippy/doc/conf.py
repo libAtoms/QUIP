9207c03d8aadabb53e4034697e45fc66e759ee46
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-08-27 16:20:12 +0000

    Updated copyright dates.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7163 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/doc/conf.py
+++ Tools/quippy/doc/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'quippy'
-copyright = u'2009, James Kermode'
+copyright = u'2008-2010, James Kermode'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
