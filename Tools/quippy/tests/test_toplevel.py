4ee1f9a6320711d5d1a31c5a41fc61041b42146e
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-11-03 17:54:59 +0000

    Skip backup files in test_toplevel.py
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7466 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/test_toplevel.py
+++ Tools/quippy/tests/test_toplevel.py
@@ -34,7 +34,7 @@ if 'QUIP_ROOT' in os.environ:
         return run_script
 
     for script in glob.glob(os.path.join(os.environ['QUIP_ROOT'], 'Tests/test_*')):
-        if '.' in script: continue # skip data files
+        if '.' in script or script.endswith('~') : continue # skip data and backup files
         setattr(Test_TopLevel, os.path.basename(script), make_test(script))
 
                           
