35973716114b1b779a72ad1d8f5e666f1b351293
pastewka <pastewka@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-11-19 14:28:56 +0000

    Utility script to check order of use statements.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7603 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

---
+++ utility_scripts/checkuse.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+
+#
+# Check order of use statements
+#
+
+import sys
+import string
+
+###
+
+def parse_use_statements(fn):
+    f = open(fn, 'r')
+    l = f.readline()
+    start_parse = False
+    stop_parse = False
+    module_name = None
+    use_statements = [ ]
+    while l and not stop_parse:
+        s = map(string.strip, l.split())
+        if len(s) > 0:
+            if s[0] == 'module' and not start_parse:
+                start_parse = True
+                module_name = s[1].lower()
+            elif start_parse:
+                if s[0] == 'use':
+                    use_statements += [ s[1].lower() ]
+                elif s[0] == 'contains':
+                    stop_parse = True
+        l = f.readline()
+    f.close()
+    return module_name, use_statements
+
+###
+
+def check_deps(deplist, name, used=None):
+    if used is None:
+        used = set()
+
+    for mod in deplist[name]:
+        if mod in used:
+            print 'In module %s: Module %s should be included earlier.' % ( name, mod )
+        if mod in deplist:
+            for depmod in deplist[mod]:
+                used.add(depmod)
+
+
+###
+
+deplist = { }
+for fn in sys.argv[1:]:
+    name, use = parse_use_statements(fn)
+    if name is not None:
+        deplist[name] = use
+
+for mod in deplist.keys():
+    check_deps(deplist, mod)
