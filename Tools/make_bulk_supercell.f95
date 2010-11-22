98ddb04638768d344d13c46f6e17568236b4a19a
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-11-22 20:21:05 +0000

    help_string messages for Tools/make_bulk_supercell.f95
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7621 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/make_bulk_supercell.f95
+++ Tools/make_bulk_supercell.f95
@@ -12,12 +12,12 @@ implicit none
   call system_initialise(verbosity=PRINT_SILENT)
 
   call initialise(cli_params)
-  call param_register(cli_params,"struct", PARAM_MANDATORY, struct, help_string="No help yet.  This source file was $LastChangedBy$")
-  call param_register(cli_params,"outfile", "stdout", outfile, help_string="No help yet.  This source file was $LastChangedBy$")
-  call param_register(cli_params,"vol_per_atom", "-1.0", vol_per_atom, help_string="No help yet.  This source file was $LastChangedBy$")
-  call param_register(cli_params,"vol_per_unit_cell", "-1.0", vol_per_unit_cell, help_string="No help yet.  This source file was $LastChangedBy$")
-  call param_register(cli_params,"repeat", "1 1 1", repeat, help_string="No help yet.  This source file was $LastChangedBy$")
-  call param_register(cli_params,"Z_values", "", Z_values_str, help_string="No help yet.  This source file was $LastChangedBy$")
+  call param_register(cli_params,"struct", PARAM_MANDATORY, struct, help_string="name of structure (or full path)")
+  call param_register(cli_params,"outfile", "stdout", outfile, help_string="name of output file")
+  call param_register(cli_params,"vol_per_atom", "-1.0", vol_per_atom, help_string="volume per atom")
+  call param_register(cli_params,"vol_per_unit_cell", "-1.0", vol_per_unit_cell, help_string="volume per unit cell")
+  call param_register(cli_params,"repeat", "1 1 1", repeat, help_string="number of times to repeate supercell in each direction")
+  call param_register(cli_params,"Z_values", "", Z_values_str, help_string="values of atomic number to assign to each atom type in structure file")
   if (.not. param_read_args(cli_params)) then
     call print("Usage: make_bulk_supercell struct=[struct_name] outfile=[filename](stdout)", PRINT_ALWAYS)
     call print("       [ vol_per_atom=volume | vol_per_unit_cell=volume ] [ repeat='n1 n2 n3'(1 1 1) ]", PRINT_ALWAYS)
