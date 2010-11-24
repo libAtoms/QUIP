24769b9fcfb8fe6a0b737f6e765b366f5c264eb4
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2010-11-24 19:57:25 +0000

    Merge snap.py into convert.py, and fix --centre command line option
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7635 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy/atomeyewriter.py
+++ Tools/quippy/quippy/atomeyewriter.py
@@ -17,35 +17,31 @@
 # HQ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 from quippy import AtomsWriters
-import numpy
+import numpy, os
 import atomeye
 
 class AtomEyeWriter(object):
     "Write atoms to image file (png/eps/jpg) using AtomEye"
 
-    def __init__(self, image, width=None, height=None, aspect=0.75, centre=None, shift=None,
-                 commands=None, script=None, nowindow=True, *showargs, **showkwargs):
+    def __init__(self, image, width=None, height=None, aspect=0.75, shift=True,
+                 commands=None, script=None, nowindow=True, number_frames=False):
         self.image = image
         self.frame = None
         self.width = width
         self.height = height
         self.aspect = aspect
-        self.centre = centre
         self.shift = shift
         self.commands = commands
         self.script = script
         self.nowindow = nowindow
-        self.showargs = showargs
-        self.showkwargs = showkwargs
         self.first_config = True
-        
 
-    def write(self, at):
+    def write(self, at, centre=None, frame=None, *showargs, **showkwargs):
         if self.first_config:
-            # If it's the first time, create the AtomEye viewer
+            # If it's the first time, create an AtomEye viewer
             self.view = atomeye.AtomEyeView(nowindow=self.nowindow, echo=True, block=True)
 
-        self.view.show(at, *self.showargs, **self.showkwargs)
+        self.view.show(at, *showargs, **showkwargs)
 
         if self.first_config:
             self.first_config = False
@@ -62,18 +58,21 @@ class AtomEyeWriter(object):
                 if self.height is None: self.height = int(self.width*self.aspect)
                 self.view.resize(self.width, self.height)
 
-            if self.shift is not None:
-                self.view.xtal_origin_goto(self.shift)
+            if self.shift:
+                self.view.xtal_origin_goto([.5, .5, .5])
 
-            if self.centre is not None:
-                if isinstance(self.centre, int):
-                    self.view.run_command('set n->anchor %d' % self.centre)
-                else:
-                    self.view.run_command('set n->anchor -1')
-                    self.view.run_command('set n->hook %f %f %f' % tuple(numpy.dot(at.g, self.centre)))
-                self.view.look_at_the_anchor()
+        if centre is not None:
+            if isinstance(centre, int):
+                self.view.run_command('set n->anchor %d' % centre)
+            else:
+                self.view.run_command('set n->anchor -1')
+                self.view.run_command('set n->hook %f %f %f' % tuple(centre + numpy.diag(at.lattice)/2.))
+            self.view.look_at_the_anchor()
 
-        self.view.capture(self.image)
+        if frame is not None:
+            self.view.capture('%s%05d%s' % (os.path.splitext(self.image)[0], frame, os.path.splitext(self.image)[1]))
+        else:
+            self.view.capture(self.image)                
 
     def close(self):
         pass
