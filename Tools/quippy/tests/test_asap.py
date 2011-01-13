157ffd17a4ba091cfc60bca265f1ea8eb5c56907
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2011-01-13 14:23:55 +0000

    Fix failing test: pseudise_sigma moved to per_type_data
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@7815 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/tests/test_asap.py
+++ Tools/quippy/tests/test_asap.py
@@ -9369,12 +9369,12 @@ class TestPseudise(QuippyTestCase):
    def setUp(self):
       self.xml = """<ASAP_params label="screened_LDA" betapol="0.75" cutoff="20.0 20.0 18.0 0.0" cutoff_coulomb="20.0" cutoff_ms="18.0" tolpol="1e-10" yuksmoothlength="10.0" iesr="-1 -1 -1" a_ew="1e-06" n_types="2" gcut="0.0" pred_order="2" maxipol="60" raggio="0.0" tewald="F" yukalpha="0.1">
 
-     <per_type_data atomic_num="8" pol="14.131863" z="-1.4295594" type="1" />
-     <per_type_data atomic_num="14" pol="0.0" z="2.8591188" type="2" />
+     <per_type_data pseudise_sigma="1.0" atomic_num="8" pol="14.131863" z="-1.4295594" type="1" />
+     <per_type_data pseudise_sigma="1.0" atomic_num="14" pol="0.0" z="2.8591188" type="2" />
 
-     <per_pair_data pseudise_sigma="1.0" C_pol="0.44302622" atnum_j="8" atnum_i="8" D_ms="0.00030700577" gamma_ms="12.165654" B_pol="1.1221903" R_ms="7.0252019" />
-     <per_pair_data pseudise_sigma="1.0" C_pol="-1.5003213" atnum_j="8" atnum_i="14" D_ms="0.0020129372" gamma_ms="11.350477" B_pol="1.973181" R_ms="4.5780828" />
-     <per_pair_data pseudise_sigma="1.0" C_pol="0.0" atnum_j="14" atnum_i="14" D_ms="0.33967532" gamma_ms="-0.17694797" B_pol="0.0" R_ms="-0.085202834" />
+     <per_pair_data C_pol="0.44302622" atnum_j="8" atnum_i="8" D_ms="0.00030700577" gamma_ms="12.165654" B_pol="1.1221903" R_ms="7.0252019" />
+     <per_pair_data C_pol="-1.5003213" atnum_j="8" atnum_i="14" D_ms="0.0020129372" gamma_ms="11.350477" B_pol="1.973181" R_ms="4.5780828" />
+     <per_pair_data C_pol="0.0" atnum_j="14" atnum_i="14" D_ms="0.33967532" gamma_ms="-0.17694797" B_pol="0.0" R_ms="-0.085202834" />
 
      </ASAP_params>"""
 
