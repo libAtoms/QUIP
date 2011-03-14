c12c7cdd755e7b508f5e02e6f125d1f7a3673f94
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2011-03-14 15:26:44 +0000

    ipython magic functions "at" and "al" for Atoms and AtomsList respectively
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@8120 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy_load.py
+++ Tools/quippy/quippy_load.py
@@ -25,6 +25,13 @@ def load_atoms(self, arg):
     ip = self.api
     f = arg
     n = os.path.splitext(os.path.basename(arg))[0].replace('-','_').replace('.','_')
-    ip.ex('%s = Atoms("%s")' % (n,f))
+    ip.ex('%s = Atoms("%s"); %s.show()' % (n,f,n))
 
-ip.expose_magic("atoms", load_atoms)
+def load_atoms_list(self, arg):
+    ip = self.api
+    f = arg
+    n = os.path.splitext(os.path.basename(arg))[0].replace('-','_').replace('.','_')
+    ip.ex('%s = AtomsList("%s"); %s.loadall(); %s.show(frame=0)' % (n,f,n,n))
+
+ip.expose_magic("at", load_atoms)
+ip.expose_magic("al", load_atoms_list)
