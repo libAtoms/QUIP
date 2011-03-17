c1ff4250298bd130ceca0abfa78c539e7360393a
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2011-03-17 18:48:22 +0000

    Workaround for ifort 10.1 compiler bug
    Add utility routine error_linebreak_string_length() to compute length of
    string returned by error_linebreak_string(). Without this, evaluation of
    string length causes arithmetic error when MPI active.
    
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@8137 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- libAtoms/error.f95
+++ libAtoms/error.f95
@@ -338,11 +338,20 @@ contains
 
   endsubroutine error_abort_from_stack
 
+  pure function error_linebreak_string_length(str, line_len) result(length)
+    character(len=*), intent(in) :: str
+    integer, intent(in) :: line_len
+    integer :: length
+
+    length = len_trim(str)+2*len_trim(str)/line_len+3
+
+  end function error_linebreak_string_length
+
   function error_linebreak_string(str, line_len) result(lb_str)
     character(len=*), intent(in) :: str
     integer, intent(in) :: line_len
 
-    character(len=len_trim(str)+2*len_trim(str)/line_len+3) :: lb_str
+    character(len=error_linebreak_string_length(str, line_len)) :: lb_str
 
     logical :: word_break
     integer :: copy_len, last_space
