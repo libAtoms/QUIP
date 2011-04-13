bf71dd6c2fabb92aee0a12e871614e381a105167
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2011-04-13 13:43:46 +0000

    Move Python elastic_fields() routine into quoippy.elastic module.
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@8263 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy/elastic.py
+++ Tools/quippy/quippy/elastic.py
@@ -444,3 +444,305 @@ def atomic_strain(at, r0, crystal_factor=1.0):
                strain[i,j,l] += n.diff[i]*n.diff[j]/r0**2.0
                
    return strain/crystal_factor
+
+
+def elastic_fields(at, a, cij, save_reference=False, use_reference=False, mask=None):
+   """
+   Compute atomistic strain field and linear elastic stress response.
+
+   Stress and strain are stored in compressed Voigt notation::
+
+      at.strain[:,i] = [e_xx,e_yy,e_zz,e_yz,e_xz,e_xy]
+      at.stress[:,i] = [sig_xx, sig_yy, sig_zz, sig_yz, sig_xz, sig_xy]
+
+   so that sig = dot(C, strain) in the appropriate reference frame.
+   
+   Four-fold coordinate atoms within `at` are used to define
+   tetrahedra. The deformation of each tetrahedra is determined
+   relative to the ideal structure, using `a` as the cubic lattice
+   constant (related to bond length by a factor :math:`sqrt{3}/4`).
+   This deformation is then split into a strain and a rotation
+   using a Polar decomposition.
+
+   If `save_reference` or `use_reference` are True then `at` must have
+   a `primitive_index` integer property which is different for each
+   atom in the primitive unit cell. `save_reference` causes the
+   local strain and rotation for one atom of each primitive type
+   to be saved as entries `at.params`. Conversely, `use_reference` uses
+   this information and to undo the local strain and rotation.
+
+   The strain is then transformed into the crystal reference frame
+   (i.e. x=100, y=010, z=001) to calculate the stress using the `cij`
+   matrix of elastic constants. Finally the resulting stress is
+   transformed back into the sample frame.
+
+   The stress and strain tensor fields are interpolated to give
+   values for atoms which are not four-fold coordinated (for example
+   oxygen atoms in silica).
+   
+   Eigenvalues and eigenvectors of the stress are stored in the
+   properties `stress_evals`,`stress_evec1`, `stress_evec2` and
+   `stress_evec3`, ordered decreasingly by eigenvalue so that the
+   principal eigenvalue and eigenvector are `stress_evals[1,:]` and
+   `stress_evec1[:,i]` respectively.
+   
+   """
+
+   def compute_stress_eig(i):
+      D, SigEvecs = numpy.linalg.eig(stress_matrix(at.stress[:,i]))
+
+      # Order by descending size of eigenvalues
+      sorted_evals, order = zip(*sorted(zip(D, [1,2,3]),reverse=True))
+         
+      at.stress_eval[:,i]  = sorted_evals
+      at.stress_evec1[:,i] = SigEvecs[:,order[0]]
+      at.stress_evec2[:,i] = SigEvecs[:,order[1]]
+      at.stress_evec3[:,i] = SigEvecs[:,order[2]]
+
+
+   # We want nearest neighbour connectivity only
+   save_cutoff, save_use_uniform_cutoff = at.cutoff, at.use_uniform_cutoff
+   at.set_cutoff_factor(1.2)
+   at.calc_connect()
+
+   if (save_reference or use_reference) and not at.has_property('primitive_index'):
+      raise ValueError('Property "primitive_index" missing from Atoms object')
+
+   # Add various properties to store results
+   at.add_property('strain', 0.0, n_cols=6)
+   at.add_property('stress', 0.0, n_cols=6)
+   at.add_property('stress_eval', 0.0, n_cols=3)
+   at.add_property('stress_evec1', 0.0, n_cols=3)
+   at.add_property('stress_evec2', 0.0, n_cols=3)
+   at.add_property('stress_evec3', 0.0, n_cols=3)
+
+   rotXYZ = fzeros((3,3))
+   rotXYZ[1,2] = 1.0
+   rotXYZ[2,3] = 1.0
+   rotXYZ[3,1] = 1.0
+   E = fidentity(3)
+   
+
+   # Consider first atoms with four neighbours
+   for i in frange(at.n):
+      if mask is not None and not mask[i]: continue
+      
+      neighb = at.neighbours[i]
+
+      if len(neighb) == 4:
+         #print '\nProcessing atom', i
+
+         # Consider neighbours in order of their index within the primitive cell
+         if hasattr(at, 'primitive_index'):
+            (j1,i1), (j2,i2), (j3,i3), (j4,i4) = sorted((at.primitive_index[n.j],i) for i,n in fenumerate(neighb))
+         else:
+            (j1,i1), (j2,i2), (j3,i3), (j4,i4) = list((n.j,i) for i,n in fenumerate(neighb))
+
+         # Find cubic axes from neighbours
+         n1 = neighb[i2].diff - neighb[i1].diff
+         n2 = neighb[i3].diff - neighb[i1].diff
+         n3 = neighb[i4].diff - neighb[i1].diff
+         #print 'n1', n1.norm(), n1
+         #print 'n2', n2.norm(), n2
+         #print 'n3', n3.norm(), n3
+
+         E[:,1] = (n1 + n2 - n3)/a
+         E[:,2] = (n2 + n3 - n1)/a
+         E[:,3] = (n3 + n1 - n2)/a
+
+         #print 'E', E
+
+         # Kill near zero elements
+         E[abs(E) < 1e-6] = 0.0
+
+         if (E < 0.0).all(): E = -E
+
+         # Find polar decomposition: E = S*R where S is symmetric, 
+         # and R is a rotation
+         #
+         #  EEt = E*E', EEt = VDV' D diagonal, S = V D^1/2 V', R = S^-1*E
+         EEt = numpy.dot(E, E.T)
+
+         D, V = numpy.linalg.eig(EEt)
+
+         S = farray(numpy.dot(numpy.dot(V, numpy.diag(numpy.sqrt(D))), V.T))
+         R = farray(numpy.dot(numpy.dot(numpy.dot(V, numpy.diag(D**-0.5)), V.T), E))
+
+         #print 'S:', S
+         #print 'R:', R
+
+         if save_reference:
+            key = 'strain_inv_%d' % at.primitive_index[i]
+            if key not in at.params:
+               at.params[key] = numpy.linalg.inv(S)
+
+         if use_reference:
+            S = numpy.dot(S, at.params['strain_inv_%d' % at.primitive_index[i]])
+            
+         #print 'S after apply S0_inv:', S
+
+         # Strain in rotated coordinate system
+         at.strain[:,i] = strain_vector(S)
+
+         # Test for permutations - check which way x points
+         RtE = numpy.dot(R.T, E)
+         if RtE[2,1] > RtE[1,1] and RtE[2,1] > RtE[3,1]:
+            R = numpy.dot(rotXYZ, R)
+         elif RtE[3,1] > RtE[1,1] and RtE[3,1] > RtE[2,1]:
+            R = numpy.dot(rotXYZ.T, R)
+
+         if save_reference:
+            key = 'rotation_inv_%d' % at.primitive_index[i]
+            if key not in at.params:
+               at.params[key] = R.T
+               
+         if use_reference:
+            R = numpy.dot(R, at.params['rotation_inv_%d' % at.primitive_index[i]])
+
+         #print 'R after apply R0t:', R            
+
+         # Rotate to crystal coordinate system to apply Cij matrix
+         RtSR = numpy.dot(numpy.dot(R.T, S), R)
+         #print 'RtSR:', RtSR
+
+         sig = stress_matrix(numpy.dot(cij, strain_vector(RtSR)))
+
+         #print 'sig', sig
+
+         # Rotate back to local coordinate system
+         RsigRt = numpy.dot(numpy.dot(R, sig), R.T)
+
+         # Symmetrise stress tensor
+         RsigRt = (RsigRt + RsigRt.T)/2.0
+         at.stress[:,i] = stress_vector(RsigRt)
+
+         compute_stress_eig(i)
+
+   # For atoms without 4 neighbours, interpolate stress and strain fields
+   for i in frange(at.n):
+      if mask is not None and not mask[i]: continue
+      
+      neighb = at.neighbours[i]
+      if len(neighb) == 4: continue
+
+      at.strain[:,i] = at.strain[:,[n.j for n in neighb]].mean(axis=1)
+      at.stress[:,i] = at.stress[:,[n.j for n in neighb]].mean(axis=1)
+
+      compute_stress_eig(i)
+
+   # Restore original neighbour cutoff
+   at.cutoff, at.use_uniform_cutoff = save_cutoff, save_use_uniform_cutoff
+
+   
+
+def elasticity_matrix_to_tensor(C):
+   """Given a 6x6 elastic matrix in compressed Voigt notation, return
+   the full 3x3x3x3 elastic constant tensor C_ijkl"""
+
+   c = zeros((3,3,3,3),'d')
+   for p in frange(6):
+      for q in frange(6):
+         i, j = voigt_map[p]
+         k, l = voigt_map[q]
+         c[i,j,k,l] = c[j,i,k,l] = c[i,j,l,k] = c[j,i,l,k] = c[k,l,i,j] = C[p,q]
+
+   return c
+
+def elasticity_tensor_to_matrix(c):
+   """Given full tensor c_ijkl, return compressed Voigt form C_ij.
+   First checks that c_ijkl obeys required symmetries."""
+   
+   tol = 1e-10
+   for i in frange(3):
+      for j in frange(3):
+         for k in frange(3):
+            for l in frange(3):
+               assert(c[i,j,k,l] - c[j,i,k,l] < tol)
+               assert(c[i,j,k,l] - c[i,j,l,k] < tol)
+               assert(c[i,j,k,l] - c[j,i,l,k] < tol)
+               assert(c[i,j,k,l] - c[k,l,i,j] < tol)
+
+   C = zeros((6,6),'d')
+   for p in frange(6):
+      for q in frange(6):
+         i, j = voigt_map[p]
+         k, l = voigt_map[q]
+         C[p,q] = c[i,j,k,l]
+
+   return C
+
+# Voigt notation: 1 = 11 (xx), 2 = 22 (yy), 3 = 33 (zz), 4 = 23 (yz), 5 = 31 (zx), 6 = 12 (xy
+voigt_map = dict( ((1,(1,1)),  # xx
+                   (2,(2,2)),  # yy
+                   (3,(3,3)),  # zz
+                   (4,(2,3)),  # yz
+                   (5,(3,1)),  # zx
+                   (6,(1,2)))) # xy
+
+def elasticity_matrix_to_tensor(C):
+   """Given a 6x6 elastic matrix in compressed Voigt notation, return
+   the full 3x3x3x3 elastic constant tensor C_ijkl"""
+
+   c = fzeros((3,3,3,3),'d')
+   for p in frange(6):
+      for q in frange(6):
+         i, j = voigt_map[p]
+         k, l = voigt_map[q]
+         c[i,j,k,l] = c[j,i,k,l] = c[i,j,l,k] = c[j,i,l,k] = c[k,l,i,j] = C[p,q]
+
+   return c
+
+def elasticity_tensor_to_matrix(c):
+   """Given full tensor c_ijkl, return compressed Voigt form C_ij.
+   First checks that c_ijkl obeys required symmetries."""
+   
+   tol = 1e-10
+   for i in frange(3):
+      for j in frange(3):
+         for k in frange(3):
+            for l in frange(3):
+               assert(c[i,j,k,l] - c[j,i,k,l] < tol)
+               assert(c[i,j,k,l] - c[i,j,l,k] < tol)
+               assert(c[i,j,k,l] - c[j,i,l,k] < tol)
+               assert(c[i,j,k,l] - c[k,l,i,j] < tol)
+
+   C = fzeros((6,6),'d')
+   for p in frange(6):
+      for q in frange(6):
+         i, j = voigt_map[p]
+         k, l = voigt_map[q]
+         C[p,q] = c[i,j,k,l]
+
+   return C
+
+def transform_elasticity(c, R):
+   """Transform c as a rank-4 tensor by the rotation matrix R and return
+   a new representation c'. If c is a 6x6 matrix it is first converted to 3x3x3x3 form,
+   and then converted back after the transformation."""
+   
+   made_tensor = False
+   if c.shape == (6,6):
+      made_tensor = True
+      c = elasticity_matrix_to_tensor(c)
+   elif c.shape == (3,3,3,3):
+      pass
+   else:
+      raise ValueError('Tensor should either be 3x3x3x3 or 6x6 matrix')
+
+   cp = fzeros((3,3,3,3),'d')
+   for i in frange(3):
+      for j in frange(3):
+         for k in frange(3):
+            for l in frange(3):
+               for p in frange(3):
+                  for q in frange(3):
+                     for r in frange(3):
+                        for s in frange(3):
+                           cp[i,j,k,l] += R[p,i]*R[q,j]*R[r,k]*R[s,l]*c[p,q,r,s]
+
+   if made_tensor:
+      return elasticity_tensor_to_matrix(cp)
+   else:
+      return cp
+
+   
