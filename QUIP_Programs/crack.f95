d3be7ab67c046f871bf2e54a9e643d44d4823839
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2011-05-31 17:31:02 +0000

    Fixed typo
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@8490 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Programs/crack.f95
+++ QUIP_Programs/crack.f95
@@ -1090,8 +1090,8 @@ program crack
                     if (params%qm_calc_force_error) call print('I err '//ds%t//' '//rms_diff(force, f_fm)//' '//maxval(abs(f_fm-force)))
 
                     if (trim(params%simulation_task) == 'damped_md') then
-                       do i=1,ds%atoms%n
-                          if (move_mask(i)) force(:,i) = 0.0_dp
+                       do j=1,ds%atoms%n
+                          if (move_mask(j) == 0) force(:,j) = 0.0_dp
                        end do
                        call print('Damped MD: normsq(force) = '//normsq(reshape(force,(/3*ds%N/)))//&
                             ' max(abs(force)) = '//maxval(abs(force)))
