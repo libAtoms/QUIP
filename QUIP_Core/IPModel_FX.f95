f39865e21d175f750a428c19bb8ab99e4b8c328e
gc121 <gc121@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2011-08-11 14:16:41 +0000

    added spline weighting. untested (Lion update broke my build env)
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@8899 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Core/IPModel_FX.f95
+++ QUIP_Core/IPModel_FX.f95
@@ -55,9 +55,13 @@ include 'IPModel_interface.h'
 
 public :: IPModel_FX
 type IPModel_FX
-   real(dp) :: cutoff = 2.0_dp
+   real(dp) :: cutoff = 0.0_dp
    logical :: return_one_body = .false.
    logical :: return_two_body = .false.
+   real(dp) :: two_body_weight_roo = 0.0_dp
+   real(dp) :: two_body_weight_delta = 0.0_dp
+   logical  :: do_two_body_weight = .false.
+   type(Spline) :: weight
 end type IPModel_FX
 
 
@@ -83,21 +87,27 @@ subroutine IPModel_FX_Initialise_str(this, args_str, param_str, error)
   type(IPModel_FX), intent(inout) :: this
   character(len=*), intent(in) :: args_str, param_str
   type(Dictionary)                :: params
-  logical :: return_two_body, return_one_body
   integer, optional, intent(out) :: error
 
   INIT_ERROR(error)
 
+  call Finalise(this)
+
   call initialise(params)
   call param_register(params, 'return_two_body', 'F', this%return_two_body, help_string="if set, return the two_body energy and force as the main return data")
   call param_register(params, 'return_one_body', 'F', this%return_one_body, help_string="if set, return the one_body energy and force as the main return data")
+  call param_register(params, 'two_body_weight_roo', '0.0', this%two_body_weight_roo, has_value_target=this%do_two_body_weight, help_string="if set, apply weight function to 2-body energy and force based on O-O distance. For a positive two_body_weight_delta, weight is 1 for rOO < two_body_weight_roo-two_body_weight_delta and weight is 0 for rOO > two_body_weight_roo+two_body_weight_delta")
+  call param_register(params, 'two_body_weight_delta', '0.25', this%two_body_weight_delta, help_string="width of weighting function for  two_body energy and force based on O-O distance. For weighting to take effect, two_body_weight_roo needs to be explicitly set. For a positive two_body_weight_delta, weight is 1 for rOO < two_body_weight_roo-two_body_weight_delta and weight is 0 for rOO > two_body_weight_roo+two_body_weight_delta")
   
   if(.not. param_read_line(params, args_str, ignore_unknown=.true.,task='IPModel_FX_Initialise args_str')) then
      RAISE_ERROR("IPModel_FX_Calc failed to parse args_str='"//trim(args_str)//"'", error)
   endif
   call finalise(params)
 
-  call Finalise(this)
+  if(this%do_two_body_weight) then
+     call initialise(this%two_body_weight, (/this%two_body_weight_roo - this%two_body_weight_delta, this%two_body_weight_roo + this%two_body_weight_delta/), (/1.0_dp, 0.0_dp/), 0.0_dp, 0.0_dp)
+  end if
+
 end subroutine IPModel_FX_Initialise_str
 
 subroutine IPModel_FX_Finalise(this)
@@ -118,7 +128,7 @@ subroutine IPModel_FX_Calc(this, at, e, local_e, f, virial, local_virial, args_s
   real(dp), dimension(3,at%N) :: RR, dRR
   real(dp) :: energy
   integer, dimension(at%N) :: rindex
-  integer :: i, j, k, ii, jj, kk
+  integer :: i, j, k, kk, Oi, Oj
 
   type(Dictionary)                :: params
   logical :: has_atom_mask_name, do_one_body, do_two_body
@@ -129,6 +139,7 @@ subroutine IPModel_FX_Calc(this, at, e, local_e, f, virial, local_virial, args_s
   real(dp) :: watpos(3,3), wat2pos(3,6), watRR(3,3), watdRR(3,3), wat2RR(3,6), wat2dRR(3,6), wat2_force(3,6)
   real(dp) :: two_body_energy, diff_iijj(3), watE, wat2E
   integer :: wat_rindex(3), wat2_rindex(6), watZ(3), wat2Z(6)
+   real(dp):: weight, dweight(3), rOiOj
 
   INIT_ERROR(error)
 
@@ -220,31 +231,46 @@ subroutine IPModel_FX_Calc(this, at, e, local_e, f, virial, local_virial, args_s
         two_body_force = 0.0_dp
         do i=1,at%N/3
            do j=i+1,at%N/3
-              ii = water_monomer_index(1,i)
-              jj = water_monomer_index(1,j)
-              wat2pos(:,1) = at%pos(:,ii)
+              Oi = water_monomer_index(1,i)
+              Oj = water_monomer_index(1,j)
+              wat2pos(:,1) = at%pos(:,Oi)
               wat2Z(1) = 8 ! first Oxygen
-              wat2pos(:,2) = at%pos(:,ii) + diff_min_image(at, ii, water_monomer_index(2,i))
-              wat2pos(:,3) = at%pos(:,ii) + diff_min_image(at, ii, water_monomer_index(3,i))
+              wat2pos(:,2) = at%pos(:,Oi) + diff_min_image(at, Oi, water_monomer_index(2,i))
+              wat2pos(:,3) = at%pos(:,Oi) + diff_min_image(at, Oi, water_monomer_index(3,i))
               wat2Z(2:3) = 1 ! first Hydrogens
-              diff_iijj = diff_min_image(at, ii, jj)
-              wat2pos(:,4) = at%pos(:,ii) + diff_iijj
+              diff_OiOj = diff_min_image(at, Oi, Oj)
+              wat2pos(:,4) = at%pos(:,Oi) + diff_OiOj
               wat2Z(4) = 8 ! second Oxygen
-              wat2pos(:,5) = at%pos(:,ii) + diff_iijj+diff_min_image(at, jj, water_monomer_index(2,j))
-              wat2pos(:,6) = at%pos(:,ii) + diff_iijj+diff_min_image(at, jj, water_monomer_index(3,j))
+              wat2pos(:,5) = at%pos(:,Oi) + diff_OiOj+diff_min_image(at, Oj, water_monomer_index(2,j))
+              wat2pos(:,6) = at%pos(:,Oi) + diff_OiOj+diff_min_image(at, Oj, water_monomer_index(3,j))
               wat2Z(5:6) = 1 ! first Hydrogens
 
               call nttm3f_readXYZ(2, wat2Z, wat2pos, wat2RR, wat2_rindex)
               call ttm3f(2, wat2RR, wat2dRR, wat2E)
-              two_body_energy = two_body_energy + wat2E * KCAL_MOL - one_body_energy(i) - one_body_energy(j)
+
+              if(this%do_two_body_weight) then
+                 rOiOj = norm(diff_OiOj)
+                 weight = spline_value(this%two_body_weight, rOiOj)
+                 dweight = spine_deriv(this%two_body_weight, rOiOj)
+              else
+                 weight = 1.0_dp
+                 dweight = 0.0_dp
+              end if
+
+              two_body_energy_ij = (wat2E * KCAL_MOL - one_body_energy(i) - one_body_energy(j)) * weight
+              two_body_energy = two_body_energy + two_body_energy_ij
+
               do k=1,6
                  wat2_force(:,k) = -wat2dRR(:,wat2_rindex(k)) * KCAL_MOL
               end do
               do k=1,3
                  kk = water_monomer_index(k, i)
-                 two_body_force(:,kk) = two_body_force(:,kk) + wat2_force(:,k)-one_body_force(:,kk)
+                 two_body_force(:,kk) = two_body_force(:,kk) + (wat2_force(:,k)-one_body_force(:,kk))*weight
+                 if(k==1) two_body_force(:,kk) = two(body_force(:,kk) - two_body_energy_ij*dweight*diff_OiOj/rOiOj
+
                  kk = water_monomer_index(k, j)
-                 two_body_force(:,kk) = two_body_force(:,kk) + wat2_force(:,3+k)-one_body_force(:,kk)
+                 two_body_force(:,kk) = two_body_force(:,kk) + (wat2_force(:,3+k)-one_body_force(:,kk))*weight
+                 if(k==1) two_body_force(:,kk) = two_body_force(:,kk) + two_body_energy_ij*dweight*diff_OiOj/rOiOj
               end do
            end do
         end do
@@ -276,6 +302,15 @@ subroutine IPModel_FX_Print(this, file)
   call print("polarisable model for water", file=file)
   call print("by G. S. Fanourgakis and S. S. Xantheas", file=file)
   call print("Journal of Chemical Physics 128, 074506 (2008)", file=file)
+  if(this%return_two_body) then
+     call print("Returning 2-body term as a result of calc()", file=file)
+  end if
+  if(this%return_one_body) then
+     call print("Returning 1-body term as a result of calc()", file=file)
+  end if
+  if(this%two_body_weighting) then
+     call print("Two-body term is weighted with parameters x0="//this%two_body_weight_roo//" delta="//this%two_body_weight_delta, file=file)
+  end if
 end subroutine IPModel_FX_Print
 
 
