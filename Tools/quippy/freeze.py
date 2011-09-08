7d35d7afecfef936c4e0f66d43b77b7ca17307d9
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2011-09-08 20:04:51 +0000

    Remove unused module
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@9090 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/freeze.py
+++
@@ -1,7 +0,0 @@
-from cx_Freeze import setup, Executable
-
-buildOptions = dict(excludes=['tcl','tk','Tkinter','ase','matplotlib'])
-
-setup(name='convert',
-      executables= [ Executable('scripts/convert.py') ],
-      options = dict(build_exe=buildOptions))
