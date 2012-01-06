5efc8fa13586dc3281627591bad5f012a00e122b
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-01-06 19:26:43 +0000

    QUIP_Core/profess_driver/profess_driver.py wraps atoms to lattice coordinates [0..1] slowly, without numpy
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@9580 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- QUIP_Core/profess_driver/profess_driver.py
+++ QUIP_Core/profess_driver/profess_driver.py
@@ -2,6 +2,8 @@
 
 import sys, os, optparse, re
 
+from math import floor
+
 p = optparse.OptionParser(usage="%prog infile outfile [force] [virial] [other options]")
 
 p.add_option("-n","--no_old_density", action="store_true", default=False, help="""If true, don't try to use old density""")
@@ -85,9 +87,65 @@ if not opts.no_old_density and os.path.exists("profess_driver.0.den"):
       f_inpt_out.write("rhof profess_driver.0.den")
 f_inpt_out.close()
 
+def scalar_triple_product(x,y,z):
+   return (x[0] * ( y[1]*z[2] - y[2]*z[1] )    - x[1] * ( y[0]*z[2] - y[2]*z[0] )    + x[2] * ( y[0]*z[1] - y[1]*z[0] ))
+
+def cross(x,y):
+   return ( [ x[1]*y[2]-x[2]*y[1] , -x[0]*y[2] + x[2]*y[0], x[0]*y[1] - x[1]*y[0] ] )
+
+def inverse_3x3(a11,a21,a31,a12,a22,a32,a13,a23,a33):
+   m_inv = [0] * 3
+   m_inv[0] = [0.0]*3
+   m_inv[1] = [0.0]*3
+   m_inv[2] = [0.0]*3
+
+   stp = scalar_triple_product( [a11,a21,a31], [a12,a22,a32], [a13, a23, a33] )
+
+   t = cross( [a12,a22,a32], [a13,a23,a33] )
+   m_inv[0][0] = t[0]/stp
+   m_inv[0][1] = t[1]/stp
+   m_inv[0][2] = t[2]/stp
+
+   t = cross( [a13,a23,a33], [a11,a21,a31] )
+   m_inv[1][0] = t[0]/stp
+   m_inv[1][1] = t[1]/stp
+   m_inv[1][2] = t[2]/stp
+
+   t = cross( [a11,a21,a31], [a12,a22,a32] )
+   m_inv[2][0] = t[0]/stp
+   m_inv[2][1] = t[1]/stp
+   m_inv[2][2] = t[2]/stp
+
+   return m_inv
+
+def wrap_pos(x,y,z,lattice,lattice_inv):
+   p_wrap = [x, y, z]
+   p_lat = [0]*3
+   p_lat[0] = lattice_inv[0][0]*x + lattice_inv[0][1]*y + lattice_inv[0][2]*z
+   p_lat[1] = lattice_inv[1][0]*x + lattice_inv[1][1]*y + lattice_inv[1][2]*z
+   p_lat[2] = lattice_inv[2][0]*x + lattice_inv[2][1]*y + lattice_inv[2][2]*z
+
+   p_lat_i = floor(p_lat[0])
+   if (p_lat_i < 0 or p_lat_i >= 1):
+      p_wrap[0] -= p_lat_i*lattice[0][0]
+      p_wrap[1] -= p_lat_i*lattice[1][0]
+      p_wrap[2] -= p_lat_i*lattice[2][0]
+   p_lat_i = floor(p_lat[1])
+   if (p_lat_i < 0 or p_lat_i >= 1):
+      p_wrap[0] -= p_lat_i*lattice[0][1]
+      p_wrap[1] -= p_lat_i*lattice[1][1]
+      p_wrap[2] -= p_lat_i*lattice[2][1]
+   p_lat_i = floor(p_lat[2])
+   if (p_lat_i < 0 or p_lat_i >= 1):
+      p_wrap[0] -= p_lat_i*lattice[0][2]
+      p_wrap[1] -= p_lat_i*lattice[1][2]
+      p_wrap[2] -= p_lat_i*lattice[2][2]
+
+   return p_wrap
+
 # create geometry from template
 f_ion_out = open("profess_driver.ion","w")
-f_input_pos = open("../"+args[0])
+f_input_pos = open("../"+infile)
 pos_lines=f_input_pos.readlines()
 N_at=int(pos_lines[0])
 if not re.search("Properties=species:S:1:pos:R:3", pos_lines[1]):
@@ -95,7 +153,7 @@ if not re.search("Properties=species:S:1:pos:R:3", pos_lines[1]):
    sys.exit(3)
 m=re.search('Lattice=["\'{]\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*["\'}]',pos_lines[1])
 if m is None:
-   sys.stderr.write("Failed to parse Lattice from %s\n" % args[0])
+   sys.stderr.write("Failed to parse Lattice from %s\n" % infile)
    sys.exit(2)
 lattice_str=m.group(0)
 f_ion_out.write("%BLOCK LATTICE_CART\n")
@@ -103,10 +161,19 @@ f_ion_out.write("%s %s %s\n" % (m.group(1), m.group(2), m.group(3)))
 f_ion_out.write("%s %s %s\n" % (m.group(4), m.group(5), m.group(6)))
 f_ion_out.write("%s %s %s\n" % (m.group(7), m.group(8), m.group(9)))
 f_ion_out.write("%END BLOCK LATTICE_CART\n")
+lattice=[
+   [ float(m.group(1)), float(m.group(4)), float(m.group(7)) ],
+   [ float(m.group(2)), float(m.group(5)), float(m.group(8)) ],
+   [ float(m.group(3)), float(m.group(6)), float(m.group(9)) ] ]
+lattice_inv=inverse_3x3(
+   float(m.group(1)), float(m.group(2)), float(m.group(3)),
+   float(m.group(4)), float(m.group(5)), float(m.group(6)),
+   float(m.group(7)), float(m.group(8)), float(m.group(9)) )
 f_ion_out.write("%BLOCK POSITIONS_CART\n")
 for i in range(2,len(pos_lines)):
    fields=pos_lines[i].split()
-   f_ion_out.write ("%s %s %s %s\n" % (fields[0], fields[1], fields[2], fields[3]))
+   p_wrap=wrap_pos(float(fields[1]),float(fields[2]),float(fields[3]),lattice,lattice_inv)
+   f_ion_out.write ("%s %f %f %f\n" % (fields[0], p_wrap[0], p_wrap[1], p_wrap[2]))
    pos_lines[i]="%s %s %s %s" % (fields[0], fields[1], fields[2], fields[3])
 f_ion_out.write("%END BLOCK POSITIONS_CART\n")
 f_ion_template = open("../%s.ion.template" % profess_template)
@@ -144,7 +211,7 @@ if (do_force):
    for i in range(N_at):
       force_str.append(profess_out_ls[2+i].strip())
 
-f_pos_output=open("../"+args[1],"w")
+f_pos_output=open("../"+outfile,"w")
 f_pos_output.write("%d\n" % N_at)
 f_pos_output.write("%s %s=%f Properties=species:S:1:pos:R:3" % (lattice_str, energy_field, energy))
 if (do_force):
