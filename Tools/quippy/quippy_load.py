020c174a51e133f07ae3987b992617396ebfdfe2
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-01-27 18:40:01 +0000

    Updated for ipython versions > 0.11
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@9691 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy_load.py
+++ Tools/quippy/quippy_load.py
@@ -5,34 +5,31 @@ import quippy
 from quippy import *
 
 import os
-import IPython.ipapi
-ip = IPython.ipapi.get()
+
+ip = _ip = get_ipython()
 
 def load_atoms(self, arg):
     """Load file into an Atoms object"""    
-    ip = self.api
     f = arg
     n = os.path.splitext(os.path.basename(arg))[0].replace('-','_').replace('.','_')
-    ip.ex('%s = Atoms("%s"); %s.show()' % (n,f,n))
+    self.ex('%s = Atoms("%s"); %s.show()' % (n,f,n))
 
 def load_atoms_list(self, arg):
     """Load file into an AtomsList object"""
-    ip = self.api
     f = arg
     n = os.path.splitext(os.path.basename(arg))[0]
     n = n.replace('-','_').replace('.','_').replace('*','').replace('?','')
     n = n.strip('_')
-    ip.ex('%s = AtomsList("%s"); %s.show()' % (n,f,n))
+    self.ex('%s = AtomsList("%s"); %s.show()' % (n,f,n))
 
 def load_atoms_reader(self, arg):
     """Load file into an AtomsReader object"""
-    ip = self.api
     f = arg
     n = os.path.splitext(os.path.basename(arg))[0]
     n = n.replace('-','_').replace('.','_').replace('*','').replace('?','')
     n = n.strip('_')
-    ip.ex('%s = AtomsReader("%s"); %s.show()' % (n,f,n))
+    self.ex('%s = AtomsReader("%s"); %s.show()' % (n,f,n))
 
-ip.expose_magic("at", load_atoms)
-ip.expose_magic("ar", load_atoms_reader)
-ip.expose_magic("al", load_atoms_list)
+ip.define_magic("at", load_atoms)
+ip.define_magic("ar", load_atoms_reader)
+ip.define_magic("al", load_atoms_list)
