bb08a6a740112267171d334f62a1d4c078dd66cc
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-02-28 18:00:50 +0000

    Implement variable cell shape MD in barostat, optional restriction to hydrostatic or diagonal strain, optional finite strain formulation (see comments in libAtoms/Barostat.f95).
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@9836 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- libAtoms/Barostat.f95
+++ libAtoms/Barostat.f95
@@ -53,9 +53,17 @@ module barostat_module
                         BAROSTAT_HOOVER_LANGEVIN      = 1
 
   !% Langevin barostat - fluctuation/dissipation ---
+  !% originally implemented for hydrostatic strain using
   !% EOM from Quigley, D. and Probert, M.I.J., \emph{J. Chem. Phys.}, 
   !% {\bfseries 120} 11432
-  !% rediscretized by Noam Bernstein
+  !% rediscretized by Noam Bernstein based on ideas from and discussion
+  !% with B. Leimkuhler.
+  !% modified for arbitrary strain based on (but not exactly using)
+  !% formulation in E. Tadmor and R. Miller Modeling Materials: Continuum, Atomistic and Multiscale 
+  !% Techniques (Cambridge University Press, 2011). Chap 9.5.  Their 
+  !% definition of stress (factors of F, F^T, J, and F^-T) for finite strain is 
+  !% optionally used, but certain terms in EOM are excluded, and their discretization 
+  !% is entirely ignored.
 
   type barostat
 
@@ -63,10 +71,12 @@ module barostat_module
      real(dp) :: gamma_epsilon = 0.0_dp   !% Friction coefficient for Langevin part
      real(dp) :: T = -1.0_dp   !% Temperature for Langevin part
      real(dp) :: Ndof  = 0.0_dp   !% The number of degrees of freedom of atoms attached to this barostat
-     real(dp) :: p_ext = 0.0_dp       !% External pressure
+     real(dp) :: stress_ext(3,3) = 0.0_dp       !% External applied stress
      real(dp) :: W_epsilon = 0.0_dp     !% Fictious cell mass in Langevin NPT
-     real(dp) :: epsilon_v = 0.0_dp !% Velocity of barostat variable
-     real(dp) :: epsilon_f = 0.0_dp !% Force on barostat variable
+     real(dp) :: epsilon_v(3,3) = 0.0_dp !% Velocity of deformation gradient for barostat
+     real(dp) :: epsilon_f(3,3) = 0.0_dp !% Force on deformation gradient
+     real(dp) :: epsilon_r(3,3) = reshape( (/ 1.0_dp, 0.0_dp, 0.0_dp,    0.0_dp, 1.0_dp, 0.0_dp,    0.0_dp, 0.0_dp, 1.0_dp /), (/ 3, 3 /) ) !% Value of deformation gradient
+     logical :: hydrostatic_strain = .true., diagonal_strain = .true., finite_strain_formulation=.false.
 
   end type barostat
 
@@ -106,11 +116,15 @@ contains
   !X
   !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-  subroutine barostat_initialise(this,type,p_ext,cell_volume,W_epsilon,Ndof,gamma_epsilon,T)
+  subroutine barostat_initialise(this,type,p_ext,stress_ext,hydrostatic_strain,diagonal_strain,finite_strain_formulation,deformation_grad,cell_volume,W_epsilon,Ndof,gamma_epsilon,T)
 
     type(barostat),   intent(inout) :: this
     integer,            intent(in)    :: type
     real(dp), optional, intent(in)    :: p_ext
+    real(dp), optional, intent(in)    :: stress_ext(3,3)
+    logical, optional, intent(in)     :: hydrostatic_strain, diagonal_strain
+    logical, optional, intent(in)     :: finite_strain_formulation
+    real(dp), optional, intent(in)    :: deformation_grad(3,3)
     real(dp), optional, intent(in)    :: cell_volume
     real(dp), optional, intent(in)    :: W_epsilon
     real(dp), optional, intent(in)    :: Ndof
@@ -119,18 +133,27 @@ contains
 
     real(dp) :: use_T
 
-    if (type /= BAROSTAT_NONE .and. .not.present(p_ext)) &
-         call system_abort('initialise: p_ext must be specified when turning on a barostat')
+    if (type /= BAROSTAT_NONE .and. .not.present(p_ext) .and. .not. present(stress_ext)) &
+         call system_abort('barostat_initialise: p_ext or stress_ext must be specified when turning on a barostat')
+
+    if (present(p_ext) .and. present(stress_ext)) &
+         call system_abort('barostat_initialise: got both p_ext and stress_ext')
 
     this%type = type
     this%epsilon_v = 0.0_dp
     this%epsilon_f = 0.0_dp
+    this%epsilon_f = 0.0_dp
+    if (present(deformation_grad)) then
+      this%epsilon_r = deformation_grad
+    else
+      this%epsilon_r = 0.0_dp; call add_identity(this%epsilon_r)
+    endif
 
     select case(this%type)
 
     case(BAROSTAT_NONE) 
 
-       this%p_ext = 0.0_dp
+       this%stress_ext = 0.0_dp
        this%gamma_epsilon = 0.0_dp
        this%W_epsilon = 0.0_dp
        this%Ndof = 0.0_dp
@@ -138,7 +161,8 @@ contains
 
     case(BAROSTAT_HOOVER_LANGEVIN)
 
-       if (.not.present(p_ext)) call system_abort('barostat initialise: p_ext is required for Hoover-Langevin barostat')
+       if (.not.present(p_ext) .and. .not. present(stress_ext)) call system_abort('barostat initialise: p_ext or stress_ext is required for Hoover-Langevin barostat')
+       if (present(p_ext) .and. present(stress_ext)) call system_abort('barostat initialise: p_ext and stress_ext conflict for Hoover-Langevin barostat')
        if (.not.present(gamma_epsilon)) call system_abort('barostat initialise: gamma_epsilon is required for Hoover-Langevin barostat')
        if (gamma_epsilon <= 0.0_dp) call system_abort('barostat initialise: gamma_epsilon must be > 0.0 for Hoover-Langevin')
        if (.not.present(Ndof)) call system_abort('barostat initialise: Ndof is required for Hoover-Langevin barostat')
@@ -152,12 +176,23 @@ contains
 	  if (cell_volume <= 0.0_dp) call system_abort('barostat initialise: cell_volume must be > 0.0 for Hoover-Langevin')
        endif
 
-       this%p_ext = p_ext
+       if (present(p_ext)) then ! S = - p_ext I
+	  this%stress_ext = 0.0_dp
+	  call add_identity(this%stress_ext)
+	  this%stress_ext = -p_ext * this%stress_ext
+       else
+	  this%stress_ext = stress_ext
+       endif
+
+       if (present(hydrostatic_strain)) this%hydrostatic_strain = hydrostatic_strain
+       if (present(diagonal_strain)) this%diagonal_strain = diagonal_strain
+       if (present(finite_strain_formulation)) this%finite_strain_formulation = finite_strain_formulation
+
        this%Ndof = Ndof
        if (present(W_epsilon)) then
 	  this%W_epsilon = W_epsilon
        else
-	  this%W_epsilon = barostat_mass(p_ext, cell_volume, Ndof, gamma_epsilon, T)
+	  this%W_epsilon = barostat_mass(maxval(abs(this%stress_ext)), cell_volume, Ndof, gamma_epsilon, T)
        endif
        use_T = optional_default(-1.0_dp, T)
        if (use_T > 0.0_dp) then
@@ -180,11 +215,15 @@ contains
 
     this%type  = BAROSTAT_NONE  
     this%gamma_epsilon = 0.0_dp
-    this%p_ext     = 0.0_dp
+    this%stress_ext     = 0.0_dp
+    this%hydrostatic_strain     = .true.
+    this%diagonal_strain     = .true.
+    this%finite_strain_formulation     = .false.
     this%Ndof  = 0.0_dp
     this%W_epsilon = 0.0_dp
     this%epsilon_v = 0.0_dp
     this%epsilon_f = 0.0_dp
+    this%epsilon_r = 0.0_dp; call add_identity(this%epsilon_r)
     this%T = -1.0_dp
 
   end subroutine barostat_finalise
@@ -196,22 +235,38 @@ contains
 
     to%type  = from%type      
     to%gamma_epsilon = from%gamma_epsilon 
-    to%p_ext     = from%p_ext
+    to%stress_ext     = from%stress_ext
+    to%hydrostatic_strain     = from%hydrostatic_strain
+    to%diagonal_strain     = from%diagonal_strain
+    to%finite_strain_formulation     = from%finite_strain_formulation
     to%W_epsilon = from%W_epsilon
     to%epsilon_v = from%epsilon_v
     to%epsilon_f = from%epsilon_f
+    to%epsilon_r = from%epsilon_r
     to%T = from%T
 
   end subroutine barostat_assignment
 
-  subroutine barostat_update_barostat(this,p_ext,W_epsilon,T)
+  subroutine barostat_update_barostat(this,p_ext,stress_ext,hydrostatic_strain,diagonal_strain,finite_strain_formulation,W_epsilon,T)
 
     type(barostat),   intent(inout) :: this
     real(dp), optional, intent(in)    :: p_ext
+    real(dp), optional, intent(in)    :: stress_ext(3,3)
+    logical, optional, intent(in)    :: hydrostatic_strain, diagonal_strain
+    logical, optional, intent(in)    :: finite_strain_formulation
     real(dp), optional, intent(in)    :: W_epsilon
     real(dp), optional, intent(in)    :: T
 
-    if( present(p_ext) ) this%p_ext = p_ext
+    if( present(p_ext) .and. present(stress_ext)) call system_abort("barostat_update_barostat: got both p_ext and stress_ext, conflict")
+    if( present(p_ext) ) then
+      this%stress_ext = 0.0_dp
+      call add_identity(this%stress_ext)
+      this%stress_ext = -p_ext * this%stress_ext  
+    endif
+    if( present(stress_ext) ) this%stress_ext = stress_ext
+    if( present(hydrostatic_strain) ) this%hydrostatic_strain = hydrostatic_strain
+    if( present(diagonal_strain) ) this%diagonal_strain = diagonal_strain
+    if( present(finite_strain_formulation) ) this%finite_strain_formulation = finite_strain_formulation
     if( present(W_epsilon) ) this%W_epsilon = W_epsilon
     if( present(T) ) this%T = T
 
@@ -234,7 +289,18 @@ contains
 	  call print('Barostat off',file=file)
 
        case(BAROSTAT_HOOVER_LANGEVIN)
-	  call print('Hoover-Langevin, p_ext = '//round(this%p_ext/GPA,5)//' GPa , gamma_epsilon = '//round(this%gamma_epsilon,5)//' fs^-1, '// &
+	  call print('Hoover-Langevin, stress_ext = '// &
+	  round(this%stress_ext(1,1)/GPA,5)//' '// &
+	  round(this%stress_ext(1,2)/GPA,5)//' '// &
+	  round(this%stress_ext(1,3)/GPA,5)//' '// &
+	  round(this%stress_ext(2,1)/GPA,5)//' '// &
+	  round(this%stress_ext(2,2)/GPA,5)//' '// &
+	  round(this%stress_ext(2,3)/GPA,5)//' '// &
+	  round(this%stress_ext(3,1)/GPA,5)//' '// &
+	  round(this%stress_ext(3,2)/GPA,5)//' '// &
+	  round(this%stress_ext(3,3)/GPA,5)//' GPa , hydrostatic_strain = '//this%hydrostatic_strain//&
+	    ' diagonal_strain = '//this%diagonal_strain//' finite_strain_formulation = '//this%finite_strain_formulation//&
+	    ' gamma_epsilon = '//round(this%gamma_epsilon,5)//' fs^-1, '// &
 	    ' W_epsilon = '//round(this%W_epsilon,5)//' eV/fs, T = '//round(this%T,2)//' K, Ndof = '// round(this%Ndof,1),file=file)
 
     end select
@@ -334,7 +400,7 @@ contains
     real(dp),         intent(in)    :: dt
     real(dp), dimension(3,3), optional, intent(in) :: virial
 
-    real(dp) :: decay, lattice_p(3,3)
+    real(dp) :: vel_decay(3,3), pos_scale(3,3), lattice_p(3,3), F_inv(3,3)
 
     select case(this%type)
 
@@ -344,31 +410,38 @@ contains
        !X
        !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     case(BAROSTAT_HOOVER_LANGEVIN)
-       !TIME_PROPAG_TEX 00
+       !TIME_PROPAG_TEX 00 
+       !TIME_PROPAG_TEX 00 {\color {blue}
        !TIME_PROPAG_TEX 00 before all Verlet and thermostat steps (barostat\_pre\_vel1)
        !TIME_PROPAG_TEX 00
-       !TIME_PROPAG_TEX 00 $$ \epsilon_v = \epsilon_v \exp\left( -(\tau/2) \gamma_\epsilon \right)$$
+       !TIME_PROPAG_TEX 00 $$ \epsilon_v = \epsilon_v \exp\left( -(\tau/2) \gamma_\epsilon \epsilon_r^{-1}\right)^T$$
        !TIME_PROPAG_TEX 00 $$ \epsilon_v = \epsilon_v +  (\tau/2) \epsilon_f/W_\epsilon $$
-       !TIME_PROPAG_TEX 00 $$ v = v \exp \left(  -(\tau/2) (1 + 3/N_d) \epsilon_v \right) $$
-       !TIME_PROPAG_TEX 00 $$ (r,h) = (r,h) \exp\left( (\tau/2) \epsilon_v \right) $$
-       !TIME_PROPAG_TEX 00 
+       !TIME_PROPAG_TEX 00 $$ v = \exp \left(  -(\tau/2) (1 + 3/N_d) \epsilon_v \right) v $$
+       !TIME_PROPAG_TEX 00 $$ (r,h) = \exp\left( (\tau/2) \epsilon_v \right) (r,h) $$
+       !TIME_PROPAG_TEX 00 }
+       !TIME_PROPAG_TEX 00
 
        if (.not. present(virial)) call system_abort("barostat_pre_vel1 needs virial")
 
        ! half step epsilon_v drag 
-       this%epsilon_v = this%epsilon_v*exp(-0.5_dp*dt*this%gamma_epsilon)
+       if (this%finite_strain_formulation) then
+	  call inverse(this%epsilon_r, F_inv)
+	  this%epsilon_v = this%epsilon_v .mult. transpose(matrix_exp(-0.5_dp*dt*this%gamma_epsilon*F_inv))
+       else
+	  this%epsilon_v = this%epsilon_v * exp(-0.5_dp*dt*this%gamma_epsilon)
+       endif
        ! half step epsilon_v force (from last step) parts 
        this%epsilon_v = this%epsilon_v + 0.5_dp*dt*this%epsilon_f/this%W_epsilon
 
        ! half step barostat drag part of v
-       decay = exp(-0.5_dp*dt*((1.0_dp + 3.0_dp/this%Ndof)*this%epsilon_v))
-       at%velo = at%velo*decay
+       vel_decay = matrix_exp(-0.5_dp*dt*((1.0_dp + 3.0_dp/this%Ndof)*this%epsilon_v))
+       at%velo = vel_decay .mult. at%velo
 
        ! half step position affine defomration
+       pos_scale = matrix_exp(0.5_dp*dt*this%epsilon_v)
        lattice_p = at%lattice
-       call set_lattice(at, lattice_p*exp(0.5_dp*dt*this%epsilon_v), scale_positions=.false.)
-       at%pos = at%pos*exp(0.5_dp*dt*this%epsilon_v)
-
+       call set_lattice(at, pos_scale .mult. lattice_p, scale_positions=.false.)
+       at%pos = pos_scale .mult. at%pos
 
     end select
 
@@ -390,22 +463,25 @@ contains
     real(dp),         intent(in)    :: dt
     real(dp), dimension(3,3), optional, intent(in) :: virial
 
-    real(dp) :: lattice_p(3,3)
+    real(dp) :: pos_scale(3,3), lattice_p(3,3)
 
     select case(this%type)
 
       case(BAROSTAT_HOOVER_LANGEVIN)
-       !TIME_PROPAG_TEX 60
+       !TIME_PROPAG_TEX 60 
+       !TIME_PROPAG_TEX 60 {\color {blue}
        !TIME_PROPAG_TEX 60 after Verlet pos step, before force calc (barostat\_post\_pos\_pre\_calc)
        !TIME_PROPAG_TEX 60
-       !TIME_PROPAG_TEX 60 $$ (r,h) = (r,h) \exp\left( (\tau/2) \epsilon_v \right) $$
+       !TIME_PROPAG_TEX 60 $$ (r,h) = \exp\left( (\tau/2) \epsilon_v \right) (r,h) $$
+       !TIME_PROPAG_TEX 60 }
        !TIME_PROPAG_TEX 60 
        if (.not. present(virial)) call system_abort("barostat_post_pos_pre_calc needs virial")
 
         ! half step position affine defomration
+	pos_scale = matrix_exp(0.5_dp*dt*this%epsilon_v)
         lattice_p = at%lattice
-        call set_lattice(at, lattice_p*exp(0.5_dp*dt*this%epsilon_v), scale_positions=.false.)
-        at%pos = at%pos*exp(0.5_dp*dt*this%epsilon_v)
+        call set_lattice(at, pos_scale .mult. lattice_p, scale_positions=.false.)
+        at%pos = pos_scale .mult. at%pos
 
     end select
 
@@ -427,39 +503,99 @@ contains
     real(dp),         intent(in)    :: dt
     real(dp), dimension(3,3), optional, intent(in) :: virial
 
-    real(dp) :: decay, volume_p, rand_f_cell
+    integer :: i, j
+    real(dp) :: vel_decay(3,3), volume_p, rand_f_cell(3,3), kinetic_virial(3,3), F_det, F_T_inv(3,3), F_inv(3,3), epsilon_f_hydrostatic
+    real(dp) :: rand_f_scale
 
     select case(this%type)
 
     case(BAROSTAT_HOOVER_LANGEVIN)
        !TIME_PROPAG_TEX 100
+       !TIME_PROPAG_TEX 100 {\color {blue}
        !TIME_PROPAG_TEX 100 after all Verlet and thermostat steps (barostat\_post\_vel2)
        !TIME_PROPAG_TEX 100
-       !TIME_PROPAG_TEX 100 $$ v = v \exp \left(  -(\tau/2) (1 + 3/N_d) \epsilon_v \right) $$
-       !TIME_PROPAG_TEX 100 $$ \epsilon_f = \left( Tr[\mathrm{vir}] + \sum_i m_i \left|v_i\right|^2 - 3 V P\right) + 3/N_d \sum_i m_i \left| v_i \right|^2 + \sqrt{2 k_B T \gamma_\epsilon W_\epsilon/\tau} r $$
+       !TIME_PROPAG_TEX 100 $$ v = \exp \left(  -(\tau/2) (1 + 3/N_d) \epsilon_v \right) v $$
+       !TIME_PROPAG_TEX 100 $$ \epsilon_r = \epsilon_r + \tau \epsilon_v $$
+       !TIME_PROPAG_TEX 100 $r.v.$ is symmetric matrix, normal distribution entries, variance 3 on diagonal and 6 off diagonal
+       !TIME_PROPAG_TEX 100 \begin{eqnarray*}
+       !TIME_PROPAG_TEX 100 \epsilon_f  & = & \left[ 3 \left( \mathrm{vir} + \sum_i m_i v_i \otimes v_i + 
+       !TIME_PROPAG_TEX 100                                 3 V \epsilon_r \sigma \epsilon_r^T / \left|\epsilon_r\right|  + 
+       !TIME_PROPAG_TEX 100                  3/N_d \sum_i m_i  v_i \otimes v_i  \right) + \right. \\
+       !TIME_PROPAG_TEX 100              &  & s \sqrt{2 k_B T \gamma_\epsilon W_\epsilon/\tau} r.v. \bigg] F^{-T}
+       !TIME_PROPAG_TEX 100 \end{eqnarray*}
        !TIME_PROPAG_TEX 100 $$ \epsilon_v = \epsilon_v +  (\tau/2) \epsilon_f/W_\epsilon $$
-       !TIME_PROPAG_TEX 100 $$ \epsilon_v = \epsilon_v \exp\left( -(\tau/2) \gamma_\epsilon \right)$$
-       !TIME_PROPAG_TEX 100 
+       !TIME_PROPAG_TEX 100 $$ \epsilon_v = \exp\left( -(\tau/2) \gamma_\epsilon \right) \epsilon_v $$
+       !TIME_PROPAG_TEX 100  }
+       !TIME_PROPAG_TEX 100
        if (.not. present(virial)) call system_abort("barostat_pre_vel1 needs virial")
 
        call system_resync_rng()
 
        !Decay the velocities for dt/2 again barostat part
-       decay = exp(-0.5_dp*dt*((1.0_dp + 3.0_dp/this%Ndof)*this%epsilon_v))
-       at%velo(:,:) = at%velo(:,:)*decay
+       vel_decay = matrix_exp(-0.5_dp*dt*((1.0_dp + 3.0_dp/this%Ndof)*this%epsilon_v))
+       at%velo(:,:) = vel_decay .mult. at%velo(:,:)
+
+       this%epsilon_r = this%epsilon_r + dt*this%epsilon_v
 
        ! calc epsilon force
        volume_p = cell_volume(at)
-       rand_f_cell = sqrt(2.0_dp*BOLTZMANN_K*this%T*this%gamma_epsilon*this%W_epsilon/dt)*ran_normal()
-       this%epsilon_f = (trace(virial)+sum(at%mass*sum(at%velo**2,dim=1))-3.0_dp*volume_p*this%p_ext) + &
-	  3.0_dp/this%Ndof*sum(at%mass*sum(at%velo**2,dim=1)) + rand_f_cell
+       rand_f_cell = 0.0_dp
+       do i=1, 3
+       do j=i, 3
+	  if (i == j) then
+	     rand_f_scale = 3.0_dp
+	  else
+	     ! empirical - maybe have something to do with fact that matrix is symmetric so there are
+	     ! fewer degrees of freedom than first appears?
+	     rand_f_scale = 6.0_dp
+	  endif
+	  rand_f_cell(i,j) = sqrt(rand_f_scale)*sqrt(2.0_dp*BOLTZMANN_K*this%T*this%gamma_epsilon*this%W_epsilon/dt)*ran_normal()
+       end do
+       end do
+
+       ! symmetrize rand_f_cell
+       rand_f_cell(2,1) = rand_f_cell(1,2)
+       rand_f_cell(3,1) = rand_f_cell(1,3)
+       rand_f_cell(3,2) = rand_f_cell(2,3)
+
+       kinetic_virial = matmul(at%velo*spread(at%mass,dim=1,ncopies=3),transpose(at%velo))
+       if (this%finite_strain_formulation) then
+	  F_det = matrix3x3_det(this%epsilon_r)
+	  call inverse(transpose(this%epsilon_r), F_T_inv)
+
+	  this%epsilon_f = (3.0_dp*(virial+kinetic_virial+volume_p*(this%epsilon_r .mult. this%stress_ext .mult. transpose(this%epsilon_r))/F_det + &
+				    3.0_dp/this%Ndof*kinetic_virial) + rand_f_cell) .mult. F_T_inv
+       else
+	  this%epsilon_f = 3.0_dp*(virial+kinetic_virial+volume_p*this%stress_ext + &
+				   3.0_dp/this%Ndof*kinetic_virial) + rand_f_cell
+       endif
+
+       if (this%diagonal_strain .or. this%hydrostatic_strain) then
+	 this%epsilon_f(1,2) = 0.0_dp
+	 this%epsilon_f(1,3) = 0.0_dp
+	 this%epsilon_f(2,3) = 0.0_dp
+	 this%epsilon_f(2,1) = 0.0_dp
+	 this%epsilon_f(3,1) = 0.0_dp
+	 this%epsilon_f(3,2) = 0.0_dp
+	 if (this%hydrostatic_strain) then
+	   epsilon_f_hydrostatic = (this%epsilon_f(1,1)+ this%epsilon_f(2,2)+ this%epsilon_f(3,3)) / 3.0_dp
+	   this%epsilon_f(1,1) = epsilon_f_hydrostatic
+	   this%epsilon_f(2,2) = epsilon_f_hydrostatic
+	   this%epsilon_f(3,3) = epsilon_f_hydrostatic
+	 endif
+       endif
 
        ! half step with epsilon force
        this%epsilon_v = this%epsilon_v + 0.5_dp*dt*this%epsilon_f/this%W_epsilon
        ! half step with epsilon drag
-       this%epsilon_v = this%epsilon_v*exp(-0.5_dp*dt*this%gamma_epsilon)
+       if (this%finite_strain_formulation) then
+	  call inverse(this%epsilon_r, F_inv)
+	  this%epsilon_v = this%epsilon_v .mult. transpose(matrix_exp(-0.5_dp*dt*this%gamma_epsilon*F_inv))
+       else
+	  this%epsilon_v = this%epsilon_v * exp(-0.5_dp*dt*this%gamma_epsilon)
+       endif
 
-       call print("BAROSTAT_KE "//(0.5_dp*this%W_epsilon*this%epsilon_v**2))
+       call print("BAROSTAT_KE "//barostat_KE(this))
 
     end select
 
@@ -471,7 +607,7 @@ contains
 
     select case(this%type)
       case(BAROSTAT_HOOVER_LANGEVIN)
-	 barostat_KE = 0.5_dp*this%W_epsilon*this%epsilon_v**2
+	 barostat_KE = 0.5_dp*this%W_epsilon*sum(this%epsilon_v*transpose(this%epsilon_v))
     end select
   end function barostat_KE
 
