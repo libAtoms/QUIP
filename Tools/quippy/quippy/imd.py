b08821754b2b9f184008bc68c64092abc9cbc42b
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-03-07 11:32:13 +0000

    New read-time arguments to set vacuum and fix atoms
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@9899 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/quippy/imd.py
+++ Tools/quippy/quippy/imd.py
@@ -19,15 +19,18 @@
 from quippy.atoms import Atoms, AtomsReaders, AtomsWriters
 from quippy.units import BOHR
 from farray import *
-import sys, numpy
+import sys
+import numpy as np
 
 __all__ = ['IMDReader']
 
 class IMDReader:
 
-    def __init__(self, filename, z=14):
+    def __init__(self, filename, z=14, fix_tags=None, vacuum=None):
         self.filename = filename
         self.z = z
+        self.fix_tags = fix_tags
+        self.vacuum = vacuum
 
     def __len__(self):
         return 1
@@ -47,18 +50,24 @@ class IMDReader:
         header = [f.readline() for i in range(8)]
 
         # Rest of file is data array
-        data = numpy.loadtxt(f)
+        data = np.loadtxt(f)
 
         lattice = fzeros((3,3))
         for i in [1,2,3]:
             lattice[:,i] = [float(x) for x in header[i+1].split()[1:]]
+        if self.vacuum is not None:
+            lattice += np.diag(self.vacuum)
         at = Atoms(n=len(data), lattice=lattice)
         at.pos[...] = data[:,3:6].T
         at.set_atoms(self.z)
-        at.add_property('tag', data[:,1])
+        at.add_property('tag', data[:,1].astype(int))
         at.add_property('mass', data[:,2])
         at.add_property('velo', data[:,6:9].T)
         at.add_property('epot', data[:,9])
+        if self.fix_tags is not None:
+            at.add_property('move_mask', 1)
+            for tag in self.fix_tags:
+                at.move_mask[at.tag == tag] = 0
         if opened:
             f.close()
 
