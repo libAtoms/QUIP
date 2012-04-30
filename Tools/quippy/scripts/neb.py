47192974dc9a533eb6ebc17773e8e9083411f2ac
jrk33 <jrk33@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-04-30 14:16:24 +0000

    Fix typos
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@10128 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/scripts/neb.py
+++ Tools/quippy/scripts/neb.py
@@ -39,45 +39,45 @@ def write_band(neb, filename):
 
 p = optparse.OptionParser(usage='%prog [OPTIONS] INFILE')
 p.add_option('-R', '--refine', action='store', type='int', help='Refine NEB pathway by this many steps')
-p.add_option('-I', '--init-args', action='store', help='QUIP Potential init_args string')
+p.add_option('-I', '--init-args', action='store', help='QUIP Potential init_args string', default="IP SW")
 p.add_option('-p', '--param-file', action='store', help='Read QUIP potential XML from this file.', default='crack.xml')
 p.add_option('-c', '--climb', action='store_true', help='If true, do climbing image NEB', default=False)
 p.add_option('-k', action='store', type='float', help='Spring constant for NEB', default=1.0)
-p.add_option('-O', '--optimizer', action='store', help='Optimizer to use: currently FIRE or MDMin')
+p.add_option('-O', '--optimizer', action='store', help='Optimizer to use: currently FIRE or MDMin', default="FIRE")
 p.add_option('-w', '--write-traj', action='store', type='int', help='Write trajectory every this many steps')
 p.add_option('-m', '--minim', action='store_true', help="Minimize start and end points before starting NEB")
 p.add_option('--minim-method', action='store', help="Method to use for initial minimisations: sd, cg, cg_n or fire", default="cg_n")
-p.add_option('--minim-tol', action='store', type='float', help="Tolerance for initial minimisations - df^2, (eV/A)^2", default=1e-3
+p.add_option('--minim-tol', action='store', type='float', help="Tolerance for initial minimisations - df^2, (eV/A)^2", default=1e-3)
 p.add_option('--minim-max-steps', action='store', type='int', help="Maximum number of steps for initial minimisations", default=100)
-p.add_option('--minim-do-pos', action='store', type='bool', help="If true, relax positions in initial minimisations", default=True)
-p.add_option('--minim-do-lat', action='store', type='bool', help="If true, relax lattice in initial minimisations", default=False)
+p.add_option('--minim-no-do-pos', action='store_true', help="If true, relax positions in initial minimisations")
+p.add_option('--minim-do-lat', action='store_true', help="If true, relax lattice in initial minimisations")
 
 opt, args = p.parse_args()
 
 try:
-    infile = args,
+    infile, = args
 except:
-    p.error()
+    p.error('Exactly one input file must be given, got %r' % args)
     
 basename = os.path.splitext(infile)[0]
 images = AtomsList(infile)
 
 if opt.minim:
     pot = Potential(opt.init_args, param_filename=opt.param_file)
-    for at in (images[0], images[-1])
+    for at in (images[0], images[-1]):
         at.set_cutoff(pot.cutoff() + 1.0)
         pot.minim(at,
                   opt.minim_method,
                   opt.minim_tol,
                   opt.minim_max_steps,
-                  do_pos=opt.minim_do_pos,
+                  do_pos=not opt.minim_no_do_pos,
                   do_lat=opt.minim_do_lat)
 
 
 images = list(images) # FIXME -- unclear why we have to convert from AtomsList to list
 
-have_constraint = hasattr(images[0], 'move_mask'):
-if have_constaint:
+have_constraint = hasattr(images[0], 'move_mask')
+if have_constraint:
     constraint = FixAtoms(mask=np.logical_not(images[0].move_mask.view(np.ndarray)))
 
 neb = SingleCalculatorNEB(images, climb=opt.climb, k=opt.k)
