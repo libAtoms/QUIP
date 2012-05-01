24f5aed5749cb25094676cface4ffe243caa917a
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-05-01 14:45:50 +0000

    Fix extra printing of energy in quippy vasp OUTCAR reader, and use optparse for options to msd.py
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@10148 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/scripts/msd.py
+++ Tools/quippy/scripts/msd.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 from quippy import *
-import sys;
+import sys, optparse;
 
 ################################################################################
 
-if (len(sys.argv) != 4 and len(sys.argv) != 5):
-   sys.stderr.write("Usage: %s infile outfile min_step [ mask ]\n" % sys.argv[0])
-   sys.exit(1)
+p = optparse.OptionParser(usage='%prog [ options ]')
+p.add_option('-i','--infile', action='store', help="""input file""", default="stdin")
+p.add_option('-o','--outfile', action='store', help="""output file""", default="stdout")
+p.add_option('-s','--min_step', action='store', help="""frame to start from""", type='int', default=0)
+p.add_option('-m','--mask', action='store', help="""mask of atoms to include (must resolve to python logical array)""", default="")
+p.add_option('-f','--fake_smooth_pos_mixing', action='store', help="""mixing coefficient for fake smooth pos""", type='float', default=-1.0)
 
-ar=AtomsReader(sys.argv[1])
-aw=AtomsWriter(sys.argv[2])
-msd_min_step=int(sys.argv[3])
-if (len(sys.argv) == 5):
-   mask_str = sys.argv[4]
-else:
-   mask_str = ""
+opt, args = p.parse_args()
+
+ar = AtomsReader(opt.infile)
+aw = AtomsWriter(opt.outfile)
 
 step=0
 for at in ar:
@@ -23,10 +23,13 @@ for at in ar:
 
    at.undo_pbc_jumps(persistent=False)
    at.undo_com_motion(persistent=False)
-   if (mask_str != ""):
-      at.calc_msd(mask=eval(mask_str), reset_msd = (step <= msd_min_step), persistent=False)
+   if (opt.mask != ""):
+      at.calc_msd(mask=eval(opt.mask), reset_msd = (step <= opt.min_step), persistent=False)
    else:
-      at.calc_msd(reset_msd = (step <= msd_min_step), persistent=False)
+      at.calc_msd(reset_msd = (step <= opt.min_step), persistent=False)
+
+   if (opt.fake_smooth_pos_mixing > 0.0):
+      at.fake_smooth_pos(opt.fake_smooth_pos_mixing, persistent=False)
 
    aw.write(at)
 
@@ -35,4 +38,5 @@ for at in ar:
    elif ((step % 10) == 0):
       sys.stderr.write(".")
 
-sys.stderr.write("\n")
+if (step >= 10):
+   sys.stderr.write("\n")
