62ff2e8b28103610a7c751531434da493fb5cd9c
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-07-12 15:52:49 +0000

    Some docs for gp_simple.f95
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@10539 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- libAtoms/gp_simple.f90
+++ libAtoms/gp_simple.f90
@@ -1,4 +1,36 @@
+! H0 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+! H0 X
+! H0 X   libAtoms+QUIP: atomistic simulation library
+! H0 X
+! H0 X   Portions of this code were written by
+! H0 X     Albert Bartok-Partay, Silvia Cereda, Gabor Csanyi, James Kermode,
+! H0 X     Ivan Solt, Wojciech Szlachta, Csilla Varnai, Steven Winfield.
+! H0 X
+! H0 X   Copyright 2006-2010.
+! H0 X
+! H0 X   These portions of the source code are released under the GNU General
+! H0 X   Public License, version 2, http://www.gnu.org/copyleft/gpl.html
+! H0 X
+! H0 X   If you would like to license the source code under different terms,
+! H0 X   please contact Gabor Csanyi, gabor@csanyi.net
+! H0 X
+! H0 X   Portions of this code were written by Noam Bernstein as part of
+! H0 X   his employment for the U.S. Government, and are not subject
+! H0 X   to copyright in the USA.
+! H0 X
+! H0 X
+! H0 X   When using this software, please cite the following reference:
+! H0 X
+! H0 X   http://www.libatoms.org
+! H0 X
+! H0 X  Additional contributions by
+! H0 X    Alessio Comisso, Chiara Gattinoni, and Gianpietro Moras
+! H0 X
+! H0 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 #include "error.inc"
+
+!% A simple Gaussian process module for 1-D functions learned from samples of
+!% their values
 
 module gp_simple_module
 use system_module
@@ -13,18 +45,22 @@ type gp_simple
    real(dp), allocatable :: Cmat(:,:), f_r(:), Cmat_inv_v(:), k(:), Cmat_inv_k(:)
 end type gp_simple
 
+!% initialise (and teach) a gp_simple
 interface initialise
    module procedure gp_simple_initialise
 end interface initialise
 
+!% finalise and deallocate a gp_simple
 interface finalise
    module procedure gp_simple_finalise
 end interface finalise
 
+!% predict a function value from a gp
 interface predict
    module procedure predict_r, predict_rr
 end interface predict
 
+!% predict a function variance from a gp
 interface predict_var
    module procedure predict_var_r, predict_var_rr
 end interface predict_var
@@ -36,10 +72,10 @@ end interface ff_kernel
 contains
 
 subroutine gp_simple_initialise(self, f_r, f_v, f_n, len_scale, f_var, error)
-   type(gp_simple), intent(inout) :: self
-   real(dp), intent(in) :: f_r(:), f_v(:), f_n(:)
-   real(dp), intent(in) :: len_scale, f_var
-   integer, optional, intent(out) :: error
+   type(gp_simple), intent(inout) :: self !% object to store GP
+   real(dp), intent(in) :: f_r(:), f_v(:), f_n(:) !% arrays of function positions, values, noise 
+   real(dp), intent(in) :: len_scale, f_var !% length scale and variance prior for GP
+   integer, optional, intent(out) :: error !% error status
 
    integer :: i
 
@@ -81,7 +117,7 @@ subroutine gp_simple_initialise(self, f_r, f_v, f_n, len_scale, f_var, error)
 end subroutine gp_simple_initialise
 
 subroutine gp_simple_finalise(self)
-   type(gp_simple), intent(inout) :: self
+   type(gp_simple), intent(inout) :: self !% object for GP
 
    self%l2 = 0.0_dp
    self%f_var = 0.0_dp
@@ -95,8 +131,8 @@ subroutine gp_simple_finalise(self)
 end subroutine gp_simple_finalise
 
 function predict_r(self, r)
-   type(gp_simple), intent(inout) :: self
-   real(dp), intent(in) :: r
+   type(gp_simple), intent(inout) :: self !% object for GP
+   real(dp), intent(in) :: r !% position at which to predict value
    real(dp) :: predict_r
 
    self%k(:) = ff_kernel(r, self%f_r(:), self%f_var, self%l2)
