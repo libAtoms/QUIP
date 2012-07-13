7fd88e5071ab80c1712f940c0f0f022ce5ebb01e
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-07-13 13:19:18 +0000

    Try to use LA_Matrix in gp_simple.  Broken, but need to check in to rename stuff
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@10544 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- libAtoms/gp_simple.f90
+++ libAtoms/gp_simple.f90
@@ -37,12 +37,14 @@ use system_module
 use linearalgebra_module
 implicit none
 
-public :: gp_simple, initialise, finalise, predict, predict_var
+public :: gp_simple, initialise, finalise, predict, predict_var, predict_grad
 
 type gp_simple
    real(dp) :: l2, f_var
    integer  :: n_f, n_teach
-   real(dp), allocatable :: Cmat(:,:), f_r(:), Cmat_inv_v(:), k(:), Cmat_inv_k(:)
+   type(LA_Matrix), allocatable :: Cmat(:)
+   real(dp), allocatable :: f_r(:), Cmat_inv_v(:), k(:), Cmat_inv_k(:)
+   logical :: initialised = .false.
 end type gp_simple
 
 !% initialise (and teach) a gp_simple
@@ -60,6 +62,11 @@ interface predict
    module procedure predict_r, predict_rr
 end interface predict
 
+!% predict a gradient of a function value from a gp
+interface predict_grad
+   module procedure predict_grad_r
+end interface predict_grad
+
 !% predict a function variance from a gp
 interface predict_var
    module procedure predict_var_r, predict_var_rr
@@ -69,6 +76,10 @@ interface ff_kernel
    module procedure ff_kernel_r_r, ff_kernel_r_rr
 end interface ff_kernel
 
+interface df_kernel
+   module procedure df_kernel_r_r, df_kernel_r_rr
+end interface df_kernel
+
 contains
 
 subroutine gp_simple_initialise(self, f_r, f_v, f_n, len_scale, f_var, error)
@@ -77,8 +88,10 @@ subroutine gp_simple_initialise(self, f_r, f_v, f_n, len_scale, f_var, error)
    real(dp), intent(in) :: len_scale, f_var !% length scale and variance prior for GP
    integer, optional, intent(out) :: error !% error status
 
+   real(dp), allocatable :: Cmat(:,:)
    integer :: i
 
+print *, "init 00"
    INIT_ERROR(error)
 
    call finalise(self)
@@ -102,38 +115,56 @@ subroutine gp_simple_initialise(self, f_r, f_v, f_n, len_scale, f_var, error)
 
    self%n_teach = self%n_f
 
-   allocate(self%Cmat(self%n_teach, self%n_teach))
-
+   allocate(Cmat(self%n_teach, self%n_teach))
    do i=1, self%n_f
-      self%Cmat(i,:) = ff_kernel(self%f_r(i), self%f_r(:), self%f_var, self%l2)
-      self%Cmat(i,i) = self%Cmat(i,i) + f_n(i)
+      Cmat(i,:) = ff_kernel(self%f_r(i), self%f_r(:), self%f_var, self%l2)
+      Cmat(i,i) = Cmat(i,i) + f_n(i)
    end do
+   allocate(self%Cmat(1))
+   call initialise(self%Cmat(1), Cmat)
+   deallocate(Cmat)
 
    allocate(self%Cmat_inv_v(self%n_teach))
-   call symmetric_linear_solve(self%Cmat, f_v, self%Cmat_inv_v)
+   call Matrix_QR_solve(self%Cmat(1), f_v, self%Cmat_inv_v)
 
    allocate(self%k(self%n_teach))
    allocate(self%Cmat_inv_k(self%n_teach))
+
+   self%initialised = .true.
+print *, "init 100"
 end subroutine gp_simple_initialise
 
 subroutine gp_simple_finalise(self)
    type(gp_simple), intent(inout) :: self !% object for GP
+print *, "fin 00"
 
+   if (self%initialised) then
+      if (allocated(self%f_r)) deallocate(self%f_r)
+      if (allocated(self%Cmat_inv_v)) deallocate(self%Cmat_inv_v)
+      if (allocated(self%k)) deallocate(self%k)
+      if (allocated(self%Cmat_inv_k)) deallocate(self%Cmat_inv_k)
+      if (allocated(self%Cmat)) then
+	 call finalise(self%Cmat(1))
+	 deallocate(self%Cmat)
+      endif
+   endif
    self%l2 = 0.0_dp
    self%f_var = 0.0_dp
    self%n_f = 0
    self%n_teach = 0
-   if (allocated(self%Cmat)) deallocate(self%Cmat)
-   if (allocated(self%f_r)) deallocate(self%f_r)
-   if (allocated(self%Cmat_inv_v)) deallocate(self%Cmat_inv_v)
-   if (allocated(self%k)) deallocate(self%k)
-   if (allocated(self%Cmat_inv_k)) deallocate(self%Cmat_inv_k)
+   self%initialised = .false.
+print *, "fin 100"
 end subroutine gp_simple_finalise
 
 function predict_r(self, r)
    type(gp_simple), intent(inout) :: self !% object for GP
    real(dp), intent(in) :: r !% position at which to predict value
    real(dp) :: predict_r
+
+   if (.not. self%initialised) then
+      predict_r = 0.0_dp
+      return
+   endif
 
    self%k(:) = ff_kernel(r, self%f_r(:), self%f_var, self%l2)
    predict_r = dot_product(self%k, self%Cmat_inv_v)
@@ -146,6 +177,11 @@ function predict_rr(self, r)
 
    integer :: i
 
+   if (.not. self%initialised) then
+      predict_rr = 0.0_dp
+      return
+   endif
+
    do i=1, size(r)
       self%k(:) = ff_kernel(r(i), self%f_r(:), self%f_var, self%l2)
       predict_rr(i) = dot_product(self%k, self%Cmat_inv_v)
@@ -157,8 +193,13 @@ function predict_var_r(self, r)
    real(dp), intent(in) :: r
    real(dp) :: predict_var_r
 
+   if (.not. self%initialised) then
+      predict_var_r = 0.0_dp
+      return
+   endif
+
    self%k(:) = ff_kernel(r, self%f_r(:), self%f_var, self%l2)
-   call symmetric_linear_solve(self%Cmat, self%k, self%Cmat_inv_k)
+   call Matrix_QR_Solve(self%Cmat(1), self%k, self%Cmat_inv_k)
    predict_var_r = self%f_var - dot_product(self%k, self%Cmat_inv_k)
 end function predict_var_r
 
@@ -169,27 +210,59 @@ function predict_var_rr(self, r)
 
    integer :: i
 
+   if (.not. self%initialised) then
+      predict_var_rr = 0.0_dp
+      return
+   endif
+
    do i=1, size(r)
       self%k(:) = ff_kernel(r(i), self%f_r(:), self%f_var, self%l2)
-      call symmetric_linear_solve(self%Cmat, self%k, self%Cmat_inv_k)
+      call Matrix_QR_Solve(self%Cmat(1), self%k, self%Cmat_inv_k)
       predict_var_rr(i) = self%f_var - dot_product(self%Cmat_inv_k, self%Cmat_inv_k)
    end do
 end function predict_var_rr
 
+function predict_grad_r(self, r)
+   type(gp_simple), intent(inout) :: self !% object for GP
+   real(dp), intent(in) :: r !% position at which to predict value
+   real(dp) :: predict_grad_r
+
+   if (.not. self%initialised) then
+      predict_grad_r = 0.0_dp
+      return
+   endif
+
+   self%k(:) = df_kernel(r, self%f_r(:), self%f_var, self%l2)
+   predict_grad_r = dot_product(self%k, self%Cmat_inv_v)
+end function predict_grad_r
 
 
 function ff_kernel_r_r(x1, x2, f_var, l2)
    real(dp), intent(in) :: x1, x2, f_var, l2
    real(dp) :: ff_kernel_r_r
 
-   ff_kernel_r_r = f_var*exp(-0.5_dp*(x2-x1)**2/l2)
+   ff_kernel_r_r = f_var*exp(-0.5_dp*((x2-x1)**2)/l2)
 end function ff_kernel_r_r
 
 function ff_kernel_r_rr(x1, x2, f_var, l2)
    real(dp), intent(in) :: x1, x2(:), f_var, l2
    real(dp) :: ff_kernel_r_rr(size(x2))
 
-   ff_kernel_r_rr(:) = f_var*exp(-0.5_dp*(x2(:)-x1)**2/l2)
+   ff_kernel_r_rr(:) = f_var*exp(-0.5_dp*((x2(:)-x1)**2)/l2)
 end function ff_kernel_r_rr
+
+function df_kernel_r_r(x1, x2, f_var, l2)
+   real(dp), intent(in) :: x1, x2, f_var, l2
+   real(dp) :: df_kernel_r_r
+
+   df_kernel_r_r = (x2-x1)/l2 * ff_kernel(x1, x2, f_var, l2)
+end function df_kernel_r_r
+
+function df_kernel_r_rr(x1, x2, f_var, l2)
+   real(dp), intent(in) :: x1, x2(:), f_var, l2
+   real(dp) :: df_kernel_r_rr(size(x2))
+
+   df_kernel_r_rr(:) = (x2(:)-x1)/l2 * ff_kernel(x1, x2, f_var, l2)
+end function df_kernel_r_rr
 
 end module gp_simple_module
