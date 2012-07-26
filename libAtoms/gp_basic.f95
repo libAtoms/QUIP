492c818a67cd71f820c647d972e3b82a6d814959
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2012-07-26 17:53:19 +0000

    Add learning from gradients to gp_basic
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@10633 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- libAtoms/gp_basic.f95
+++ libAtoms/gp_basic.f95
@@ -38,12 +38,13 @@ use linearalgebra_module
 implicit none
 
 public :: gp_basic, initialise, finalise, f_predict, f_predict_var, f_predict_grad
+public :: SE_kernel_r_rr
 
 type gp_basic
    real(dp) :: l2, f_var
-   integer  :: n_f, n_teach
+   integer  :: n_f, n_g, n_teach
    type(LA_Matrix), allocatable :: Cmat(:)
-   real(dp), allocatable :: f_r(:), Cmat_inv_v(:), k(:), Cmat_inv_k(:)
+   real(dp), allocatable :: f_r(:), g_r(:), Cmat_inv_v(:), k(:), Cmat_inv_k(:)
    logical :: initialised = .false.
 end type gp_basic
 
@@ -59,7 +60,7 @@ end interface finalise
 
 !% f_predict a function value from a gp
 interface f_predict
-   module procedure f_predict_r, f_predict_rr
+   module procedure f_predict_r
 end interface f_predict
 
 !% f_predict a gradient of a function value from a gp
@@ -69,35 +70,33 @@ end interface f_predict_grad
 
 !% f_predict a function variance from a gp
 interface f_predict_var
-   module procedure f_predict_var_r, f_predict_var_rr
+   module procedure f_predict_var_r
 end interface f_predict_var
-
-interface ff_kernel
-   module procedure ff_kernel_r_r, ff_kernel_r_rr
-end interface ff_kernel
-
-interface df_kernel
-   module procedure df_kernel_r_r, df_kernel_r_rr
-end interface df_kernel
 
 contains
 
-subroutine gp_basic_initialise(self, f_r, f_v, f_n, len_scale, f_var, error)
+subroutine gp_basic_initialise(self, len_scale, f_var, kernel, f_r, f_v, f_n, g_r, g_v, g_n, sparse_set, error)
    type(gp_basic), intent(inout) :: self !% object to store GP
-   real(dp), intent(in) :: f_r(:), f_v(:), f_n(:) !% arrays of function positions, values, noise 
    real(dp), intent(in) :: len_scale, f_var !% length scale and variance prior for GP
+   interface
+      subroutine kernel(x1, x2, f_var, l2, f_f, g_f, f_g, g_g)
+	 use system_module, only : dp
+	 real(dp), intent(in) :: x1, x2(:), f_var, l2
+	 real(dp), optional :: f_f(:), g_f(:), f_g(:), g_g(:)
+      end subroutine kernel
+   end interface
+   real(dp), optional, intent(in) :: f_r(:), f_v(:), f_n(:) !% arrays of function positions, values, noise 
+   real(dp), optional, intent(in) :: g_r(:), g_v(:), g_n(:) !% arrays of function gradient positions, values, noise 
+   integer, optional, intent(in) :: sparse_set(:) !% set of points to use for sparsifcation
    integer, optional, intent(out) :: error !% error status
 
-   real(dp), allocatable :: Cmat(:,:)
-   integer :: i
+   real(dp), allocatable :: Cmat(:,:), all_v(:)
+   integer :: i, i_glob
 
    INIT_ERROR(error)
 
    call finalise(self)
 
-   if (size(f_r) /= size(f_v) .or. size(f_r) /= size(f_n)) then
-      RAISE_ERROR("size(f_r)="//size(f_r)//" size(f_v)="//size(f_v)//" size(f_n)="//size(f_n)//" not all equal", error)
-   endif
    if (len_scale <= 0.0_dp) then
       RAISE_ERROR("invalid len_scale="//len_scale, error)
    endif
@@ -105,26 +104,85 @@ subroutine gp_basic_initialise(self, f_r, f_v, f_n, len_scale, f_var, error)
       RAISE_ERROR("invalid f_var="//f_var, error)
    endif
 
+   i = count( (/ present(f_r), present(f_v), present(f_n) /) )
+   if (i /= 0 .and. i /= 3) then
+      RAISE_ERROR("got something other than all or none of f_r, f_v, f_n", error)
+   endif
+   i = count( (/ present(g_r), present(g_v), present(g_n) /) )
+   if (i /= 0 .and. i /= 3) then
+      RAISE_ERROR("got something other than all or none of g_r, g_v, g_n", error)
+   endif
+
+   if (present(f_r)) then
+      if (size(f_r) /= size(f_v) .or. size(f_r) /= size(f_n)) then
+	 RAISE_ERROR("size(f_r)="//size(f_r)//" size(f_v)="//size(f_v)//" size(f_n)="//size(f_n)//" not all equal", error)
+      endif
+   endif
+   if (present(g_r)) then
+      if (size(g_r) /= size(g_v) .or. size(g_r) /= size(g_n)) then
+	 RAISE_ERROR("size(g_r)="//size(g_r)//" size(g_v)="//size(g_v)//" size(g_n)="//size(g_n)//" not all equal", error)
+      endif
+   endif
+
    self%l2 = len_scale**2
    self%f_var = f_var
 
-   self%n_f = size(f_r)
-   allocate(self%f_r(self%n_f))
-   self%f_r(:) = f_r(:)
+   if (present(sparse_set)) then
+      RAISE_ERROR("no sparsification implemented yet", error)
+   endif
 
-   self%n_teach = self%n_f
+   if (present(f_r)) then
+      self%n_f = size(f_r)
+      allocate(self%f_r(self%n_f))
+      self%f_r(:) = f_r(:)
+   else
+      self%n_f = 0
+   endif
+   if (present(g_r)) then
+      self%n_g = size(g_r)
+      allocate(self%g_r(self%n_g))
+      self%g_r(:) = g_r(:)
+   else
+      self%n_g = 0
+   endif
+
+   if (self%n_f + self%n_g == 0) then
+      RAISE_ERROR("no teaching data provided", error)
+   endif
+
+   self%n_teach = self%n_f + self%n_g
 
    allocate(Cmat(self%n_teach, self%n_teach))
    do i=1, self%n_f
-      Cmat(i,:) = ff_kernel(self%f_r(i), self%f_r(:), self%f_var, self%l2)
+      ! f on f
+      call kernel(self%f_r(i), self%f_r(:), self%f_var, self%l2, f_f=Cmat(i,1:self%n_f))
       Cmat(i,i) = Cmat(i,i) + f_n(i)
+      if (self%n_g > 0) then
+	 ! f on g
+	 call kernel(self%f_r(i), self%g_r(:), self%f_var, self%l2, f_g=Cmat(i,self%n_f+1:self%n_f+self%n_g))
+      endif
    end do
+   do i=1, self%n_g
+      i_glob = self%n_f + i
+      ! g on g
+      call kernel(self%g_r(i), self%g_r(:), self%f_var, self%l2, g_g=Cmat(i_glob,self%n_f+1:self%n_f+self%n_g))
+      Cmat(i_glob,i_glob) = Cmat(i_glob,i_glob) + g_n(i)
+      if (self%n_f > 0) then
+	 ! g on f
+	 call kernel(self%g_r(i), self%f_r(:), self%f_var, self%l2, g_f=Cmat(i_glob,1:self%n_f))
+      endif
+   end do
+
    allocate(self%Cmat(1))
    call initialise(self%Cmat(1), Cmat)
    deallocate(Cmat)
 
    allocate(self%Cmat_inv_v(self%n_teach))
-   call Matrix_QR_solve(self%Cmat(1), f_v, self%Cmat_inv_v)
+   allocate(all_v(self%n_teach))
+   if (self%n_f > 0) all_v(1:self%n_f) = f_v(:)
+   if (self%n_g > 0) all_v(self%n_f+1:self%n_f+self%n_g) = g_v(:)
+   call Matrix_QR_solve(self%Cmat(1), all_v, self%Cmat_inv_v)
+   deallocate(all_v)
 
    allocate(self%k(self%n_teach))
    allocate(self%Cmat_inv_k(self%n_teach))
@@ -137,6 +195,7 @@ subroutine gp_basic_finalise(self)
 
    if (self%initialised) then
       if (allocated(self%f_r)) deallocate(self%f_r)
+      if (allocated(self%g_r)) deallocate(self%g_r)
       if (allocated(self%Cmat_inv_v)) deallocate(self%Cmat_inv_v)
       if (allocated(self%k)) deallocate(self%k)
       if (allocated(self%Cmat_inv_k)) deallocate(self%Cmat_inv_k)
@@ -148,13 +207,21 @@ subroutine gp_basic_finalise(self)
    self%l2 = 0.0_dp
    self%f_var = 0.0_dp
    self%n_f = 0
+   self%n_g = 0
    self%n_teach = 0
    self%initialised = .false.
 end subroutine gp_basic_finalise
 
-function f_predict_r(self, r)
+function f_predict_r(self, r, kernel)
    type(gp_basic), intent(inout) :: self !% object for GP
    real(dp), intent(in) :: r !% position at which to f_predict value
+   interface
+      subroutine kernel(x1, x2, f_var, l2, f_f, g_f, f_g, g_g)
+	 use system_module, only : dp
+	 real(dp), intent(in) :: x1, x2(:), f_var, l2
+	 real(dp), optional :: f_f(:), g_f(:), f_g(:), g_g(:)
+      end subroutine kernel
+   end interface
    real(dp) :: f_predict_r
 
    if (.not. self%initialised) then
@@ -162,31 +229,21 @@ function f_predict_r(self, r)
       return
    endif
 
-   self%k(:) = ff_kernel(r, self%f_r(:), self%f_var, self%l2)
+   if (self%n_f > 0) call kernel(r, self%f_r(1:self%n_f), self%f_var, self%l2, f_f=self%k(1:self%n_f))
+   if (self%n_g > 0) call kernel(r, self%g_r(1:self%n_g), self%f_var, self%l2, f_g=self%k(self%n_f+1:self%n_f+self%n_g))
    f_predict_r = dot_product(self%k, self%Cmat_inv_v)
 end function f_predict_r
 
-function f_predict_rr(self, r)
-   type(gp_basic), intent(inout) :: self
-   real(dp), intent(in) :: r(:)
-   real(dp) :: f_predict_rr(size(r))
-
-   integer :: i
-
-   if (.not. self%initialised) then
-      f_predict_rr = 0.0_dp
-      return
-   endif
-
-   do i=1, size(r)
-      self%k(:) = ff_kernel(r(i), self%f_r(:), self%f_var, self%l2)
-      f_predict_rr(i) = dot_product(self%k, self%Cmat_inv_v)
-   end do
-end function f_predict_rr
-
-function f_predict_var_r(self, r)
+function f_predict_var_r(self, r, kernel)
    type(gp_basic), intent(inout) :: self
    real(dp), intent(in) :: r
+   interface
+      subroutine kernel(x1, x2, f_var, l2, f_f, g_f, f_g, g_g)
+	 use system_module, only : dp
+	 real(dp), intent(in) :: x1, x2(:), f_var, l2
+	 real(dp), optional :: f_f(:), g_f(:), f_g(:), g_g(:)
+      end subroutine kernel
+   end interface
    real(dp) :: f_predict_var_r
 
    if (.not. self%initialised) then
@@ -194,33 +251,22 @@ function f_predict_var_r(self, r)
       return
    endif
 
-   self%k(:) = ff_kernel(r, self%f_r(:), self%f_var, self%l2)
+   if (self%n_f > 0) call kernel(r, self%f_r(1:self%n_f), self%f_var, self%l2, f_f=self%k(1:self%n_f))
+   if (self%n_g > 0) call kernel(r, self%g_r(1:self%n_g), self%f_var, self%l2, f_g=self%k(self%n_f+1:self%n_f+self%n_g))
    call Matrix_QR_Solve(self%Cmat(1), self%k, self%Cmat_inv_k)
    f_predict_var_r = self%f_var - dot_product(self%k, self%Cmat_inv_k)
 end function f_predict_var_r
 
-function f_predict_var_rr(self, r)
-   type(gp_basic), intent(inout) :: self
-   real(dp), intent(in) :: r(:)
-   real(dp) :: f_predict_var_rr(size(r))
-
-   integer :: i
-
-   if (.not. self%initialised) then
-      f_predict_var_rr = 0.0_dp
-      return
-   endif
-
-   do i=1, size(r)
-      self%k(:) = ff_kernel(r(i), self%f_r(:), self%f_var, self%l2)
-      call Matrix_QR_Solve(self%Cmat(1), self%k, self%Cmat_inv_k)
-      f_predict_var_rr(i) = self%f_var - dot_product(self%Cmat_inv_k, self%Cmat_inv_k)
-   end do
-end function f_predict_var_rr
-
-function f_predict_grad_r(self, r)
+function f_predict_grad_r(self, r, kernel)
    type(gp_basic), intent(inout) :: self !% object for GP
    real(dp), intent(in) :: r !% position at which to f_predict value
+   interface
+      subroutine kernel(x1, x2, f_var, l2, f_f, g_f, f_g, g_g)
+	 use system_module, only : dp
+	 real(dp), intent(in) :: x1, x2(:), f_var, l2
+	 real(dp), optional :: f_f(:), g_f(:), f_g(:), g_g(:)
+      end subroutine kernel
+   end interface
    real(dp) :: f_predict_grad_r
 
    if (.not. self%initialised) then
@@ -228,37 +274,19 @@ function f_predict_grad_r(self, r)
       return
    endif
 
-   self%k(:) = df_kernel(r, self%f_r(:), self%f_var, self%l2)
+   if (self%n_f > 0) call kernel(r, self%f_r(1:self%n_f), self%f_var, self%l2, g_f=self%k(1:self%n_f))
+   if (self%n_g > 0) call kernel(r, self%g_r(1:self%n_g), self%f_var, self%l2, g_g=self%k(self%n_f+1:self%n_f+self%n_g))
    f_predict_grad_r = dot_product(self%k, self%Cmat_inv_v)
 end function f_predict_grad_r
 
-
-function ff_kernel_r_r(x1, x2, f_var, l2)
-   real(dp), intent(in) :: x1, x2, f_var, l2
-   real(dp) :: ff_kernel_r_r
-
-   ff_kernel_r_r = f_var*exp(-0.5_dp*((x2-x1)**2)/l2)
-end function ff_kernel_r_r
-
-function ff_kernel_r_rr(x1, x2, f_var, l2)
+subroutine SE_kernel_r_rr(x1, x2, f_var, l2, f_f, g_f, f_g, g_g)
    real(dp), intent(in) :: x1, x2(:), f_var, l2
-   real(dp) :: ff_kernel_r_rr(size(x2))
+   real(dp), optional :: f_f(:), g_f(:), f_g(:), g_g(:)
 
-   ff_kernel_r_rr(:) = f_var*exp(-0.5_dp*((x2(:)-x1)**2)/l2)
-end function ff_kernel_r_rr
-
-function df_kernel_r_r(x1, x2, f_var, l2)
-   real(dp), intent(in) :: x1, x2, f_var, l2
-   real(dp) :: df_kernel_r_r
-
-   df_kernel_r_r = (x2-x1)/l2 * ff_kernel(x1, x2, f_var, l2)
-end function df_kernel_r_r
-
-function df_kernel_r_rr(x1, x2, f_var, l2)
-   real(dp), intent(in) :: x1, x2(:), f_var, l2
-   real(dp) :: df_kernel_r_rr(size(x2))
-
-   df_kernel_r_rr(:) = (x2(:)-x1)/l2 * ff_kernel(x1, x2, f_var, l2)
-end function df_kernel_r_rr
+   if (present(f_f)) f_f(:) = f_var*exp(-0.5_dp*((x2(:)-x1)**2)/l2)
+   if (present(g_f)) g_f(:) = (x2(:)-x1)/l2 * f_var*exp(-0.5_dp*((x2(:)-x1)**2)/l2)
+   if (present(f_g)) f_g(:) = -(x2(:)-x1)/l2 * f_var*exp(-0.5_dp*((x2(:)-x1)**2)/l2)
+   if (present(g_g)) g_g(:) = (1.0_dp-(x1-x2(:))**2/l2)/l2 * f_var*exp(-0.5_dp*((x2(:)-x1)**2)/l2)
+end subroutine SE_kernel_r_rr
 
 end module gp_basic_module
