242960dfbb35bb94faedfee211fcb157d1f9ac1c
nb326 <nb326@9aeadc09-fa51-0410-b5d8-e5c65b1e449c>
2013-03-29 18:41:28 +0000

    Add --perturb option to Tools/quippy/scripts/interpolate_configs.py
    git-svn-id: svn+ssh://cvs/home/jrk33/repo/trunk/QUIP@11507 9aeadc09-fa51-0410-b5d8-e5c65b1e449c

--- Tools/quippy/scripts/interpolate_configs.py
+++ Tools/quippy/scripts/interpolate_configs.py
@@ -5,6 +5,7 @@ import optparse, sys
 
 p = optparse.OptionParser(usage='%prog [option] [file1] [file2] ...')
 p.add_option('-n', '--n_images', action='store', type='int', default=1, help="""number of images to add between each pair""")
+p.add_option('-p', '--perturb', action='store', type='float', default=0.0, help="""magnitude of perturbation of intermediate configs""")
 opt, args = p.parse_args()
 
 ar_i = Atoms(args[0])
@@ -26,6 +27,11 @@ for i in frange(len(args)-1):
       dii = float(ii)/float(opt.n_images+1)
       at_out.set_lattice(ar_i.lattice+dii*dlattice, False)
       at_out.pos[:,:] = ar_i.pos + dii*dpos
+      if not ( (i == 1 and ii == 0) or (i == len(args)-1 and ii == opt.n_images+1) ):
+	 for iii in frange(at_out.n):
+	    at_out.pos[1,iii] += opt.perturb * ran_normal()
+	    at_out.pos[2,iii] += opt.perturb * ran_normal()
+	    at_out.pos[3,iii] += opt.perturb * ran_normal()
       at_out.write("stdout")
 
    ar_i = ar_ip1.copy()
