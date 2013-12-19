895e1ac0e0e28fa4d0b11c73d154ee43d3b9536b
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2013-12-19 11:12:28 -0500

    Add some things missing from use Potential_Module, only in Potential_Precon_Minim.f95

--- QUIP_Core/Potential_Precon_Minim.f95
+++ QUIP_Core/Potential_Precon_Minim.f95
@@ -3,7 +3,7 @@
 module Potential_Precon_Minim_module
 
 
-  use Potential_Module, only : Potential, potential_minimise, energy_func, gradient_func, cutoff, max_rij_change, calc, print_hook
+  use Potential_Module, only : Potential, potential_minimise, energy_func, gradient_func, cutoff, max_rij_change, calc, print_hook, constrain_virial_post, fix_atoms_deform_grad, pack_pos_dg, unpack_pos_dg, prep_atoms_deform_grad
   use error_module
   use system_module, only : dp, inoutput, print, PRINT_ALWAYS, PRINT_NORMAL, PRINT_VERBOSE, PRINT_NERD, initialise, finalise, INPUT, &
    optional_default, current_verbosity, mainlog, round, verbosity_push_decrement, verbosity_push,verbosity_push_increment, verbosity_pop, print_warning, system_timer, system_abort, operator(//)
