7adf6ebf9e1801ddb488c77eafb6c607435f977e
James Kermode <james.kermode@gmail.com>
2014-01-13 19:03:33 +0000

    Prototype working version of socket server

--- Tools/quippy/scripts/quippy-client.py
+++ Tools/quippy/scripts/quippy-client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import multiprocessing
 import sys
 import socket
 import threading
@@ -15,105 +16,113 @@ MSG_END_MARKER_SIZE = len(MSG_END_MARKER)
 ATOMS_HOST = ''
 ATOMS_PORT = 8888
 
-CLIENT_ID = int(sys.argv[1])
+N_ATOMS = 64 # hard-coded for this test
+N_JOBS = int(sys.argv[1])
+CLIENT_ID = int(sys.argv[2])
+N_STEPS = N_ATOMS/N_JOBS
 
 pot = Potential('IP SW')
 args_str = 'force energy virial'
 
-for step in range(64):
+def quippy_client(client_id, maxsteps):
+
+    print 'Starting client with ID %d maxsteps %d' % (client_id, maxsteps)
     
-    # open connection to server
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ATOMS_HOST, ATOMS_PORT))
-    try:
-        # say hello and ask server for some Atoms (status 'A')
-        # identify ourselves with an ID number, formatted as an 8-byte ascii string
-        id_str = ('A%7d' % CLIENT_ID).encode('ascii')
-        totalsent = 0
-        while totalsent < MSG_LEN_SIZE:
-            sent = sock.send(id_str[totalsent:])
-            if sent == 0:
-                raise RuntimeError('socket connection broken while sending client ID')
-            totalsent += sent
+    for step in range(maxsteps):
         
-        # now receive the length of the data, formatted as an 8-byte ascii string
-        data_length = ''
-        while len(data_length) < MSG_LEN_SIZE:
-            chunk = sock.recv(MSG_LEN_SIZE - len(data_length))
-            if not chunk:
-                raise RuntimeError('socket connection broken while reading length')
-            data_length += chunk
-        data_length = int(data_length)
-        print 'Data length: %d' % data_length
+        # open connection to server
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ATOMS_HOST, ATOMS_PORT))
+        try:
+            # say hello and ask server for some Atoms (status 'A')
+            # identify ourselves with an ID number, formatted as an 8-byte ascii string
+            id_str = ('A%7d' % client_id).encode('ascii')
+            totalsent = 0
+            while totalsent < MSG_LEN_SIZE:
+                sent = sock.send(id_str[totalsent:])
+                if sent == 0:
+                    raise RuntimeError('socket connection broken while sending client ID')
+                totalsent += sent
 
-        # now receive the data itself
-        data = ''
-        while len(data) < data_length:
-            chunk = sock.recv(data_length - len(data))
-            if not chunk:
-                raise RuntimeError('socket connection broken while reading data')
-            data += chunk
+            # now receive the length of the data, formatted as an 8-byte ascii string
+            data_length = ''
+            while len(data_length) < MSG_LEN_SIZE:
+                chunk = sock.recv(MSG_LEN_SIZE - len(data_length))
+                if not chunk:
+                    raise RuntimeError('socket connection broken while reading length')
+                data_length += chunk
+            data_length = int(data_length)
 
-        # and finally receive the end marker
-        marker = ''
-        while len(marker) < MSG_END_MARKER_SIZE:
-            chunk = sock.recv(MSG_END_MARKER_SIZE - len(marker))
-            if not chunk:
-                raise RuntimeError('socket connection broken while reading end marker')
-            marker += chunk
+            # now receive the data itself
+            data = ''
+            while len(data) < data_length:
+                chunk = sock.recv(data_length - len(data))
+                if not chunk:
+                    raise RuntimeError('socket connection broken while reading data')
+                data += chunk
 
-        assert marker == MSG_END_MARKER
+            # and finally receive the end marker
+            marker = ''
+            while len(marker) < MSG_END_MARKER_SIZE:
+                chunk = sock.recv(MSG_END_MARKER_SIZE - len(marker))
+                if not chunk:
+                    raise RuntimeError('socket connection broken while reading end marker')
+                marker += chunk
 
-    finally:
-        sock.close()
+            assert marker == MSG_END_MARKER
 
-    # construct an Atoms object from data string
-    at = Atoms(data, format='string')
-    print 'Received Atoms with %d atoms' % len(at)
+        finally:
+            sock.close()
 
-    # do the calculation
-    pot.calc(at, args_str=args_str)
+        # construct an Atoms object from data string
+        at = Atoms(data, format='string')
 
-    # format the results into a string
-    data = at.write('string')
-    data_length = ('%8s' % len(data)).encode('ascii')
+        # do the calculation
+        pot.calc(at, args_str=args_str)
 
-    # preceed message by its length
-    data = data_length + data
+        print 'client %d calculation on %d atoms complete' % (client_id, len(at))
 
-    # open a new connection to the server to send back the results
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ATOMS_HOST, ATOMS_PORT))
-    try:
-        # say hello and tell server we've got some results (status 'R')
-        # identify ourselves with an ID number, formatted as an 8-byte ascii string
-        id_str = ('R%7d' % CLIENT_ID).encode('ascii')
-        totalsent = 0
-        while totalsent < MSG_LEN_SIZE:
-            sent = sock.send(id_str[totalsent:])
-            if sent == 0:
-                raise RuntimeError('socket connection broken while sending client ID')
-            totalsent += sent
-        
-        # send back results of the calculation
-        totalsent = 0
-        while totalsent < len(data):
-            sent = sock.send(data[totalsent:])
-            if sent == 0:
-                raise RuntimeError('socket connection broken while sending results')
-            totalsent += sent
+        # format the results into a string
+        data = at.write('string')
+        data_length = ('%8s' % len(data)).encode('ascii')
 
-        # wait to receive the end marker
-        marker = ''
-        while len(marker) < MSG_END_MARKER_SIZE:
-            chunk = sock.recv(MSG_END_MARKER_SIZE - len(marker))
-            if not chunk:
-                raise RuntimeError('socket connection broken while reading end marker')
-            marker += chunk
+        # preceed message by its length
+        data = data_length + data
 
-        assert marker == MSG_END_MARKER
+        # open a new connection to the server to send back the results
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((ATOMS_HOST, ATOMS_PORT))
+        try:
+            # say hello and tell server we've got some results (status 'R')
+            # identify ourselves with an ID number, formatted as an 8-byte ascii string
+            id_str = ('R%7d' % client_id).encode('ascii')
+            totalsent = 0
+            while totalsent < MSG_LEN_SIZE:
+                sent = sock.send(id_str[totalsent:])
+                if sent == 0:
+                    raise RuntimeError('socket connection broken while sending client ID')
+                totalsent += sent
 
-    finally:
-        sock.close()
+            # send back results of the calculation
+            totalsent = 0
+            while totalsent < len(data):
+                sent = sock.send(data[totalsent:])
+                if sent == 0:
+                    raise RuntimeError('socket connection broken while sending results')
+                totalsent += sent
 
-    print 'Sent Atoms with %d atoms in %d bytes' % (len(at), len(data))
+            # wait to receive the end marker
+            marker = ''
+            while len(marker) < MSG_END_MARKER_SIZE:
+                chunk = sock.recv(MSG_END_MARKER_SIZE - len(marker))
+                if not chunk:
+                    raise RuntimeError('socket connection broken while reading end marker')
+                marker += chunk
+
+            assert marker == MSG_END_MARKER
+
+        finally:
+            sock.close()
+            
+
+quippy_client(CLIENT_ID, N_STEPS)
--- Tools/quippy/scripts/quippy-server.py
+++ Tools/quippy/scripts/quippy-server.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
+import sys
 import socket
 import threading
 import SocketServer
 import Queue
 
 from quippy.atoms import Atoms
-from quippy.clusters import create_hybrid_weights, create_cluster_simple
+from quippy.lotf import iter_atom_centered_clusters
 from quippy.potential import Potential
 from quippy.structures import diamond, supercell
-from quippy.util import args_str
 
 MSG_LEN_SIZE = 8
 MSG_END_MARKER = 'done.\n'
@@ -18,7 +18,7 @@ MSG_END_MARKER_SIZE = len(MSG_END_MARKER)
 QUIPPY_HOST = ''
 QUIPPY_PORT = 8888
 
-N_JOBS = 1
+N_JOBS = int(sys.argv[1])
 
 class QuippyRequestHandler(SocketServer.StreamRequestHandler):
 
@@ -33,26 +33,23 @@ class QuippyRequestHandler(SocketServer.StreamRequestHandler):
         if client_id > N_JOBS-1:
             raise RuntimeError('Unknown client ID %d outside of range 0 < ID < %d' % (client_id, N_JOBS-1))
 
-        print 'Serving "%s" request from %s:%d client %d' % (request, ip, port, client_id)
+        print '"%s" request from %s:%d client %d qsize %d' % (request, ip, port, client_id, input_qs[client_id].qsize())
 
         if request == 'A':
             # client is ready for some more work to do
-            print 'Queue size: %d' % input_qs[client_id].qsize()
             at = input_qs[client_id].get()
             data = at.write('string').encode('ascii')
             data_length = ('%8d' % len(data)).encode('ascii')
             self.wfile.write(data_length)
             self.wfile.write(data)
-            print 'Sent Atoms to client %s' % client_id
 
         elif request == 'R':
             # results are available from client
             data_size = int(self.rfile.read(MSG_LEN_SIZE))
             data = self.rfile.read(data_size)
             at = Atoms(data, format='string')
-            print 'Got Atoms from client %s, energy=%.3f' % (client_id, at.energy)
             input_qs[client_id].task_done()
-            output_qs[client_id].put(at)
+            output_qs[client_id].put(at.force[:, 1].copy())
                         
         else:
             raise RuntimeError('Unknown request code "%s"' % request)
@@ -67,51 +64,47 @@ server = ThreadedQuippyServer((QUIPPY_HOST, QUIPPY_PORT), QuippyRequestHandler)
 
 server_thread = threading.Thread(target=server.serve_forever)
 server_thread.daemon = True
-print 'Starting threaded quippy server on %s:%d...' % (QUIPPY_HOST, QUIPPY_PORT)
+print 'Starting threaded quippy server on %s:%d with N_JOBS=%d' % (QUIPPY_HOST, QUIPPY_PORT, N_JOBS)
 server_thread.start()
 
+# we need an input and an output Queue for each client: this is so
+# that we can exploit wavefunction reuse by sending consecutive
+# clusters belonging to the same atom to the same QM partition
 input_qs = [Queue.Queue() for i in range(N_JOBS)]
 output_qs = [Queue.Queue() for i in range(N_JOBS)]
 
-def iter_little_clusters(at, **cluster_args):
-    saved_hybrid_mark = None
-    if hasattr(at, 'hybrid_mark'):
-        saved_hybrid_mark = at.properties['hybrid_mark'].copy()
-        indices = saved_hybrid_mark.nonzero()[0]
-    else:
-        indices = at.indices
-    
-    at.add_property('hybrid_mark', 0, overwrite=True)
-
-    for i in indices:
-        at.hybrid_mark[:] = 0
-        at.hybrid_mark[i] = True
-        create_hybrid_weights(at, args_str=args_str(cluster_args))
-        c = create_cluster_simple(at,
-                                  args_str=args_str(cluster_args),
-                                  mark_name='hybrid_mark')
-        yield c
-
-    if saved_hybrid_mark is not None:
-        at.add_property('hybrid_mark', saved_hybrid_mark, overwrite=True)
-    else:
-        del at.properties['hybrid_mark']
-
-
+# setup clusters to be calculated
 d = diamond(5.44, 14)
 at = supercell(d, 2, 2, 2)
 at.rattle(0.05)
 at.calc_connect()
-clusters = list(iter_little_clusters(at, buffer_hops=3))
-        
-for i, c in enumerate(clusters):
+n_clusters = 0
+for i, c in enumerate(iter_atom_centered_clusters(at, buffer_hops=3)):
+    n_clusters += 1
     input_qs[i % N_JOBS].put(c)
 
+# wait for all clients to finish processing clusters
 for input_q in input_qs:
     input_q.join()
 
+# collect results
+result_at = at.copy()
+result_at.add_property('cluster_force', 0.0, n_cols=3)
+for i in range(n_clusters):
+    result = output_qs[i % N_JOBS].get()
+    result_at.cluster_force[:, i+1] = result
+    output_qs[i % N_JOBS].task_done()
+    
+# reference calculation of full system
+pot = Potential('IP SW')
+pot.calc(result_at, args_str='force')
 
-print 'Shutting down server'
+# compare cluster and reference forces
+max_force_err = abs(result_at.force - result_at.cluster_force).max()
+print 'max_force_error = %.3e eV/A' % max_force_err
+assert max_force_err < 1.0e-6
+
+print 'Shutting down quippy server'
 server.shutdown()
 
 
