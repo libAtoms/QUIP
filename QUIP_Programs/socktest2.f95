2184624c1b052861241cec39c637e9b568b0d375
James Kermode <james.kermode@gmail.com>
2014-02-21 09:49:24 +0000

    Do MM calcs on the QUIP CNs and QM on other CNs

--- QUIP_Programs/socktest2.f95
+++ QUIP_Programs/socktest2.f95
@@ -6,15 +6,17 @@ program socktest2
 
   implicit none
 
-  type(Atoms) :: at
+  type(Atoms) :: at, cluster, qm_cluster
   type(Potential) :: pot
   type(MPI_context) :: mpi
 
-  character(STRING_LENGTH) :: ip, tmp
+  character(STRING_LENGTH) :: ip, tmp, cluster_args
   integer :: port, client_id, buffsize, n_atoms, i, n, label, old_n
   real(dp) :: e
 
-  call system_initialise
+  cluster_args = 'cluster_calc_connect=F min_images_only hysteretic_connect_cluster_radius=10.0 hysteretic_buffer cluster_periodic_z hysteretic_buffer_inner_radius=6.0 single_cluster=F hysteretic_connect=F cluster_hopping=F randomise_buffer=F hysteretic_connect_outer_factor=1.5 hysteretic_buffer_outer_radius=8.0 cluster_hopping_nneighb_only=F hysteretic_connect_inner_factor=1.3'
+
+  call system_initialise(enable_timing=.true.)
 
   call initialise(mpi)
 
@@ -41,32 +43,46 @@ program socktest2
 
      n = 0
      do while (.true.)
+        call system_timer('step')
 
+        call system_timer('make_cluster')
         call set_cutoff(at, cutoff(pot))
         call calc_connect(at)
+        call create_hybrid_weights(at, args_str=cluster_args)
+        call create_cluster_simple(at, args_str=cluster_args, cluster=cluster)
+        call system_timer('make_cluster')
+
+        call system_timer('socket_send')
+        call set_value(cluster%params, 'label', label)
+        call socket_send_xyz(ip, port, client_id, cluster)
+        call system_timer('socket_send')
+
+        ! do MM calc while we're waiting for QM
+        call system_timer('MM calc')
         call calc(pot, at, args_str="energy force virial")
         if (.not. get_value(at%params, 'energy', e)) &
              call system_abort('energy value missing from atoms after calc() call')
+        call print('completed MM calculation n='//n//' label= '//label//' on '//at%n//' atoms. energy='//e)
+        call system_timer('MM calc')
 
-        call set_value(at%params, 'label', label)
-        call print('completed calculation n='//n//' label= '//label//' on '//at%n//' atoms. energy='//e)
-        call socket_send_xyz(ip, port, client_id, at)
-
+        call system_timer('socket_recv')
         n = n + 1
-
         old_n = at%n
-        call socket_recv_xyz(ip, port, client_id, buffsize, at)
-        if (.not. get_value(at%params, 'label', label)) call system_abort('missing label param in received atoms')
+        call socket_recv_xyz(ip, port, client_id, buffsize, qm_cluster)
+        call system_timer('socket_recv')
 
-        if (at%n == 0 .or. old_n /= at%n) then
-           call print('old_n='//old_n//', at%n='//at%n//' - shutting down QUIP server')
-           exit
-        end if
+        if (.not. get_value(qm_cluster%params, 'label', label)) call system_abort('missing label param in received atoms')
+        call print('Received qm_cluster label='//label)
 
-
+        ! fill in QM forces from qm_cluster
+        ! advance the dynamics
+        
+        call system_timer('step')
      end do
 
      call finalise(at)
+     call finalise(cluster)
+     call finalise(qm_cluster)
      call finalise(pot)
 
   end if
