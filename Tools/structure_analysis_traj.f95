f176926e419860e5d4d478cfc7b8e48e26f319b6
Noam Bernstein <bernstei@tin.(none)>
2014-05-02 14:36:35 -0400

    Don't skip angles between images in adfd analysis of structure_analysis_traj

--- Tools/structure_analysis_traj.f95
+++ Tools/structure_analysis_traj.f95
@@ -1417,6 +1417,7 @@ subroutine adfd_calc(adfd, at, zone_center, n_angle_bins, dist_bin_width, n_dist
   integer, allocatable :: n_in_zone(:)
   real(dp) :: dist_bin_inner_rad, dist_bin_outer_rad, angle_bin_width, angle_bin_min, angle_bin_max
   real(dp) :: r, r_ij, r_ik, r_jk, jik_angle
+  integer :: sj(3), sk(3)
 
   allocate(center_mask_a(at%N), neighbour_1_mask_a(at%N), neighbour_2_mask_a(at%N))
   call is_in_mask(center_mask_a, at, center_mask_str)
@@ -1448,7 +1449,7 @@ subroutine adfd_calc(adfd, at, zone_center, n_angle_bins, dist_bin_width, n_dist
   end if
 
   adfd = 0.0_dp
-  call set_cutoff(at, 2*n_dist_bins*dist_bin_width)
+  call set_cutoff(at, max(neighbour_1_max_dist,2*n_dist_bins*dist_bin_width))
   call calc_connect(at)
   do i_at=1, at%N ! center atom
     if (.not. center_mask_a(i_at)) cycle
@@ -1461,8 +1462,8 @@ subroutine adfd_calc(adfd, at, zone_center, n_angle_bins, dist_bin_width, n_dist
     endif
     n_in_zone(i_zone) = n_in_zone(i_zone) + 1
     do ji=1, n_neighbours(at, i_at)
-      j_at = neighbour(at, i_at, ji, distance=r_ij)
-      if (j_at == i_at) cycle
+      j_at = neighbour(at, i_at, ji, shift=sj, distance=r_ij)
+      if (r_ij == 0.0_dp) cycle
       if (.not. neighbour_1_mask_a(j_at)) cycle
       if (neighbour_1_max_dist > 0.0_dp) then
 	if (r_ij > neighbour_1_max_dist) cycle
@@ -1470,8 +1471,8 @@ subroutine adfd_calc(adfd, at, zone_center, n_angle_bins, dist_bin_width, n_dist
 	if (r_ij > bond_length(at%Z(i_at),at%Z(j_at))*at%nneightol) cycle
       endif
       do ki=1, n_neighbours(at, i_at)
-	k_at = neighbour(at, i_at, ki, distance=r_ik)
-	if (k_at == i_at .or. k_at == j_at) cycle
+	k_at = neighbour(at, i_at, ki, shift=sk, distance=r_ik)
+	if (r_ik == 0.0_dp .or. (k_at == j_at .and. all (sj == sk))) cycle
 	if (.not. neighbour_2_mask_a(k_at)) cycle
 	r_jk = distance_min_image(at, j_at, k_at)
 	if (dist_bin_rc2) then
