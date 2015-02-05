5d1720db74a2a451e867078638f668e0b5306e1b
Albert Bartok-Partay <apbartok@gmail.com>
2015-02-05 13:23:35 +0000

    fallback if not a GIT repo

--- utility_scripts/gapversion.sh
+++ utility_scripts/gapversion.sh
@@ -12,20 +12,22 @@ function git_date
 }
 
 QUIP_ROOT=$(dirname $0)/..
-
-if [[ -s "${QUIP_ROOT}/GAP_VERSION" ]]; then
-   echo -ne $(cat ${QUIP_ROOT}/GAP_VERSION)
-   exit 0
-fi
-
 GAP_FILES="QUIP_Core/IPModel_GAP.f95 GAP/gp_predict.f95 GAP-filler/clustering.f95 GAP-filler/gp_teach.f95 GAP-filler/teach_sparse_module.f95 GAP-filler/teach_sparse.f95"
 
-for I in $GAP_FILES
-do
-   if [ -d ${QUIP_ROOT}/$(dirname $I) ]
-   then
-      cd ${QUIP_ROOT}/$(dirname $I)
-      git_date $(basename $I)
-      cd - >/dev/null
-   fi
-done | sort -n | tail -1
+if [ -s "${QUIP_ROOT}/GAP_VERSION" ]; then
+   echo -ne $(cat ${QUIP_ROOT}/GAP_VERSION)
+   exit 0
+elif [ -d ${QUIP_ROOT}/.git ]; then
+   for I in $GAP_FILES
+   do
+      if [ -d ${QUIP_ROOT}/$(dirname $I) ]
+      then
+         cd ${QUIP_ROOT}/$(dirname $I)
+         git_date $(basename $I)
+         cd - >/dev/null
+      fi
+   done | sort -n | tail -1
+else
+   echo "0"
+   exit 0
+fi
