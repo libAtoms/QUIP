2826e6b328be16dba90b6e2ff23f5a997a06ebc9
Noam Bernstein <bernstei@tin.(none)>
2016-09-27 15:01:48 -0400

    Fix handling of both signs of T_increment in md.f95

--- src/Programs/md.f95
+++ src/Programs/md.f95
@@ -587,7 +587,8 @@ function cur_temp(params, ds_t) result(T_cur)
   if (params%T_final > 0.0_dp) then
     i_increment = ds_t/params%T_increment_time
     T_cur = params%T_initial + i_increment*params%T_increment
-    if (T_cur > params%T_final) T_cur = params%T_final
+    if ((params%T_increment > 0.0 .and. T_cur > params%T_final) .or. &
+        (params%T_increment < 0.0 .and. T_cur < params%T_final)) T_cur = params%T_final
   else
     T_cur = params%T_initial
   endif
