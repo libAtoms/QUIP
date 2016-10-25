b821c8f19bfa27026762c6a692cd4f9e97250ad8
Noam Bernstein <bernstei@tin.(none)>
2016-10-25 16:21:19 -0400

    In minimization constrain virial before it's tranformed by deformation gradient, otherwise final virial doesn't really obey constraints

--- src/Potentials/Potential.f95
+++ src/Potentials/Potential.f95
@@ -363,7 +363,7 @@ module Potential_module
   public :: fix_atoms_deform_grad
   public :: prep_atoms_deform_grad
   public :: max_rij_change
-  public :: constrain_virial_post
+  public :: constrain_virial
 
 
   public :: potential_minimise
@@ -1775,10 +1775,10 @@ end subroutine undo_travel
 
     f = transpose(deform_grad) .mult. f
 
+    call constrain_virial(am%minim_at, virial)
+
     call inverse(deform_grad, deform_grad_inv)
     virial = virial .mult. transpose(deform_grad_inv)
-
-    call constrain_virial_post(am%minim_at, virial)
 
     call pack_pos_dg(-f, -virial, gradient_func, 1.0_dp/am%pos_lat_preconditioner_factor)
     if (current_verbosity() >= PRINT_NERD) then
@@ -2023,10 +2023,10 @@ endif
 
     f = transpose(deform_grad) .mult. f
 
+    call constrain_virial(am%minim_at, virial)
+
     call inverse(deform_grad, deform_grad_inv)
     virial = virial .mult. transpose(deform_grad_inv)
-
-    call constrain_virial_post(am%minim_at, virial)
 
     call pack_pos_dg(-f, -virial, grad, 1.0_dp/am%pos_lat_preconditioner_factor)
     call print ("both_func gradient packed as", PRINT_NERD)
@@ -2352,7 +2352,7 @@ end subroutine pack_pos_dg
     endif
   end subroutine constrain_DG
 
-  subroutine constrain_virial_post(at, virial)
+  subroutine constrain_virial(at, virial)
     type(Atoms), intent(in) :: at
     real(dp), intent(inout) :: virial(3,3)
 
@@ -2397,7 +2397,7 @@ end subroutine pack_pos_dg
       virial(3,3) = virial(3,3) - virial_trace/3.0
     endif
 
-  end subroutine constrain_virial_post
+  end subroutine constrain_virial
 
 
 
