cca2cca8ff928ec4a125c94920af629e729604ae
Tom Daff <tdd20@cam.ac.uk>
2017-05-16 11:23:55 +0100

    bond_angle.py py3 + style

--- quippy/scripts/bond_angle.py
+++ quippy/scripts/bond_angle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-import sys
-import os
+from __future__ import print_function
+
 import optparse
-import itertools
+import numpy as np
+
 from quippy import AtomsReader
 
 p = optparse.OptionParser(usage='%prog [options] <input file>...')
@@ -19,7 +20,8 @@ opt, args = p.parse_args()
 
 if opt.offset is not None:
     if len(opt.offset) != len(args):
-        p.error('Number of -O/--offset options must match number of input files')
+        p.error('Number of -O/--offset options must match '
+                'number of input files')
 else:
     opt.offset = [0]*len(args)
 
@@ -29,14 +31,16 @@ if len(opt.bond) == 0 and len(opt.angle) == 0:
 atoms = set([idx for bond in opt.bond for idx in bond] +
             [idx for angle in opt.angle for idx in angle])
 atoms_start = min(atoms)
-atoms_stop  = max(atoms)
-sources = [AtomsReader(arg, start=offset, range=[atoms_start, atoms_stop]) for arg, offset in zip(args, opt.offset)]
+atoms_stop = max(atoms)
+sources = [AtomsReader(arg, start=offset, range=[atoms_start, atoms_stop])
+           for arg, offset in zip(args, opt.offset)]
 
-bonds =  [ (i-atoms_start+1, j-atoms_start+1) for (i, j) in opt.bond ]
-angles =  [ (i-atoms_start+1, j-atoms_start+1, k-atoms_start+1) for (i, j, k) in opt.angle ]
+bonds = [(i-atoms_start+1, j-atoms_start+1) for (i, j) in opt.bond]
+angles = [(i-atoms_start+1, j-atoms_start+1, k-atoms_start+1)
+          for (i, j, k) in opt.angle]
 
 fmt_string = '%18.6f'*(1 + len(sources)*(len(bonds) + len(angles)))
-for configs in itertools.izip(*sources):
+for configs in zip(*sources):
     times = []
     bondlengths = []
     angles = []
@@ -46,7 +50,7 @@ for configs in itertools.izip(*sources):
             if opt.min_image:
                 bondlength = at.distamce_min_image(i, j)
             else:
-                bondlength = (at.pos[:,j] - at.pos[:,i]).norm() 
+                bondlength = (at.pos[:,j] - at.pos[:,i]).norm()
             bondlengths.append(bondlength)
 
         for i, j, k in angles:
@@ -59,10 +63,10 @@ for configs in itertools.izip(*sources):
 
             u_ij = u_ij/u_ij.norm()
             u_ik = u_ik/u_ik.norm()
-            angle = np.acos(np.dot(u_ij, u_ik))
+            angle = np.arccos(np.dot(u_ij, u_ik))
             angles.append(angle)
-                            
-    if times.count(times[0]) != len(times):
-        p.error('Misalignment: times=%=s' % times)
 
-    print fmt_string % tuple(times[:1] + bondlengths)
+    if times.count(times[0]) != len(times):
+        p.error('Misalignment: times=%s' % times)
+
+    print(fmt_string % tuple(times[:1] + bondlengths))
