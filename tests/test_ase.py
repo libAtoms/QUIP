b0d096faad9049b0ac3016742edb79198c4bf805
Tom Daff <tdd20@cam.ac.uk>
2017-07-04 14:25:00 +0100

    Take repr of unknown values in dict conversion
    quippy would throw errors for non-simple objects in dicts when
    converting to a fortran rooted dict. The repr of the object is now
    used instead.
    
    Special case for conversion from ase atoms takes the .symbol for
    the Spacegroup.
    
    Fixes #77

--- tests/test_ase.py
+++ tests/test_ase.py
@@ -25,45 +25,60 @@ import ase
 
 class TestConvert_Simple(QuippyTestCase):
 
-   def setUp(self):
-      self.dia_quippy   = diamond(5.44, 14)
-      self.dia_ase      = ase.Atoms(self.dia_quippy)
-      self.dia_quippy_2 = quippy.Atoms(self.dia_ase)
+    def setUp(self):
+        self.dia_quippy   = diamond(5.44, 14)
+        self.dia_ase      = ase.Atoms(self.dia_quippy)
+        self.dia_quippy_2 = quippy.Atoms(self.dia_ase)
 
-   def test_equal(self):
-      self.assertEqual(self.dia_quippy, self.dia_quippy_2)
+    def test_equal(self):
+        self.assertEqual(self.dia_quippy, self.dia_quippy_2)
 
 class TestConvert_ExtraProperties(QuippyTestCase):
 
-   def setUp(self):
-      self.dia_quippy   = diamond(5.44, 14)
-      self.dia_quippy.add_property('magmoms', np.random.uniform(-1,1,size=3*8).reshape(3,8))
-      self.dia_quippy.add_property('charge', [1.0]*8)
-      self.dia_ase      = ase.Atoms(self.dia_quippy)
-      self.dia_quippy_2 = quippy.Atoms(self.dia_ase)
+    def setUp(self):
+        self.dia_quippy   = diamond(5.44, 14)
+        self.dia_quippy.add_property('magmoms', np.random.uniform(-1,1,size=3*8).reshape(3,8))
+        self.dia_quippy.add_property('charge', [1.0]*8)
+        self.dia_ase      = ase.Atoms(self.dia_quippy)
+        self.dia_quippy_2 = quippy.Atoms(self.dia_ase)
 
-   def test_equal(self):
-      self.assertEqual(self.dia_quippy, self.dia_quippy_2)
+    def test_equal(self):
+        self.assertEqual(self.dia_quippy, self.dia_quippy_2)
 
 class TestConvert_UnsupportedProperties(QuippyTestCase):
 
-   def setUp(self):
-      self.dia_quippy   = diamond(5.44, 14)
-      self.dia_quippy.add_property('new', -1.0)
-      self.dia_ase      = ase.Atoms(self.dia_quippy)
-      self.dia_quippy_2 = quippy.Atoms(self.dia_ase)
+    def setUp(self):
+        self.dia_quippy   = diamond(5.44, 14)
+        self.dia_quippy.add_property('new', -1.0)
+        self.dia_ase      = ase.Atoms(self.dia_quippy)
+        self.dia_quippy_2 = quippy.Atoms(self.dia_ase)
 
-   def test_not_equal(self):
-      self.assertNotEqual(self.dia_quippy, self.dia_quippy_2)
+    def test_not_equal(self):
+        self.assertNotEqual(self.dia_quippy, self.dia_quippy_2)
 
-   def test_equal_after_remove(self):
-      self.dia_quippy.remove_property('new')
-      self.assertEqual(self.dia_quippy, self.dia_quippy_2)
+    def test_equal_after_remove(self):
+        self.dia_quippy.remove_property('new')
+        self.assertEqual(self.dia_quippy, self.dia_quippy_2)
 
+class TestConvert_UnsupportedInfo(QuippyTestCase):
+
+    def setUp(self):
+        from datetime import datetime
+        from ase.spacegroup import crystal
+        # creating a crystal puts a Spacegroup() in the info dict
+        self.fcc_al = crystal('Al', [[0, 0, 0]], spacegroup=225, cellpar=4.05)
+        # Other random class that gets repr'd
+        self.now = datetime.now()
+        self.fcc_al.info['timestamp'] = self.now
+
+    def test_info_custom_types(self):
+        conv_atoms = quippy.Atoms(self.fcc_al)
+        self.assertEqual(conv_atoms.info['timestamp'], repr(self.now))
+        self.assertEqual(conv_atoms.info['spacegroup'], 'F m -3 m')
 
 class TestCalculator_SW_Potential(QuippyTestCase):
-   def setUp(self):
-      xml="""
+    def setUp(self):
+        xml="""
       <SW_params n_types="2" label="PRB_31_plus_H">
       <comment> Stillinger and Weber, Phys. Rev. B  31 p 5262 (1984), extended for other elements </comment>
       <per_type_data type="1" atomic_num="1" />
@@ -92,49 +107,49 @@ class TestCalculator_SW_Potential(QuippyTestCase):
       </SW_params>
       """
 
-      system_reseed_rng(2065775975)
-      self.pot = Potential('IP SW', param_str=xml)
+        system_reseed_rng(2065775975)
+        self.pot = Potential('IP SW', param_str=xml)
 
-      self.at = diamond(5.44, 14)
-      randomise(self.at.pos, 0.1)
-      self.at.set_cutoff(self.pot.cutoff())
-      self.at.calc_connect()
+        self.at = diamond(5.44, 14)
+        randomise(self.at.pos, 0.1)
+        self.at.set_cutoff(self.pot.cutoff())
+        self.at.calc_connect()
 
-      self.e, = fvar('e')
-      self.f = fzeros((3,self.at.n))
-      self.df = fzeros((3,self.at.n))
-      self.v = fzeros((3,3))
+        self.e, = fvar('e')
+        self.f = fzeros((3,self.at.n))
+        self.df = fzeros((3,self.at.n))
+        self.v = fzeros((3,3))
 
-      self.e_ref = -34.5038375509
+        self.e_ref = -34.5038375509
 
-      self.f_ref = farray([[ 0.89920374, -0.38025157, -0.38727027],
-                           [ 0.36623356, -0.52403757,  0.7200206 ],
-                           [-0.36952654,  0.12899529,  0.00458111],
-                           [-0.19912365, -0.1632057 ,  1.08509495],
-                           [-0.67565314, -0.59410498, -0.47921521],
-                           [ 0.17097454,  0.5847822 , -0.31088749],
-                           [ 0.43613712,  0.90811269,  0.1600328 ],
-                           [-0.62824563,  0.03970963, -0.79235649]]).T
+        self.f_ref = farray([[ 0.89920374, -0.38025157, -0.38727027],
+                             [ 0.36623356, -0.52403757,  0.7200206 ],
+                             [-0.36952654,  0.12899529,  0.00458111],
+                             [-0.19912365, -0.1632057 ,  1.08509495],
+                             [-0.67565314, -0.59410498, -0.47921521],
+                             [ 0.17097454,  0.5847822 , -0.31088749],
+                             [ 0.43613712,  0.90811269,  0.1600328 ],
+                             [-0.62824563,  0.03970963, -0.79235649]]).T
 
-      self.v_ref = farray([[-0.34103601,  0.60925144, -0.02138795],
-                           [ 0.60925144, -0.36145702, -0.19375487],
-                           [-0.02138795, -0.19375487, -0.34640615]]).T
+        self.v_ref = farray([[-0.34103601,  0.60925144, -0.02138795],
+                             [ 0.60925144, -0.36145702, -0.19375487],
+                             [-0.02138795, -0.19375487, -0.34640615]]).T
 
-      self.ase_at = ase.Atoms(self.at)
-      self.ase_at.set_calculator(self.pot)
+        self.ase_at = ase.Atoms(self.at)
+        self.ase_at.set_calculator(self.pot)
 
-   def test_energy(self):
-      self.assertAlmostEqual(self.ase_at.get_potential_energy(), self.e_ref)
+    def test_energy(self):
+        self.assertAlmostEqual(self.ase_at.get_potential_energy(), self.e_ref)
 
-   def test_forces(self):
-      self.assertArrayAlmostEqual(self.ase_at.get_forces(), self.f_ref.T)
+    def test_forces(self):
+        self.assertArrayAlmostEqual(self.ase_at.get_forces(), self.f_ref.T)
 
-   def test_stress(self):
-      from quippy.elasticity import stress_vector
-      self.assertArrayAlmostEqual(self.ase_at.get_stress(), stress_vector(-self.v_ref/self.at.cell_volume()))
+    def test_stress(self):
+        from quippy.elasticity import stress_vector
+        self.assertArrayAlmostEqual(self.ase_at.get_stress(), stress_vector(-self.v_ref/self.at.cell_volume()))
 
-   def test_numeric_forces(self):
-      self.assertArrayAlmostEqual(self.pot.get_numeric_forces(self.ase_at), self.f_ref.T, tol=1e-4)
+    def test_numeric_forces(self):
+        self.assertArrayAlmostEqual(self.pot.get_numeric_forces(self.ase_at), self.f_ref.T, tol=1e-4)
 
 
 class TestNumbersSpecies(QuippyTestCase):
@@ -163,15 +178,15 @@ class TestNumbersSpecies(QuippyTestCase):
         self.assertEqual(list(self.at.z), [4,5,6])
         self.assertEqual(list(self.at.get_atomic_numbers()), [4,5,6])
         self.assertEqual(list(self.at.get_chemical_symbols()), ['Be', 'B', 'C'])
-        sp = list([''.join(s).strip() for s in self.at.species])        
+        sp = list([''.join(s).strip() for s in self.at.species])
         self.assertEqual(sp, ['Be', 'B', 'C'])
 
     def test_set_chemical_symbols(self):
         self.at.set_chemical_symbols(['Be', 'B', 'C'])
-        self.assertEqual(list(self.at.z), [4,5,6])        
+        self.assertEqual(list(self.at.z), [4,5,6])
         self.assertEqual(list(self.at.get_atomic_numbers()), [4,5,6])
         self.assertEqual(list(self.at.get_chemical_symbols()), ['Be', 'B', 'C'])
-        sp = list([''.join(s).strip() for s in self.at.species])        
+        sp = list([''.join(s).strip() for s in self.at.species])
         self.assertEqual(sp, ['Be', 'B', 'C'])
 
     def test_set_atomic_numbers_set_species_false(self):
@@ -179,12 +194,12 @@ class TestNumbersSpecies(QuippyTestCase):
         self.assertEqual(list(self.at.z), [4,5,6])
         self.assertEqual(list(self.at.get_atomic_numbers()), [4,5,6])
         self.assertEqual(list(self.at.get_chemical_symbols()), ['Be', 'B', 'C'])
-        sp = list([''.join(s).strip() for s in self.at.species])        
+        sp = list([''.join(s).strip() for s in self.at.species])
         self.assertEqual(sp, ['H', 'He', 'Li']) # still previous value
 
     def test_set_chemical_symbols_set_species_false(self):
         self.at.set_chemical_symbols(['Be', 'B', 'C'], set_species=False)
-        self.assertEqual(list(self.at.z), [4,5,6])        
+        self.assertEqual(list(self.at.z), [4,5,6])
         self.assertEqual(list(self.at.get_atomic_numbers()), [4,5,6])
         self.assertEqual(list(self.at.get_chemical_symbols()), ['Be', 'B', 'C'])
         sp = list([''.join(s).strip() for s in self.at.species])
@@ -193,7 +208,7 @@ class TestNumbersSpecies(QuippyTestCase):
     def test_constructor_set_species_false(self):
         at = Atoms(self.ase_at, set_species=False)
         self.assert_(not at.has_property('species'))
-        
+
 
 class TestOperators(QuippyTestCase):
 
@@ -215,12 +230,12 @@ C       3.00000000       3.00000000       3.00000000        6""", format='string
                                                               self.b.get_positions()])
 
     def test_operator_plus_equals(self):
-        a0 = self.a.copy()        
+        a0 = self.a.copy()
         self.a += self.b
         self.assertEquals(list(self.a.z), list(a0.z) + list(self.b.z))
         self.assertArrayAlmostEqual(self.a.get_positions(), np.r_[a0.get_positions(),
                                                                   self.b.get_positions()])
-        
+
 
     def test_operator_times(self):
         at = self.a * (3, 3, 3)
@@ -233,4 +248,4 @@ C       3.00000000       3.00000000       3.00000000        6""", format='string
 
 
 if __name__ == '__main__':
-   unittest.main()
+    unittest.main()
