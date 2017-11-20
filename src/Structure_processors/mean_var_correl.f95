92bc0ac1a72228ecb988343a50c0aba0b9891b28
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2017-11-20 13:52:56 -0500

    Add bimodality to mean_var_correl

--- src/Structure_processors/mean_var_correl.f95
+++ src/Structure_processors/mean_var_correl.f95
@@ -247,8 +247,9 @@ subroutine calc_binning_effective_N(data, over_bins, effective_N)
 
 	    if (err_est < prev_err_est .and. abs(err_est-prev_err_est)/(0.5_dp*(err_est+prev_err_est)) < 0.1_dp) best_err_est = err_est
 	    call print("binning_effective_N " // bin_size//" "//err_est, verbosity=PRINT_VERBOSE)
-	    bin_size_d = bin_size_d * 4
+	    bin_size_d = bin_size_d * 2
 	 end do
+         call print("", verbosity=PRINT_VERBOSE)
 
 	 if (best_err_est < 0.0_dp) best_err_est = prev_err_est
 
@@ -271,8 +272,9 @@ subroutine calc_binning_effective_N(data, over_bins, effective_N)
 
 	    if (err_est < prev_err_est .and. abs(err_est-prev_err_est)/(0.5_dp*(err_est+prev_err_est)) < 0.1_dp) best_err_est = err_est
 	    call print("binning_effective_N " // bin_size//" "//err_est, verbosity=PRINT_VERBOSE)
-	    bin_size_d = bin_size_d * 4
+	    bin_size_d = bin_size_d * 2
 	 end do
+         call print("", verbosity=PRINT_VERBOSE)
 
 	 if (best_err_est < 0.0_dp) best_err_est = prev_err_est
 
@@ -320,6 +322,7 @@ implicit none
   character(len=128), allocatable :: bin_labels(:)
   type(Dictionary) :: cli_params, data_params
   logical :: do_mean, do_var, do_histogram, do_correl, correlation_subtract_mean, do_correlation_var_effective_N, do_summed_ac_effective_N, do_sliding_window_effective_N, do_binning_effective_N, do_exp_smoothing
+  logical :: do_bimod
   real(dp) :: exp_smoothing_time
   integer :: exp_smoothing_bin_i
   integer :: histogram_n_bins
@@ -329,6 +332,7 @@ implicit none
   character(len=102400) :: myline
   type(inoutput) :: infile, outfile
   real(dp), allocatable :: data_mean(:), data_var(:), data_correl(:,:), data_histogram(:,:,:), data_histogram_data(:,:,:), data_histogram_correl(:,:)
+  real(dp), allocatable :: data_bimod(:), data_3_mom(:), data_4_mom(:)
   real(dp), allocatable :: smooth_data_v(:)
   integer :: reduction_index, other_index, sz, r_sz, correlation_max_lag, n_correl_print, correlation_var_effective_N_long_lag, sliding_window_effective_N_max_k, summed_ac_effective_N_max_lag
   logical :: over_bins, over_time
@@ -347,6 +351,7 @@ implicit none
   call param_register(cli_params, "exp_smoothing_time", "100", exp_smoothing_time, help_string="time constant for exponential smoothing (in units of frames). 0 => no smoothing")
   call param_register(cli_params, "exp_smoothing_bin_i", "0", exp_smoothing_bin_i, help_string="bin to evaluate for exponential smoothing")
   call param_register(cli_params, "variance", "F", do_var, help_string="calculate variance")
+  call param_register(cli_params, "bimodality", "F", do_bimod, help_string="calculate Sarle's bimodality coefficient")
   call param_register(cli_params, "correlation_var_effective_N", "F", do_correlation_var_effective_N, help_string="calculate effective N from ratio of initial variance to long time autocorrelation variance")
   call param_register(cli_params, "correlation_var_effective_N_long_lag", "1001", &
     correlation_var_effective_N_long_lag, help_string="lag after which autocorrelation is assumed to be in long time regime (for effective_N calculation)")
@@ -528,7 +533,7 @@ implicit none
       data_mean = sum(data,reduction_index)/real(size(data,reduction_index),dp)
   endif
 
-  if (do_var) then
+  if (do_var .or. do_bimod) then
     allocate(data_var(sz))
     if (over_bins) then
       do i=1, sz
@@ -545,6 +550,33 @@ implicit none
           end do
        endif
     endif
+  endif
+
+  ! b = \frac{(\mu_3^2/\mu_2^3) + 1.0}{\mu_4/\mu_2^2},
+  if (do_bimod) then
+    allocate(data_bimod(sz))
+    allocate(data_3_mom(sz))
+    allocate(data_4_mom(sz))
+    if (over_bins) then
+      do i=1, sz
+        data_3_mom(i) = sum((data(:,i)-data_mean(i))**3)/size(data,reduction_index)
+        data_4_mom(i) = sum((data(:,i)-data_mean(i))**4)/size(data,reduction_index)
+      end do
+    else 
+      if (n_weights > 0) then
+          do i=1, sz
+            data_3_mom(i) = sum(weights(:)*(data(i,:)-data_mean(i))**3)/sum(weights)
+            data_4_mom(i) = sum(weights(:)*(data(i,:)-data_mean(i))**4)/sum(weights)
+          end do
+       else
+          do i=1, sz
+            data_3_mom(i) = sum((data(i,:)-data_mean(i))**3)/size(data,reduction_index)
+            data_4_mom(i) = sum((data(i,:)-data_mean(i))**4)/size(data,reduction_index)
+          end do
+       endif
+    endif
+    data_bimod = ((data_3_mom**2 / data_var**3) + 1.0) / (data_4_mom/data_var**2)
+    deallocate(data_3_mom, data_4_mom)
   endif
 
   if (do_correl .or. do_correlation_var_effective_N .or. do_summed_ac_effective_N) then
@@ -608,7 +640,7 @@ implicit none
     endif
   end if ! do_exp_smoothing
 
-  if (do_mean .or. do_var .or. do_correlation_var_effective_N .or. do_summed_ac_effective_N .or. do_sliding_window_effective_N .or. do_binning_effective_N) then
+  if (do_mean .or. do_var .or. do_bimod .or. do_correlation_var_effective_N .or. do_summed_ac_effective_N .or. do_sliding_window_effective_N .or. do_binning_effective_N) then
     if (over_bins) then
       myline = "#"
     else
@@ -616,6 +648,7 @@ implicit none
     endif
     if (do_mean) myline = trim(myline) //" mean"
     if (do_var) myline = trim(myline) //" var N"
+    if (do_bimod) myline = trim(myline) //" bimod"
     if (do_correlation_var_effective_N) myline = trim(myline) // " correlation_var_effective_N correlation_var_effective_decorrel_time"
     if (do_summed_ac_effective_N) myline = trim(myline) // " summed_ac_effective_N summed_ac_effective_decorrel_time"
     if (do_sliding_window_effective_N) myline = trim(myline) // " sliding_window_effective_N sliding_window_effective_decorrel_time"
@@ -631,6 +664,7 @@ implicit none
       endif
       if (do_mean) myline = trim(myline) //" " // data_mean(i)
       if (do_var) myline = trim(myline) //" " // data_var(i)//" "//size(data,reduction_index)
+      if (do_bimod) myline = trim(myline) //" " // data_bimod(i)
       if (do_correlation_var_effective_N) then
 	if (correlation_var_effective_N(i) > 0) then
 	  myline = trim(myline) // " " // correlation_var_effective_N(i) // " " // (size(data,reduction_index)/correlation_var_effective_N(i))
