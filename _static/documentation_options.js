2768f8a8ec15eab5d74430cad0923b12716d301a
Travis-CI <build@travis.org>
2018-07-13 00:35:27 +0000

    docpush

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '531330f+',
+    VERSION: '18188c7+',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
