fd621157f078d8ab732f5a00cae6382acdc6df5e
Tamas K. Stenczel <stenczelt@gmail.com>
2019-01-09 23:00:40 +0000

    empty init.py and updates on atoms object

---
+++ quippy/quippy/__init__.py
@@ -0,0 +1,1 @@
+# empty __init__.py
--- quippy/quippy/atoms.py
+++ quippy/quippy/atoms.py
@@ -12,6 +12,18 @@ import quippy
 
 class Atoms(ase.Atoms):
 
+    """
+    There is a internal fortran atoms object, which shall not be manipulated by the user.
+    The main holder of all information should be the oject itself, the fortran object is only manipulated when there is
+    work to do with it and then the output of a calculation is used to update the main object.
+    This is especially nice because the length of the fortran object is defined, cannot change on the way. (Can it?)
+
+
+    Lattice: thought about using decorators and a property, but having the same thing in two places is nay-nay
+        - only functionality it would add is self.lattice being an accessible thing
+
+    """
+
     def __init__(self, symbols=None,
                  positions=None, numbers=None,
                  tags=None, momenta=None, masses=None,
@@ -21,27 +33,37 @@ class Atoms(ase.Atoms):
                  constraint=None,
                  calculator=None,
                  info=None):
+
+        # do the ase object initialisation
         ase.Atoms.__init__(self, symbols=symbols, positions=positions, numbers=numbers, tags=tags, momenta=momenta,
                            masses=masses, magmoms=magmoms, charges=charges, scaled_positions=scaled_positions,
                            cell=cell, pbc=pbc, celldisp=celldisp, constraint=constraint, calculator=calculator,
                            info=info)
-
+        # constuct the fortran object to have it, but don't populate it yet, only when needed
         self._fortran_atoms = quippy.atoms_types_module.Atoms(len(self), self.get_lattice())
 
         pass
 
     def get_lattice(self):
-        """ Fortran compatibility
-
-            fortran Atoms uses the transpose of the ase.Atoms.cell as lattice"""
+        """Fortran compatibility
+        fortran Atoms uses the transpose of the ase.Atoms.cell as lattice"""
         return self.get_cell().transpose()
 
     def set_lattice(self, lattice):
-        """ Fortran compatibility
-
+        """Fortran compatibility
         fortran Atoms uses the transpose of the ase.Atoms.cell as lattice"""
         lattice = np.array(lattice)
         self.set_cell(lattice.transpose())
+
+    def _update_fortran_object(self):
+        """Updates the internal fortran atoms object with the current """
+        self._fortran_atoms = quippy.atoms_types_module.Atoms(len(self), self.get_lattice())
+        # pass on all data to it
+        pass
+
+    def _update_ase_object(self):
+        """Update the main object with the data of the fortran object."""
+        pass
 
     # ase.Atoms functions which need to be rewritten here
 
