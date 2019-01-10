d690f4f0d13c84e87ea3a0f2036671fd0a72db15
Tamas K. Stenczel <stenczelt@gmail.com>
2019-01-10 18:46:08 +0000

    use 4-space indentation, reformat code by PyCharm

--- tests/quippytest.py
+++ tests/quippytest.py
@@ -23,88 +23,103 @@ from StringIO import StringIO
 
 Atoms.__eq__ = FortranDerivedType.__eq__
 
+
 def string_to_array(s):
-   return loadtxt(StringIO(s)).T
+    return loadtxt(StringIO(s)).T
+
 
 class QuippyTestCase(unittest.TestCase):
 
-   def assertDictionariesEqual(self, d1, d2, skip_keys=[], ignore_case=True):
+    def assertDictionariesEqual(self, d1, d2, skip_keys=[], ignore_case=True):
 
-      def lower_if_ignore_case(k):
-         if ignore_case:
-            return k.lower()
-         else:
-            return k
-      
-      d1 = dict([(lower_if_ignore_case(k),v) for (k,v) in d1.iteritems() if k not in skip_keys])
-      d2 = dict([(lower_if_ignore_case(k),v) for (k,v) in d2.iteritems() if k not in skip_keys])
+        def lower_if_ignore_case(k):
+            if ignore_case:
+                return k.lower()
+            else:
+                return k
 
-      if sorted(d1.keys()) != sorted(d2.keys()):
-         self.fail('Dictionaries differ: d1.keys() (%r) != d2.keys() (%r)'  % (d1.keys(), d2.keys()))
-      for key in d1:
-         v1, v2 = d1[key], d2[key]
-         if isinstance(v1, FortranArray):
-            try:
-               self.assertArrayAlmostEqual(v1, v2)
-            except AssertionError:
-               print key, v1, v2
-               raise
-         else:
-            if v1 != v2:
-               self.fail('Dictionaries differ: key=%s value1=%r value2=%r' % (key, v1, v2))
-      
-   def assertEqual(self, a, b):
-      if a == b: return
-      # Repeat comparison with debug-level logging
-      import logging
-      level = logging.root.level
-      logging.root.setLevel(logging.DEBUG)
-      a == b
-      logging.root.setLevel(level)
-      self.fail('%s != %s' % (a,b))
-      
-   
-   def assertArrayAlmostEqual(self, a, b, tol=1e-7):
-      a = farray(a)
-      b = farray(b)
-      self.assertEqual(a.shape, b.shape)
+        d1 = dict([(lower_if_ignore_case(k), v) for (k, v) in d1.iteritems() if k not in skip_keys])
+        d2 = dict([(lower_if_ignore_case(k), v) for (k, v) in d2.iteritems() if k not in skip_keys])
 
-      if isnan(a).any() or isnan(b).any():
-         print 'a'
-         print a
-         print 'b'
-         print b
-         self.fail('Not a number (NaN) found in array')
-      
-      if a.dtype.kind != 'f':
-         self.assert_((a == b).all())
-      else:
-         absdiff = abs(a-b)
-         if absdiff.max() > tol:
-            loc = [x+1 for x in unravel_index(absdiff.argmax()-1, absdiff.shape) ]
-            print 'a'
-            print a
+        if sorted(d1.keys()) != sorted(d2.keys()):
+            self.fail('Dictionaries differ: d1.keys() (%r) != d2.keys() (%r)' % (d1.keys(), d2.keys()))
+        for key in d1:
+            v1, v2 = d1[key], d2[key]
+            if isinstance(v1, FortranArray):
+                try:
+                    self.assertArrayAlmostEqual(v1, v2)
+                except AssertionError:
+                    print
+                    key, v1, v2
+                    raise
+            else:
+                if v1 != v2:
+                    self.fail('Dictionaries differ: key=%s value1=%r value2=%r' % (key, v1, v2))
+
+    def assertEqual(self, a, b):
+        if a == b: return
+        # Repeat comparison with debug-level logging
+        import logging
+        level = logging.root.level
+        logging.root.setLevel(logging.DEBUG)
+        a == b
+        logging.root.setLevel(level)
+        self.fail('%s != %s' % (a, b))
+
+    def assertArrayAlmostEqual(self, a, b, tol=1e-7):
+        a = farray(a)
+        b = farray(b)
+        self.assertEqual(a.shape, b.shape)
+
+        if isnan(a).any() or isnan(b).any():
             print
-            print 'b'
-            print b
+            'a'
             print
-            print 'Absolute difference'
-            print absdiff
-            self.fail('Maximum abs difference between array elements is %e at location %r' % (absdiff.max(), loc))
-   
+            a
+            print
+            'b'
+            print
+            b
+            self.fail('Not a number (NaN) found in array')
+
+        if a.dtype.kind != 'f':
+            self.assert_((a == b).all())
+        else:
+            absdiff = abs(a - b)
+            if absdiff.max() > tol:
+                loc = [x + 1 for x in unravel_index(absdiff.argmax() - 1, absdiff.shape)]
+                print
+                'a'
+                print
+                a
+                print
+                print
+                'b'
+                print
+                b
+                print
+                print
+                'Absolute difference'
+                print
+                absdiff
+                self.fail('Maximum abs difference between array elements is %e at location %r' % (absdiff.max(), loc))
+
 
 def skip(f):
-   def g(self):
-      logging.warning('skipping test %s' % f.__name__)
-   return g
+    def g(self):
+        logging.warning('skipping test %s' % f.__name__)
+
+    return g
+
 
 def profile(f):
-   import cProfile, pstats, functools
+    import cProfile, pstats, functools
 
-   @functools.wraps(f)
-   def g(self):
-      cProfile.runctx('f(self)', globals(), locals(), f.__name__+'.profile')
-      p = pstats.Stats(f.__name__+'.profile')
-      p.strip_dirs().sort_stats('cumulative').print_stats()
-      return f(self)
-   return g
+    @functools.wraps(f)
+    def g(self):
+        cProfile.runctx('f(self)', globals(), locals(), f.__name__ + '.profile')
+        p = pstats.Stats(f.__name__ + '.profile')
+        p.strip_dirs().sort_stats('cumulative').print_stats()
+        return f(self)
+
+    return g
