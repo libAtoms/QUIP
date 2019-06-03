29bf17cdc0ff720a9cb5450d62460b9731114dcf
Travis-CI <build@travis.org>
2019-06-03 16:24:30 +0000

    docpush

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'ad62f72+',
+    VERSION: '41420fe+',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
