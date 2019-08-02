e181ecdb5fd6c1f1922a749dc3c65d14dce161d5
Adam Fekete <adam.fekete@kcl.ac.uk>
2019-08-02 04:27:41 +0100

    cleanup of travis deploy scripts

---
+++ travis/trigger_gap_release.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -i http://www.libatoms.org/gap/cgi-bin/update.cgi
---
+++ travis/trigger_quip_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -s --output /dev/null -X POST \
+	-H "Content-Type: application/json" \
+	-H "Accept: application/json" \
+	-H "Travis-API-Version: 3" \
+	-H "Authorization: token ${TRAVIS_TOKEN}" \
+	-d "{\"request\": {\"branch\": \"master\"}}" \
+	https://api.travis-ci.org/repo/libatoms%2Fquip-docker/requests
