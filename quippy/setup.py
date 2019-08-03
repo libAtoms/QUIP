95d10754df739a97e6a71ccb595536564a2caea4
Adam Fekete <fekad@users.noreply.github.com>
2019-08-03 02:56:09 +0100

    Revert "fixing missing module _quippy"
    This reverts commit ecdf0a556eeffe7b4b436311bef7b28ff6c490d5.

--- quippy/setup.py
+++ quippy/setup.py
@@ -1,7 +1,6 @@
-import glob
 import subprocess
-# from setuptools import setup
-from distutils.core import setup
+import glob
+from setuptools import setup
 
 version = subprocess.getoutput(["../../bin/gitversion"]).strip()
 print('version:', version)
@@ -10,6 +9,6 @@ setup(
     name='quippy',
     version=version,
     author='James Kermode <james.kermode@gmail.com>',
-    packages=['quippy'],
-    package_data={'quippy': glob.glob('_quippy.*.so')},
+    packages=['', 'quippy'],
+    package_data={'': glob.glob('_quippy.*.so')},
 )
