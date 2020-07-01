fed4416bf304c55f13bc1eb08a08df4480af02ef
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-01 23:25:51 +0000

    Deploy libAtoms/QUIP to github.com/libAtoms/QUIP.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4e6ef64+',
+    VERSION: '1d35243+',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
