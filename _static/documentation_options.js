3ce2313a97c75455768c85e01d3c6247ec4b97a4
stenczelt <stenczelt@users.noreply.github.com>
2021-07-06 14:17:56 +0000

    deploy: 818ca4e858abbd59ba3723b6ab7605b80895ad10

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5c6524d+',
+    VERSION: '818ca4e+',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
