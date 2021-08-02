7fc9184ea908a21b6d6a6292812043a50d36a8fb
James Kermode <J.R.Kermode@warwick.ac.uk>
2021-08-02 14:55:48 +0100

    simplify test skipping logic

--- tests/test_shell_scripts.py
+++ tests/test_shell_scripts.py
@@ -8,15 +8,15 @@ import quippytest
 
 class TestShellScripts(quippytest.QuippyTestCase):
     def test_shell_scripts(self):
+        if 'QUIP_WHEEL_TEST' in os.environ
+            print(f'Skipping test when testing built wheels')
+            return
+        
         for f in Path(__file__).parent.glob('test_*.sh'):
             print("shell script test", f)
 
             p = Popen([f], stdout=PIPE, stderr=PIPE)
             stdout, stderr = p.communicate()
-
-            if 'QUIP_WHEEL_TEST' in os.environ and (p.returncode == 2 and f.endswith('test_xyz_6vector.sh')):
-                print(f'Skipping {f} when testing built wheels')
-                continue
 
             self.assertEqual(p.returncode, 0, f'Shell script test {f} failed with error code '
                                               f'{p.returncode} stderr {stderr.decode()} stdout {stdout.decode()}')
