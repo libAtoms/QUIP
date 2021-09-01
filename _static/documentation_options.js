b587b0b6594afcbfc7af3513dd0c810bf047c3cb
stenczelt <stenczelt@users.noreply.github.com>
2021-09-01 06:54:34 +0000

    deploy: 86b2edc830a5773f0be543793562c6c8163b51c7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'b993294+',
+    VERSION: '86b2edc+',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
