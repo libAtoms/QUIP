09e440fd3862ef387c9d43ac0a2a15bfe5cc7f65
bernstei <bernstei@users.noreply.github.com>
2021-12-09 20:15:55 +0000

    deploy: b95ae3e83c0b508caf6c8a5596c557a0cec691ce

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '126c892',
+    VERSION: 'b95ae3e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
