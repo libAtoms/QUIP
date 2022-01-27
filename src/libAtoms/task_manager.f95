d954101cecaa700139b66d615ca79d8765cb10c9
Sascha Klawohn <sascha.klawohn@warwick.ac.uk>
2022-01-27 07:53:59 +0000

    feat: add shared tasks

--- src/libAtoms/task_manager.f95
+++ src/libAtoms/task_manager.f95
@@ -4,6 +4,11 @@ module task_manager_module
   !>
   !> Each task stores integer data in %idata.
   !> %idata(1) is used for distribution.
+  !>
+  !> The special %worker_id=SHARED is used for shared tasks. Each worker
+  !> has a local entry with local %idata. %idata(1) contains the local
+  !> size, %idata(2) contains the global offset.
+
   use libatoms_module, only : initialise, finalise, print_title, print, &
       operator(//), system_abort, inoutput, optional_default, OUTPUT, PRINT_VERBOSE
   use linearalgebra_module, only : heap_sort
@@ -13,13 +18,14 @@ module task_manager_module
   implicit none
   private
 
-  public :: task_manager_type, task_type, worker_type
+  public :: task_manager_type, task_type, worker_type, SHARED
   public :: task_manager_init_tasks, task_manager_init_workers, task_manager_deallocate
   public :: task_manager_add_task, task_manager_add_worker
-  public :: task_manager_distribute_tasks
+  public :: task_manager_distribute_tasks, task_manager_check_distribution
   public :: task_manager_print, task_manager_export_distribution
 
   integer, parameter :: UNDEFINED = -1
+  integer, parameter :: SHARED = 0
 
   type task_type
     integer :: index = UNDEFINED !> initial task index
@@ -42,6 +48,7 @@ module task_manager_module
 
     integer :: n_tasks = 0 !> number of tasks
     integer :: n_workers = 0 !> number of workers
+    integer :: n_shared = 0 !> number of shared tasks
 
     type(MPI_Context) :: MPI_obj
     type(ScaLAPACK) :: ScaLAPACK_obj
@@ -68,6 +75,7 @@ module task_manager_module
     if (this%n_tasks > size(this%tasks)) call system_abort("More tasks added than allocated.")
     associate(task => this%tasks(this%n_tasks))
         allocate(task%idata(my_n_idata))
+        task%idata = UNDEFINED
         task%index = this%n_tasks
         if (present(idata1)) task%idata(1) = idata1
         if (present(worker_id)) task%worker_id = worker_id
@@ -119,7 +127,7 @@ module task_manager_module
     do t = 1, this%n_tasks
         i = index_list(t)
         w = this%tasks(i)%worker_id
-        if (w /= UNDEFINED) then
+        if (w >= 1) then
           this%workers(w)%n_tasks = this%workers(w)%n_tasks + 1
           workloads(w) = workloads(w) + idata1_list(t)
         end if
@@ -138,24 +146,81 @@ module task_manager_module
     this%unified_workload = maxval(workloads)
 
     ! fill workers with task id lists (replication for simpler retrieval)
+    this%n_shared = count(this%tasks(:)%worker_id == SHARED)
     do w = 1, this%n_workers
-        n = this%workers(w)%n_tasks
+        n = this%workers(w)%n_tasks + this%n_shared
         allocate(this%workers(w)%tasks(n))
         this%workers(w)%n_tasks = 0
     end do
     do t = 1, this%n_tasks
         w = this%tasks(t)%worker_id
+        if (w < 1) cycle
         n = this%workers(w)%n_tasks + 1
         this%workers(w)%tasks(n) = this%tasks(t)
         this%workers(w)%n_tasks = n
     end do
 
     do w = 1, this%n_workers
-        this%workers(w)%n_padding = this%unified_workload - workloads(w)
+      this%workers(w)%n_padding = this%unified_workload - workloads(w)
     end do
+
+    call task_manager_distribute_shared_tasks(this)
 
     this%distributed = .true.
   end subroutine task_manager_distribute_tasks
+
+  subroutine task_manager_distribute_shared_tasks(this)
+    ! fill padding first, then distribute rows evenly
+    type(task_manager_type), intent(inout) :: this
+  
+    integer :: i, n, t, w
+    integer :: rest, n_split, n_extra, n_plus, offset
+
+    if (.not. this%active) return
+
+    do t = 1, this%n_tasks
+      if (this%tasks(t)%worker_id /= SHARED) cycle
+
+      rest = max(this%tasks(t)%idata(1) - sum(this%workers(:)%n_padding), 0)
+      n_split = rest / this%n_workers
+      n_extra = mod(rest, this%n_workers)
+      n_plus = n_split
+      if (n_extra > 0) n_plus = n_split + 1
+
+      rest = this%tasks(t)%idata(1)
+      offset = 0
+      do w = 1, this%n_workers
+        n = this%workers(w)%n_tasks + 1
+        associate(worker => this%workers(w), w_task => this%workers(w)%tasks(n))
+          call task_deepcopy(w_task, this%tasks(t))
+
+          w_task%idata(1) = worker%n_padding + n_split
+          if (w <= n_extra) w_task%idata(1) =  w_task%idata(1) + 1
+          w_task%idata(1) = min(w_task%idata(1), rest)
+          w_task%idata(2) = offset
+          worker%n_padding = worker%n_padding + n_plus - w_task%idata(1)
+
+          rest = rest - w_task%idata(1)
+          offset =  offset + w_task%idata(1)
+          worker%n_tasks = n
+        end associate
+      end do
+      this%unified_workload = this%unified_workload + n_plus
+    end do
+  end subroutine task_manager_distribute_shared_tasks
+
+  subroutine task_manager_check_distribution(this)
+    type(task_manager_type), intent(in) :: this
+
+    integer :: t, w, n, tmp
+
+    if (.not. this%active) return
+    if (.not. this%distributed) return
+
+    tmp = task_manager_get_max_idata1_sum(this)
+    if (tmp /= this%unified_workload) call system_abort("task_manager_check_distribution: unified_workload is inconsistent: "//tmp//" "//this%unified_workload)
+    if (any(this%workers(:)%n_padding < 0)) call system_abort("task_manager_check_distribution: negative padding: "//this%workers(:)%n_padding)
+  end subroutine task_manager_check_distribution
 
   subroutine task_manager_show_distribution(this)
     type(task_manager_type), intent(in) :: this
@@ -170,7 +235,7 @@ module task_manager_module
         n_tasks = 0
         do i = 1, this%workers(w)%n_tasks
           t = this%workers(w)%tasks(i)%index
-          call print(i // ": " // t // ": " // this%tasks(t)%idata)
+          call print(i // ": " // t // ": " // this%workers(w)%tasks(i)%idata)
           idata1_sum = idata1_sum + this%tasks(t)%idata(1)
         end do
         call print("Summary: "//this%workers(w)%n_tasks//" tasks, "//idata1_sum//" idata1_sum, "//this%workers(w)%n_padding//" padding")
@@ -186,7 +251,6 @@ module task_manager_module
     type(Inoutput) :: file
 
     if (.not. this%active) return
-
     only_id_opt = optional_default(UNDEFINED, only_id)
     if (only_id_opt >= 0 .and. only_id_opt /= this%my_worker_id) return
 
@@ -195,7 +259,7 @@ module task_manager_module
     do w = 1, this%n_workers
         do i = 1, this%workers(w)%n_tasks
           t = this%workers(w)%tasks(i)%index
-          call print(w // " " // i // " " // t // " " // this%tasks(t)%idata, file=file)
+          call print(w // " " // i // " " // t // " " // this%workers(w)%tasks(i)%idata, file=file)
         end do
     end do
     call finalise(file)
@@ -215,10 +279,14 @@ module task_manager_module
     type(task_manager_type), intent(inout) :: this
     integer, intent(in) :: n_workers !> number of workers to allocate and set
 
+    integer :: w
+
     if (.not. this%active) return
 
-    this%n_workers = n_workers
-    allocate(this%workers(this%n_workers))
+    allocate(this%workers(n_workers))
+    do w = 1, n_workers
+      call task_manager_add_worker(this)
+    end do
   end subroutine task_manager_init_workers
 
   subroutine task_manager_deallocate(this)
@@ -249,5 +317,29 @@ module task_manager_module
     call task_manager_show_distribution(this)
     call print_title('')
   end subroutine task_manager_print
+
+  function task_manager_get_max_idata1_sum(this) result(res)
+    type(task_manager_type), intent(in) :: this
+
+    integer :: res
+    integer :: n, t, w
+
+    if (.not. this%active) return
+
+    res = 0
+    do w = 1, this%n_workers
+      n = sum([(this%workers(w)%tasks(t)%idata(1), t = 1, this%workers(w)%n_tasks)])
+      res = max(n, res)
+    end do
+  end function task_manager_get_max_idata1_sum
+
+  subroutine task_deepcopy(this, from)
+    type(task_type), intent(inout) :: this
+    type(task_type), intent(in) :: from
+
+    this%index = from%index
+    allocate(this%idata, source=from%idata)
+    this%worker_id = from%worker_id
+  end subroutine task_deepcopy
 
 end module task_manager_module
