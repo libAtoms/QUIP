bc944e5698cb203212da971201daca598119a7c4
jameskermode <jameskermode@users.noreply.github.com>
2022-02-07 12:07:29 +0000

    deploy: 58ef0b7cd09a527cd6e094164f4a373ab62a4213

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'cdb170e',
+    VERSION: '58ef0b7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
