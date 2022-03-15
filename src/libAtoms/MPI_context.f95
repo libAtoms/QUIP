d86c9aaae14c73d2c57a070913ea14bdf7a0e744
Sascha Klawohn <sascha.klawohn@warwick.ac.uk>
2022-03-15 16:48:12 +0000

    feat(mpi): add gatherv int1, real2

--- src/libAtoms/MPI_context.f95
+++ src/libAtoms/MPI_context.f95
@@ -134,6 +134,12 @@ interface sum_in_place
   module procedure MPI_context_sum_in_place_complex2
 end interface
 
+public :: gatherv
+interface gatherv
+  module procedure MPI_context_gatherv_int1
+  module procedure MPI_context_gatherv_real2
+end interface gatherv
+
 public :: allgatherv
 interface allgatherv
   module procedure MPI_context_allgatherv_real2
@@ -1138,6 +1144,110 @@ subroutine MPI_Print(this, lines, file)
   endif
 
 end subroutine MPI_Print
+
+subroutine MPI_context_gatherv_int1(this, v_in, v_out, counts, root, error)
+  type(MPI_context), intent(in) :: this
+  integer, intent(in) :: v_in(:)
+  integer, intent(out) :: v_out(:)
+  integer, intent(out), allocatable, optional :: counts(:)
+  integer, intent(in), optional :: root
+  integer, intent(out), optional :: error
+
+  integer :: my_root, err, i, my_count
+  integer, allocatable :: displs(:), my_counts(:)
+
+  INIT_ERROR(error)
+
+  if (.not. this%active) then
+    if (any(shape(v_in) /= shape(v_out))) then
+      RAISE_ERROR("MPI_context_gatherv_int1 (no MPI) shape mismatch v_in " // shape(v_in) // " v_out " // shape(v_out), error)
+    endif
+    v_out = v_in
+    return
+  endif
+
+#ifdef _MPI
+  my_root = optional_default(ROOT_, root)
+  if (is_root(this)) allocate(my_counts(this%n_procs))
+
+  my_count = size(v_in)
+  call mpi_gather(my_count, 1, MPI_INTEGER, my_counts, 1, MPI_INTEGER, my_root, this%communicator, err)
+  PASS_MPI_ERROR(err, error)
+
+  if (sum(my_counts) /= size(v_out)) then
+    RAISE_ERROR("MPI_context_gatherv_int1 not enough space sum(my_counts) " // sum(my_counts) // " size(v_out) " // size(v_out), error)
+  endif
+
+  if (is_root(this)) then
+    allocate(displs(this%n_procs))
+    displs(1) = 0
+    do i = 2, this%n_procs
+      displs(i) = displs(i-1) + my_counts(i-1)
+    end do
+  end if
+
+  call MPI_gatherv(v_in, my_count, MPI_INTEGER, v_out, my_counts, displs, MPI_INTEGER, my_root, this%communicator, err)
+  PASS_MPI_ERROR(err, error)
+
+  if (present(counts)) then
+    if (allocated(counts)) deallocate(counts)
+    allocate(counts, source=my_counts)
+  end if
+#endif
+
+end subroutine MPI_context_gatherv_int1
+
+subroutine MPI_context_gatherv_real2(this, v_in, v_out, counts, root, error)
+  type(MPI_context), intent(in) :: this
+  real(dp), intent(in) :: v_in(:,:)
+  real(dp), intent(out) :: v_out(:,:)
+  integer, intent(out), allocatable, optional :: counts(:)
+  integer, intent(in), optional :: root
+  integer, intent(out), optional :: error
+
+  integer :: my_root, err, i, my_count
+  integer, allocatable :: displs(:), my_counts(:)
+
+  INIT_ERROR(error)
+
+  if (.not. this%active) then
+    if (any(shape(v_in) /= shape(v_out))) then
+      RAISE_ERROR("MPI_context_gatherv_real2 (no MPI) shape mismatch v_in " // shape(v_in) // " v_out " // shape(v_out), error)
+    endif
+    v_out = v_in
+    return
+  endif
+
+#ifdef _MPI
+  my_root = optional_default(ROOT_, root)
+  if (is_root(this)) allocate(my_counts(this%n_procs))
+
+  my_count = size(v_in)
+  call mpi_gather(my_count, 1, MPI_INTEGER, my_counts, 1, MPI_INTEGER, my_root, this%communicator, err)
+  PASS_MPI_ERROR(err, error)
+
+  if (sum(my_counts) /= size(v_out)) then
+    RAISE_ERROR("MPI_context_gatherv_real2 not enough space sum(my_counts) " // sum(my_counts) // " size(v_out) " // size(v_out), error)
+  endif
+
+  if (is_root(this)) then
+    allocate(displs(this%n_procs))
+    displs(1) = 0
+    do i = 2, this%n_procs
+      displs(i) = displs(i-1) + my_counts(i-1)
+    end do
+  end if
+
+  call MPI_gatherv(v_in, my_count, MPI_DOUBLE_PRECISION, v_out, my_counts, displs, MPI_DOUBLE_PRECISION, my_root, this%communicator, err)
+  PASS_MPI_ERROR(err, error)
+
+  if (present(counts)) then
+    if (allocated(counts)) deallocate(counts)
+    allocate(counts, source=my_counts)
+  end if
+#endif
+
+end subroutine MPI_context_gatherv_real2
 
 subroutine MPI_context_allgatherv_real2(this, v_in, v_out, error)
   type(MPI_context), intent(in) :: this
