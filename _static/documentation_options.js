cd379c3345698c2fb483a0ac4df0ea0f245420ee
albapa <albapa@users.noreply.github.com>
2022-05-06 08:45:23 +0000

    deploy: c7f80b90b2260079b17d5ec61f721e111ce766ea

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'a5013b8',
+    VERSION: 'c7f80b9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
