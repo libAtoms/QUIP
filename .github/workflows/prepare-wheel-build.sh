8d055f9c365b48a8804e903018aa30302043185a
James Kermode <J.R.Kermode@warwick.ac.uk>
2022-09-13 22:21:24 +0100

    get openblas from homebrew on macOS x86_64 (but not on arm64)

--- .github/workflows/prepare-wheel-build.sh
+++ .github/workflows/prepare-wheel-build.sh
@@ -3,14 +3,23 @@ echo "prepare-build.sh received environment ARCHS=${ARCHS} QUIP_ARCH=${QUIP_ARCH
 # Install Openblas -- adapted from https://github.com/numpy/numpy/blob/main/tools/wheels/cibw_before_build.sh
 echo "Installing OpenBLAS..."
 
-basedir=$(python .github/workflows/openblas_support.py)
-cp -r $basedir/lib/* /usr/local/lib
-cp $basedir/include/* /usr/local/include
-if [[ "${RUNNER_OS}" == "macOS" && "$ARCHS" == "arm64" ]]; then
-    sudo mkdir -p /opt/arm64-builds/lib /opt/arm64-builds/include
-    sudo chown -R $USER /opt/arm64-builds
-    cp -r $basedir/lib/* /opt/arm64-builds/lib
-    cp $basedir/include/* /opt/arm64-builds/include
+if [[ "${RUNNER_OS" == "Linux" ]]; then
+	basedir=$(python .github/workflows/openblas_support.py)
+	cp -r $basedir/lib/* /usr/local/lib
+	cp $basedir/include/* /usr/local/include
+elif [[ "${RUNNER_OS} == "macOS" ]]; then
+	if [[ "$ARCHS" == "arm64" ]]; then
+		basedir=$(python .github/workflows/openblas_support.py)
+		cp -r $basedir/lib/* /usr/local/lib
+		cp $basedir/include/* /usr/local/include
+		sudo mkdir -p /opt/arm64-builds/lib /opt/arm64-builds/include
+		sudo chown -R $USER /opt/arm64-builds
+		cp -r $basedir/lib/* /opt/arm64-builds/lib
+		cp $basedir/include/* /opt/arm64-builds/include
+	else
+		brew install openblas
+		brew link --force openblas
+	fi
 fi
 
 WORK_DIR=$(dirname $0)
