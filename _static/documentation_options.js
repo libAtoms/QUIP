3483d16955bb9bda68ef6f9b25ec1e6187710a3b
gabor1 <gabor1@users.noreply.github.com>
2023-04-24 01:57:42 +0000

    deploy: f17aef9519f1b103d00d41cbc16ca71f4373b35b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '15eeaea',
+    VERSION: 'f17aef9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
