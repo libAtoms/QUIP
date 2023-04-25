6d529fd45431683d20902591f20d2b81c9d49d6d
gabor1 <gabor1@users.noreply.github.com>
2023-04-25 11:30:25 +0000

    deploy: 0cbbb02be31972a154ca1222642c2e8002cfb172

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'f17aef9',
+    VERSION: '0cbbb02',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
