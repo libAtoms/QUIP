2d3c0dfde3d618efa034d883cfb3a1456f3a316e
albapa <albapa@users.noreply.github.com>
2023-05-19 15:12:53 +0000

    deploy: eb1c9ef8477d2c278430c0812f4562e5c9ac8bf9

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '47fe810',
+    VERSION: 'eb1c9ef',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
