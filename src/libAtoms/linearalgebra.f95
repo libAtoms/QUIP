a67e0e5469948e83b6af30c5ef2dee72d857f335
Sascha Klawohn <sascha.klawohn@warwick.ac.uk>
2023-05-21 18:20:14 +0100

    Conform insertion_sort signature, adjust calls
    Make insertion_sort behave like the other sort procedures, co-sorting a
    provded optional integer array instead of filling it with indices first.
    Where necessary, arrays are filled before the calls.

--- src/libAtoms/linearalgebra.f95
+++ src/libAtoms/linearalgebra.f95
@@ -5988,32 +5988,28 @@ endsubroutine
 
    endsubroutine heap_sort_i_2dim
 
-   !% Do an in place insertion sort on 'this', in ascending order.
-   !% If 'idx' is present  then on exit it will contain the list
-   !% of indices into 'this' permuted in the same way as the entries
-   !% have been.
-   subroutine insertion_sort_i(this, idx)
-     integer, intent(inout), dimension(:) :: this
-     integer, intent(out), dimension(size(this)), optional :: idx
+   !% Sort an array of integers into ascending order (stable, slow: scales as N$^2$).
+   !% i_data is an accompanying array of integers on which the same reordering is performed
+   subroutine insertion_sort_i(array, i_data)
+     integer, intent(inout), dimension(:) :: array
+     integer, intent(inout), dimension(size(array)), optional :: i_data
 
      integer :: i, vi, j, v
      vi = 1
 
-     if(present(idx)) idx = (/ (i, i=1,size(this)) /)
-
-     do i = 2, size(this)
-        if (this(i) >= this(i-1)) cycle
-        v = this(i)
-        if (present(idx)) vi = idx(i)
+     do i = 2, size(array)
+        if (array(i) >= array(i-1)) cycle
+        v = array(i)
+        if (present(i_data)) vi = i_data(i)
 
         j = i-1
         do while (j >= 1)
-           if (v > this(j)) exit
-           this(j+1) = this(j)
-           this(j) = v
-           if (present(idx)) then
-              idx(j+1) = idx(j)
-              idx(j) = vi
+           if (v > array(j)) exit
+           array(j+1) = array(j)
+           array(j) = v
+           if (present(i_data)) then
+              i_data(j+1) = i_data(j)
+              i_data(j) = vi
            end if
            j = j - 1
         end do
@@ -6021,29 +6017,29 @@ endsubroutine
 
    end subroutine insertion_sort_i
 
-   subroutine insertion_sort_r(this, idx)
-     real(dp), intent(inout), dimension(:) :: this
-     integer, intent(out), dimension(size(this)), optional :: idx
+   !% Sort an array of reals into ascending order (stable, slow: scales as N$^2$).
+   !% i_data is an accompanying array of integers on which the same reordering is performed
+   subroutine insertion_sort_r(array, i_data)
+     real(dp), intent(inout), dimension(:) :: array
+     integer, intent(inout), dimension(size(array)), optional :: i_data
 
      integer :: i, vi, j
      real(dp) :: v
      vi = 1
 
-     if(present(idx)) idx = (/ (i, i=1,size(this)) /)
-
-     do i = 2, size(this)
-        if (this(i) >= this(i-1)) cycle
-        v = this(i)
-        if (present(idx)) vi = idx(i)
+     do i = 2, size(array)
+        if (array(i) >= array(i-1)) cycle
+        v = array(i)
+        if (present(i_data)) vi = i_data(i)
 
         j = i-1
         do while (j >= 1)
-           if (v > this(j)) exit
-           this(j+1) = this(j)
-           this(j) = v
-           if (present(idx)) then
-              idx(j+1) = idx(j)
-              idx(j) = vi
+           if (v > array(j)) exit
+           array(j+1) = array(j)
+           array(j) = v
+           if (present(i_data)) then
+              i_data(j+1) = i_data(j)
+              i_data(j) = vi
            end if
            j = j - 1
         end do
