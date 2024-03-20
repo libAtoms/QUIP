f23cec8038c92e54e3634423c839d993851749d5
gabor1 <gabor1@users.noreply.github.com>
2024-03-20 15:36:55 +0000

    deploy: caff154899626ec9d5c38e7b63dc5a0de6c05690

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'c0cee95',
+    VERSION: 'caff154',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
