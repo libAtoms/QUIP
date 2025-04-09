0b227646dd78da688f02a67b175f3bb125fee916
Albert Bartok-Partay <apbartok@gmail.com>
2025-04-09 17:51:15 +0100

    renamed source files to have F90 extension

--- src/libAtoms/f90wrap_stub.f95
+++
@@ -1,8 +0,0 @@
-! stub file to include dummy defition of Python error abort function when
-! building standalone exectuble
-subroutine f90wrap_abort(message)
-  character(*) :: message
-
-  ! do nothing
-  
-end subroutine f90wrap_abort
